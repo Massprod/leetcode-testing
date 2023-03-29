ec7dfee01c381c9d6b0df4e1fa3065fdc15df78d
Pampam <piankovpe@gmail.com>
2023-03-29 23:44:41 +0600

    add experience

--- most_simple/linked_list_2.py
+++ most_simple/linked_list_2.py
@@ -17,9 +17,39 @@ class ListNode:
 def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode] = None) -> Optional[ListNode]:
     print(l1)
     print(l2)
-
-
-
+    head = l1
+    values_l1 = []
+    values_l2 = []
+    while head:
+        val = head.val
+        head = head.next
+        values_l1.append(val)
+    head = l2
+    while head:
+        val = head.val
+        head = head.next
+        values_l2.append(val)
+    print(values_l1)
+    print(values_l2)
+    values_l1.reverse()
+    values_l2.reverse()
+    print(values_l1)
+    print(values_l2)
+    values_l1 = [str(_) for _ in values_l1]
+    values_l2 = [str(_) for _ in values_l2]
+    number_1 = int("".join(values_l1))
+    number_2 = int("".join(values_l2))
+    print(number_1)
+    print(number_2)
+    number_3 = number_1 + number_2
+    print(number_3)
+    new_list_value = list(str(number_3))
+    new_list_value.reverse()
+    print(new_list_value)
+    new_list = ListNode(val=new_list_value[0],
+                        next=ListNode(val=new_list_value[1],
+                                      next=new_list_value[2]))
+    print(new_list)
 
 
 
