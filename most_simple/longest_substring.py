37bd55645bbba05e4368cf6de23bec6f1c1b3ac9
Pampam <piankovpe@gmail.com>
2023-03-30 17:29:11 +0600

    reset

--- most_simple/longest_substring.py
+++ most_simple/longest_substring.py
@@ -1,21 +1,19 @@
 # Given a string s, find the length of the longest
 # substring without repeating characters
-from itertools import permutations
+
+
+# A string U is a substring of a string T if there exist two strings P and S such that
+# T = PUS. The empty string is a substring of every string
 
 def lengthOfLongestSubstring(s: str) -> int:
-    slice = list(s)
-    print(slice)
-    sub = ""
-    for x in range(1, len(slice) - 1):
-        if slice[x] == slice[x+1] and slice[x] not in sub:
-            sub += slice[x]
-        elif slice[x] != slice[x+1] and slice[x] not in sub:
-            sub += slice[x]
-    length = len(sub)
-    return length
+
+
 
 print(lengthOfLongestSubstring("abcabcbb"))
 print(lengthOfLongestSubstring("bbbbb"))
 print(lengthOfLongestSubstring("pwwkew"))
 print(lengthOfLongestSubstring(" "))
 print(lengthOfLongestSubstring(""))
+print(lengthOfLongestSubstring("aa"))
+print(lengthOfLongestSubstring("aabb"))
+print(lengthOfLongestSubstring("au"))
