247a44160917a9ce3d9abd607a3d34f23728fcb2
Pampam <piankovpe@gmail.com>
2023-04-10 22:37:38 +0600

    working_sol

--- most_simple/p15_3_sum.py
+++ most_simple/p15_3_sum.py
@@ -8,16 +8,18 @@ def three_sum(nums: list[int]) -> list[list[int]]:
     checked = ""
     sums = []
     for x in range(len(nums)):
-        first_slice = nums[x + 1:] + nums[:x]
-        if str(x) in checked:
+        first_slice = nums[:x] + nums[x + 1:]
+        if str(nums[x]) in checked:
             continue
         checked += str(nums[x])
         for y in range(len(first_slice)):
-            second_slice = first_slice[y + 1:] + first_slice[:y]
+            second_slice = first_slice[:y] + first_slice[y + 1:]
             for z in range(len(second_slice)):
-                if nums[x] + nums[y] + nums[z] == 0:
-                    sums.append([nums[x], nums[y], nums[z]])
-                    break
+                if nums[x] + first_slice[y] + second_slice[z] == 0:
+                    tempo = [nums[x], first_slice[y], second_slice[z]]
+                    tempo.sort()
+                    if tempo not in sums:
+                        sums.append(tempo)
     return sums
 
 
@@ -32,3 +34,12 @@ print(three_sum(test))
 #         print(tes_slice[j +1:] + tes_slice[:j])
 #         tes_slice2 = tes_slice[j + 1:] + tes_slice[:j]
 #         # for k in range(len(tes_slice2)):
+test1 = [-7, -4, -6, 6, 4, -6, -9, -10, -7, 5, 3, -1, -5, 8, -1, -2, -8, -1, 5, -3, -5, 4, 2, -5, -4, 4, 7]
+test1_out = [[-10, 2, 8], [-10, 3, 7], [-10, 4, 6], [-10, 5, 5], [-9, 2, 7], [-9, 3, 6], [-9, 4, 5], [-8, 2, 6],
+             [-8, 3, 5], [-8, 4, 4], [-7, -1, 8], [-7, 2, 5], [-7, 3, 4], [-6, -2, 8], [-6, -1, 7], [-6, 2, 4],
+             [-5, -3, 8], [-5, -2, 7], [-5, -1, 6], [-5, 2, 3], [-4, -4, 8], [-4, -3, 7], [-4, -2, 6], [-4, -1, 5],
+             [-3, -2, 5], [-3, -1, 4], [-2, -1, 3], [-1, -1, 2]]
+print(three_sum(test1))
+for _ in three_sum(test1):
+    assert _ in test1_out
+# my solution is working, but they don't order correctly for Leetcode test.........
