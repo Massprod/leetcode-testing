79b7e878c4b5092955e7be13d047dbfe15b3567d
Pampam <piankovpe@gmail.com>
2023-04-11 19:49:20 +0600

    initial

---
+++ most_simple/p16_3sum_closest.py
@@ -0,0 +1,16 @@
+# Given an integer array nums of length n and an integer target,
+# find three integers in nums such that the sum is closest to target.
+#
+# Return the sum of the three integers.
+#
+# You may assume that each input would have exactly one solution.
+
+
+def sum_closest(nums: list[int], target: int) -> int:
+    pass
+
+
+
+test1 = [-1, 2, 1, -4]
+test1_target = 1
+test1_out = 2
