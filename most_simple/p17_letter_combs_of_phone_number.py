116a869991baf9b2198bc8d1b9f6b64b537ccd4a
Pampam <piankovpe@gmail.com>
2023-04-12 19:07:32 +0600

    add working_sol (23.18%, 19.89%)

--- most_simple/p17_letter_combs_of_phone_number.py
+++ most_simple/p17_letter_combs_of_phone_number.py
@@ -26,24 +26,31 @@ def total_combs(digits: str) -> list[str]:
     to_use = []
     for digit in digits:
         to_use.append(options[digit])
-    print(to_use)
     for x in range(len(to_use[0])):
         first = to_use[0][x]
         for y in range(len(to_use[1])):
             second = to_use[1][y]
+            if len(to_use) == 2:
+                to_add = first + second
+                combs.append(to_add)
+                continue
             for z in range(len(to_use[2])):
                 third = to_use[2][z]
-                to_add = first + second + third
-                combs.append(to_add)
-
-
-
+                if len(to_use) == 3:
+                    to_add = first + second + third
+                    combs.append(to_add)
+                    continue
+                for g in range(len(to_use[3])):
+                    fourth = to_use[3][g]
+                    to_add = first + second + third + fourth
+                    combs.append(to_add)
     return combs
 
 
-# test1 = "23"
-# test1_out = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-# print(total_combs(test1))
+test1 = "23"
+test1_out = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+print(total_combs(test1))
+assert test1_out == total_combs(test1)
 # test2 = "23456789"
 # print(total_combs(test2))
 # combb = combinations(test2, r=2)
@@ -61,14 +68,15 @@ def total_combs(digits: str) -> list[str]:
 # test5 = "6"
 # print(total_combs(test5))
 # failed test5, cuz made a typo in dict m, m, o -> m, n, o  .......................nc
-# test6 = "22"
-# print(total_combs(test6))
+test6 = "22"
+print(total_combs(test6))
 # failed tes6, cuz im using SET and instead of using sorted() on str I was brute checking more or less and forgot equal
 test7 = "234"
 test7_out = ["adg","adh","adi","aeg","aeh","aei","afg","afh","afi","bdg","bdh","bdi","beg","beh","bei","bfg","bfh","bfi","cdg","cdh","cdi","ceg","ceh","cei","cfg","cfh","cfi"]
 print(total_combs(test7))
 assert test7_out == total_combs(test7)
-
-# rebuild. cuz we need to check whole number not just combs of 2 digits
-# test8 = "954"
-# print(total_combs(test8))
+# task limiting numbers from 0-4, so I can make brute options for 2, 3, 4 or ????
+# try to commit and not hit TLE with brute at least for 1 time
+# Brute is working 23.18% , 19.89%. But it's only for 0-4  it's a bad solution....
+test8 = "954"
+print(total_combs(test8))
