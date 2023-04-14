2387f611455a9bc9041bafe8b204df9cc5c5c87d
Pampam <piankovpe@gmail.com>
2023-04-14 23:58:08 +0600

    add working_sol (56.93%, 10.33%)

--- most_simple/p24_swap_nodes_in_pairs.py
+++ most_simple/p24_swap_nodes_in_pairs.py
@@ -10,21 +10,35 @@ class ListNode:
     def __str__(self):
         return f"{self.val} -> {self.next}"
 
+
 def swap_pairs(head: ListNode) -> ListNode:
-    pass
+    counter = 0
+    if not head:
+        return head
+    new = cursor = ListNode(val=head.val)
+    while head.next:
+        counter += 1
+        if counter % 2 != 0:
+            cursor.val = head.next.val
+        cursor.next = ListNode(val=head.val)
+        cursor = cursor.next
+        head = head.next
+    if counter % 2 == 0:
+        cursor.val = head.val
+    return new
 
-
-test1_values = [1, 2, 3, 4]
+test1_values = [1, 2, 3, 4, 5, 6, 8, 9, 10]
 test1 = cursor = ListNode(val=test1_values[0])
 for _ in test1_values[1:]:
     new_node = ListNode(val=_)
     cursor.next = new_node
-    cursor = new_node
-print(test1)
+    cursor = cursor.next
+# print(test1)
 test1_out_values = [2, 1, 4, 3]
 test1_out = cursor = ListNode(val=test1_out_values[0])
 for _ in test1_out_values[1:]:
     new_node = ListNode(val=_)
     cursor.next = new_node
-    cursor = new_node
-print(test1_out)
+    cursor = cursor.next
+# print(test1_out)
+print(swap_pairs(test1))
