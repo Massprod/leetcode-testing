c8b8ce5656fc3f6f67d29c25be4996f132bddabd
Pampam <piankovpe@gmail.com>
2023-04-14 21:13:06 +0600

    pre commit

--- most_simple/p23_merge_k_sorted.py
+++ most_simple/p23_merge_k_sorted.py
@@ -13,8 +13,46 @@ class ListNode:
 
 
 def merge_k_sorted(lists: list[ListNode]) -> ListNode:
-    pass
+    values = []
+    for _ in lists:
+        tempor = _
+        while tempor:
+            values.append(tempor.val)
+            tempor = tempor.next
+    values.sort()
+    new = ListNode(val=values[0])
+    cursor = new
+    for _ in values[1:]:
+        new_chain = ListNode(val=_)
+        cursor.next = new_chain
+        cursor = new_chain
+    return new
 
 
-test1 = [[1, 4, 5], [1, 3, 4], [2, 6]]
-test1_out = [1, 1, 2, 3, 4, 4, 5, 6]
+test_values = [[1, 4, 5], [1, 3, 4], [2, 6]]
+test1 = []
+# creating linked lists to work with
+for val in test_values:
+    temp = ListNode(val=val[0])
+    tempo = temp
+    for _ in val[1:]:
+        new_node = ListNode(val=_)
+        tempo.next = new_node
+        tempo = new_node
+    test1.append(temp)
+test1_out_values = [1, 1, 2, 3, 4, 4, 5, 6]
+# creating linked list to test with
+test1_out = ListNode(val=test1_out_values[0])
+tempo = test1_out
+for _ in test1_out_values[1:]:
+    new_node = ListNode(val=_)
+    tempo.next = new_node
+    tempo = new_node
+print(test1_out)
+print(merge_k_sorted(test1))
+existed = test1_out
+new = merge_k_sorted(test1)
+while new:
+    assert new.val == existed.val
+    new = new.next
+    existed = existed.next
