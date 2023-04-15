2c64db096e8788da924429cd57a96970f7368f93
Pampam <piankovpe@gmail.com>
2023-04-15 21:58:58 +0600

    add working_sol (8.70%, 9.26%)

--- most_simple/p28_find_index_first_occur.py
+++ most_simple/p28_find_index_first_occur.py
@@ -5,13 +5,14 @@
 
 
 def first_occur(haystack: str, needle: str) -> int:
+    # working_sol (8.70%, 9.26%)
     if len(needle) > len(haystack):
         return -1
 
     def in_slice(sliced: str, check: str):
         if len(check) == 0:
             return True
-        elif sliced[0] == check[0]:
+        elif len(sliced) > 0 and sliced[0] == check[0]:
             if in_slice(sliced[1:], check[1:]):
                 return True
         return False
@@ -22,13 +23,23 @@ def first_occur(haystack: str, needle: str) -> int:
     return -1
 
 
+# Rushed solution and didn't think about lengths - failed 2 submits for no reason w.e
+# Wanted to solve it with recursion.
+
+
 test1 = "sadbutsad"
 test1_target = "sad"
 test1_out = 0
 print(first_occur(test1, test1_target))
 test2 = "aaa"
-test2_target = "aaaa"
+test2_target = "aaa"
 print(first_occur(test2, test2_target))
 test3 = "mississippi"
 test3_target = "issipi"
 print(first_occur(test3, test3_target))
+test4 = "leetcode"
+test4_target = "leeto"
+print(first_occur(test4, test4_target))
+test5 = "hello"
+test5_target = "ll"
+print(first_occur(test5, test5_target))
