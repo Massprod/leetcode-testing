63a63f5c0ffa6a7799111f1b9342c6b4795cf2cb
Pampam <piankovpe@gmail.com>
2023-04-17 23:23:43 +0600

    tests

--- most_simple/p33_search_in_rotated.py
+++ most_simple/p33_search_in_rotated.py
@@ -12,9 +12,63 @@
 
 
 def search_rotated(nums: list[int], target: int) -> int:
-    pass
+    if len(nums) == 0:
+        return -1
+    elif len(nums) == 1:
+        if nums[0] == target:
+            return 0
+        else:
+            return -1
+    # if rotated
+    elif nums[-1] < nums[0]:
+        if nums[-1] < target < nums[0]:
+            return -1
+        elif target >= nums[0]:
+            for x in range(len(nums)):
+                if nums[x] == target:
+                    return x
+                elif nums[x] > nums[x + 1]:
+                    return -1
+        elif target <= nums[-1]:
+            for x in range(len(nums) - 1, -1, -1):
+                if nums[x] == target:
+                    return x
+                elif nums[x] < nums[x - 1]:
+                    return -1
+    # if not rotated
+    elif target > nums[-1]:
+        return -1
+    elif target < nums[0]:
+        return -1
+    elif nums[0] <= target <= nums[-1]:
+        for x in range(len(nums)):
+            if nums[x] == target:
+                return x
 
 
+# if rotated -> nums[-1] always lower than nums[0]
+# else nums[0] always lower than nums[-1]
 test1 = [4, 5, 6, 7, 0, 1, 2]
 test1_target = 0
 test1_out = 4
+print(search_rotated(test1, test1_target))
+
+test2 = [1]
+test2_target = 1
+test2_out = 0
+print(search_rotated(test2, test2_target))
+
+test3 = []
+test3_target = 5
+test3_out = -1
+print(search_rotated(test3, test3_target))
+
+test4 = [1, 2, 3, 4, 5, 6, 7]
+test4_target = 4
+test4_out = 3
+print(search_rotated(test4, test4_target))
+
+test5 = [4, 5, 6, 7, 0, 1, 2]
+test5_target = 0
+test5_out = 4
+print(search_rotated(test5, test5_target))
