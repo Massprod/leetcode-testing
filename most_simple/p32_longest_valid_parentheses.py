ff34a0d17032a108efb90e2d9b43cabafe5b4481
Pampam <piankovpe@gmail.com>
2023-04-17 17:47:17 +0600

    initial

---
+++ most_simple/p32_longest_valid_parentheses.py
@@ -0,0 +1,12 @@
+# Given a string containing just the characters '(' and ')',
+# return the length of the longest valid (well-formed) parentheses substring.
+
+def longest_parent(s: str) -> int:
+    pass
+
+
+test1 = "(()"
+test1_out = 2
+
+test2 = ")()())"
+test2_out = 4
