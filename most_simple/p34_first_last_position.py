09cf66047c48789210c4092b3104add942ee4097
Pampam <piankovpe@gmail.com>
2023-04-18 16:34:51 +0600

    initial

---
+++ most_simple/p34_first_last_position.py
@@ -0,0 +1,20 @@
+# Given an array of integers nums sorted in non-decreasing order,
+# find the starting and ending position of a given target value.
+#
+# If target is not found in the array, return [-1, -1].
+#
+# You must write an algorithm with O(log n) runtime complexity.
+
+
+def first_last(nums: list[int], target: int) -> list[int]:
+    pass
+
+
+test1 = [5, 7, 7, 8, 8, 10]
+test1_target = 8
+test1_out = [3, 4]
+
+
+test2 = [5, 7, 7, 8, 8, 10]
+test2_target = 6
+test2_out = [-1, -1]
