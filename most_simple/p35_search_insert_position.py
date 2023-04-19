ecd54f3504f3d78d1162aacaf55380c9921f4446
Pampam <piankovpe@gmail.com>
2023-04-19 19:05:23 +0600

    pre commit

--- most_simple/p35_search_insert_position.py
+++ most_simple/p35_search_insert_position.py
@@ -4,13 +4,41 @@
 # You must write an algorithm with O(log n) runtime complexity.
 
 def search_insert(nums: list[int], target: int) -> int:
-    pass
+    length = len(nums)
+    if length == 0:
+        return 0
+    if nums[-1] < target:
+        return length
+    if nums[0] > target:
+        return 0
+    x, y = 0, (length - 1)
+    while x < y:
+        if nums[x] == target:
+            return x
+        if nums[y] == target:
+            return y
+        if target > nums[x]:
+            if target < nums[x + 1] and target != nums[x +1]:
+                return x + 1
+        if target < nums[y]:
+            if target > nums[y - 1] and target != nums[y - 1]:
+                return y
+        if nums[x] < target < nums[y]:
+            x += 1
+            y -= 1
 
 
 test1 = [1, 3, 5, 6]
 test1_target = 5
 test1_out = 2
+print(search_insert(test1, test1_target))
 
 test2 = [1, 3, 5, 6]
 test2_target = 2
 test2_out = 1
+print(search_insert(test2, test2_target))
+
+test3 = [1, 3, 5, 6]
+test3_target = 7
+test3_out = 4
+print(search_insert(test3, test3_target))
