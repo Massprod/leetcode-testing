4e48606d88c18f9ee8683ba0316f7128cf5fac0d
Pampam <piankovpe@gmail.com>
2023-04-19 18:54:23 +0600

    initial

---
+++ most_simple/p35_search_insert_position.py
@@ -0,0 +1,16 @@
+# Given a sorted array of distinct integers and a target value,
+# return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+
+def search_insert(nums: list[int], target: int) -> int:
+    pass
+
+
+test1 = [1, 3, 5, 6]
+test1_target = 5
+test1_out = 2
+
+test2 = [1, 3, 5, 6]
+test2_target = 2
+test2_out = 1
