3229592900fbd82c05f83f455901d42f8bf693e3
Pampam <piankovpe@gmail.com>
2023-04-20 23:16:45 +0600

    rename/revisit

--- most_simple/p8_string_to_int.py
+++ most_simple/p8_string_to_int.py
@@ -1,12 +1,20 @@
-# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+# Implement the myAtoi(string s) function, which converts a string
+# to a 32-bit signed integer (similar to C/C++'s atoi function).
 #
 # The algorithm for myAtoi(string s) is as follows:
 #
 # Read in and ignore any leading whitespace.
-# Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
-# Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
-# Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
-# If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+# Check if the next character (if not already at the end of the string) is '-' or '+'.
+# Read this character in if it is either. This determines if the final result is negative or positive respectively.
+# Assume the result is positive if neither is present.
+# Read in next the characters until the next non-digit character or the end of the input is reached.
+# The rest of the string is ignored.
+# Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0.
+# Change the sign as necessary (from step 2).
+# If the integer is out of the 32-bit signed integer range [-231, 231 - 1],
+# then clamp the integer so that it remains in the range.
+# Specifically, integers less than -231 should be clamped to -231,
+# and integers greater than 231 - 1 should be clamped to 231 - 1.
 # Return the integer as the final result.
 # Note:
 #
@@ -14,85 +22,7 @@
 # Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
 
 def my_atoi(s: str) -> int:
-    # to_check = [str(_) for _ in range(0, 10)]
-    # sign = 1
-    # digits = ""
-    # for char in s:
-    #     if char == "-":
-    #         sign *= -1
-    #     elif char not in to_check and len(digits) != 0:
-    #         break
-    #     elif char not in to_check:
-    #         continue
-    #     elif char in to_check:
-    #         digits += char
-    # if len(digits) == 0:
-    #     return 0
-    # num = int(digits) * sign
-    # min_num = -2 ** 31
-    # max_num = 2 ** 31 - 1
-    # if min_num < num < max_num:
-    #     return num
-    # elif num <= min_num:
-    #     return min_num
-    # elif num >= max_num:
-    #     return max_num
-    # Working but not with correct rules, leave it anyway
-    # to_check = [str(_) for _ in range(0, 10)]
-    # sign = 1
-    # digits = ""
-    # start = " "
-    # string_dict = {}
-    # for key, value in enumerate(s):
-    #     string_dict[key] = value
-    # lenght = len(string_dict)
-    # for x in range(lenght):
-    #     if x == lenght - 1 and string_dict[x] not in to_check:
-    #         break
-    #     elif x == lenght - 1 and string_dict[x] in to_check:
-    #         digits += string_dict[x]
-    #     elif string_dict[x] == start and string_dict[x + 1] != start:
-    #         for y in range(x + 1, lenght):
-    #             if string_dict[y] == "-":
-    #                 sign *= -1
-    #             elif string_dict[y] == "+":
-    #                 sign *= 1
-    #             elif string_dict[y] in to_check:
-    #                 digits += string_dict[y]
-    #             elif string_dict[y] not in to_check:
-    #                 break
-    #         break
-    #     elif string_dict[x] in to_check:
-    #         digits += string_dict[x]
-    #         for z in range(x + 1, lenght):
-    #             if string_dict[z] == "-":
-    #                 sign *= -1
-    #             elif string_dict[z] == "+":
-    #                 sign *= 1
-    #             elif string_dict[z] in to_check:
-    #                 digits += string_dict[z]
-    #             elif string_dict[z] not in to_check:
-    #                 break
-    #         break
-    #     elif string_dict[x] == "-":
-    #         sign *= -1
-    #     elif string_dict[x] == ".":
-    #         break
-    #     elif string_dict[x] not in to_check:
-    #         continue
-    #
-    # if len(digits) == 0:
-    #     return 0
-    # num = int(digits) * sign
-    # min_num = -2 ** 31
-    # max_num = 2 ** 31 - 1
-    # if min_num < num < max_num:
-    #     return num
-    # elif num <= min_num:
-    #     return min_num
-    # elif num >= max_num:
-    #     return max_num
-
+    # working_sol (76.50%, 16.30%)  time: O(n) | space: O(n)
     to_check = [str(_) for _ in range(0, 10)]
     digits = ""
     whitespace = " "
@@ -141,6 +71,9 @@ def my_atoi(s: str) -> int:
     elif num >= max_num:
         return max_num
 
+
+# Time complexity: O(n) -> in worst case we are going through whole LIST.
+# Space complexity: O(n) -> creating new dictionary of input len.
 
 # Overall it's really easy Task, but I was not careful with rules and expected this to READ ANY STRING
 # and return ANY number in it. Even if we had more than 1 sign to change and no matter what was before DIGIT
