adab05610d693ea27cfc035f511965616f711b97
Pampam <piankovpe@gmail.com>
2023-04-22 16:34:55 +0600

    tests

--- leetcode_problems/p39_combination_sum.py
+++ leetcode_problems/p39_combination_sum.py
@@ -15,20 +15,61 @@ def comb_sum(candidates: list[int], target: int) -> list[list[int]]:
     combos = []
     tempo = []
 
-    def combinations(sliced: list[int], summ: int) -> list[int]:
-        pass
-    combinations(candidates, target)
+    def combinations(sliced: list[int], temp: list) -> None:
+        if sum(temp) == target:
+            to_add = temp.copy()
+            to_add.sort()
+            if to_add not in combos:
+                combos.append(to_add)
+                return
+        if sum(temp) > target:
+            return
+        for _ in sliced:
+            temp.append(_)
+            combinations(sliced, temp)
+            temp.pop()
+
+    for _ in candidates:
+        tempo.append(_)
+        combinations(candidates, tempo)
+        tempo.pop()
     return combos
 
 
 test1 = [2, 3, 6, 7]
 test1_target = 7
 test1_out = [[2, 2, 3], [7]]
+print(comb_sum(test1, test1_target))
+assert comb_sum(test1, test1_target) == test1_out
 
 test2 = [2, 3, 5]
 test2_target = 8
 test2_out = [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
+print(comb_sum(test2, test2_target))
+assert comb_sum(test2, test2_target) == test2_out
 
 test3 = [2]
 test3_target = 1
 test3_out = []
+print(comb_sum(test3, test3_target))
+assert comb_sum(test3, test3_target) == test3_out
+
+# Test4 - failed, -> I was using sort to check if there's duplicate already existing in COMBOS.
+#                   Stupid enough to SORT temp where's values going to be removed by POP() and it's always right_most
+#                   obviously after sorting it's not last_value added......
+#                   There's 2 way to work it fine is to make copy and sort it before appending,
+#                   or we have all DISTINCT_DIGITS and I could use remove(value) to remove last added value.
+#                   Because it's a SUM there's no matter what order of this value in a temp would be.
+test4 = [7, 3, 2]
+test4_target = 18
+test4_out = [
+    [7, 7, 2, 2],
+    [7, 3, 3, 3, 2],
+    [7, 3, 2, 2, 2, 2],
+    [3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 2, 2, 2],
+    [3, 3, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2]
+]
+print(comb_sum(test4, test4_target))
+assert test4_out == comb_sum(test4, test4_target)
