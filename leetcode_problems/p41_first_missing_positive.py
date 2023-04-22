dfd44f80b0a059ea93fac17fe164332ab3db0247
Pampam <piankovpe@gmail.com>
2023-04-22 22:35:39 +0600

    initial

---
+++ leetcode_problems/p41_first_missing_positive.py
@@ -0,0 +1,17 @@
+# Given an unsorted integer array nums, return the smallest missing positive integer.
+#
+# You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+def first_positive(nums: list[int]) -> int:
+    pass
+
+
+test1 = [1, 2, 0]
+test1_out = 3
+
+test2 = [3, 4, -1, 1]
+test2_out = 2
+
+test3 = [7, 8, 9, 11, 12]
+test3_out = 1
+
