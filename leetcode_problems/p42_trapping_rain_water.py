c1d07754c8bafdf1ee1f86b0167b43297cb76761
Pampam <piankovpe@gmail.com>
2023-04-23 18:06:42 +0600

    initial

---
+++ leetcode_problems/p42_trapping_rain_water.py
@@ -0,0 +1,13 @@
+# Given n non-negative integers representing an elevation
+# map where the width of each bar is 1,
+# compute how much water it can trap after raining.
+
+def rain_water(height: list[int]) -> int:
+    pass
+
+
+test1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+test1_out = 6
+
+test2 = [4, 2, 0, 3, 2, 5]
+test2_out = 9
