26d039ec17243be789db65bf7c96189c01ffd877
Pampam <piankovpe@gmail.com>
2023-04-23 14:23:57 +0600

    initial

---
+++ leetcode_problems/p43_multiply_strings.py
@@ -0,0 +1,16 @@
+# Given two non-negative integers num1 and num2 represented as strings,
+# return the product of num1 and num2, also represented as a string.
+#
+# Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+def multiply_ascii(num1: str, num2: str) -> str:
+    pass
+
+
+test1_1 = "2"
+test1_2 = "3"
+test1_out = "6"
+
+test2_1 = "123"
+test2_2 = "456"
+test2_out = "56088"
