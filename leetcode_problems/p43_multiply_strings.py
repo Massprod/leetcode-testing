8d5e3e8cf955f42e5b417a33b85e82dda6fc20ed
Pampam <piankovpe@gmail.com>
2023-04-23 15:17:35 +0600

    add working_sol (73.95%, 95.23%) -> (34ms, 13.8mb)

--- leetcode_problems/p43_multiply_strings.py
+++ leetcode_problems/p43_multiply_strings.py
@@ -45,12 +45,62 @@ def multiply_ascii(num1: str, num2: str) -> str:
 # But what about product to str? Try to rebuild without using str()
 
 
+def multiply_ascii_no_str(num1: str, num2: str) -> str:
+    # working_sol (73.95%, 95.23%) -> (34ms, 13.8mb)  time: O(n) | space: O(n)
+    to_check = {
+        "0": 48,
+        "1": 49,
+        "2": 50,
+        "3": 51,
+        "4": 52,
+        "5": 53,
+        "6": 54,
+        "7": 55,
+        "8": 56,
+        "9": 57,
+    }
+    first = []
+    for _ in num1:
+        first.append(to_check[_] - 48)
+    second = []
+    for _ in num2:
+        second.append(to_check[_] - 48)
+    first_int = 0
+    for x in range(len(first)):
+        first_int += first[x] * 10 ** ((len(first) - 1) - x)
+    second_int = 0
+    for y in range(len(second)):
+        second_int += second[y] * 10 ** ((len(second) - 1) - y)
+    product = first_int * second_int
+    if product == 0:
+        return chr(48 + product % 10)
+    product_list = []
+    while product:
+        product_list.append(chr(48 + product % 10))
+        product = product // 10
+    product_str = "".join(reversed(product_list))
+    return product_str
+
+# Time complexity: O(n) -> linear scaling with len() of input.
+# Space complexity: O(n) -> creating 3 lists of the same size as input.
+# Guess, now it's not using anything: no ord(), no str() -> somewhat reproducing them, actually.
+
+
 test1_1 = "2"
 test1_2 = "3"
 test1_out = "6"
 print(multiply_ascii(test1_1, test1_2))
+print(multiply_ascii_no_str(test1_1, test1_2))
 
 test2_1 = "123"
 test2_2 = "456"
 test2_out = "56088"
 print(multiply_ascii(test2_1, test2_2))
+print(multiply_ascii_no_str(test2_1, test2_2))
+
+# Failed with product == 0. It didn't go into a loop -> unique option, we can just return chr(48) for this.
+test3_1 = "0"
+test3_2 = "0"
+test3_out = "0"
+print(multiply_ascii(test3_1, test3_2))
+print(multiply_ascii_no_str(test3_1, test3_2))
