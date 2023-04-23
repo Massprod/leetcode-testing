318074e05ed2e6260353c2eb140c8e5d0cd5882c
Pampam <piankovpe@gmail.com>
2023-04-23 20:37:36 +0600

    add working_sol (5.3%, 35.77%) -> (3790ms, 16.1mb)

--- leetcode_problems/p42_trapping_rain_water.py
+++ leetcode_problems/p42_trapping_rain_water.py
@@ -3,6 +3,7 @@
 # compute how much water it can trap after raining.
 
 def rain_water(height: list[int]) -> int:
+    # working_sol (5.3%, 35.77%) -> (3790ms, 16.1mb)  time: O(n**n) | space: O(n)
     volumes = []
     left = 0
     length = len(height)
@@ -29,12 +30,23 @@ def rain_water(height: list[int]) -> int:
                     volumes.append(tempo_vol - busy_boxes)
                     left = right
                     break
+                if height[x] <= height[x + 1] and (height[x] >= height[left] or height[x] >= max(height[left + 1:])):
+                    right = x
+                    rect_height = min(height[left], height[x])
+                    rect_length = right - left - 1
+                    tempo_vol = rect_height * rect_length
+                    volumes.append(tempo_vol - busy_boxes)
+                    left = right
+                    break
                 busy_boxes += height[x]
-                if x == length - 1:
-                    left += 1
         elif height[left + 1] >= height[left]:
             left += 1
     return sum(volumes)
+
+# Time complexity: O(n**n) -> looping through whole input, and for every value looping through left values slice.
+#                             worst case -> looping through every index
+# Space complexity: O(n) -> volumes and slice sizes all depends on input_size, we're not storing slices for max(height)
+#                           there's always one n - size slice and volumes of n - size depending on input.
 
 
 
@@ -53,3 +65,14 @@ print(rain_water(test3))
 test4 = [10, 11, 12, 8, 0, 4, 4, 4]
 test4_out = 4
 print(rain_water(test4))
+
+# test5 - failed -> cuz I wasn't taking in credit that we can encounter right_wall, without diff point ->
+#                   height[x] < height[x + 1] but height[x + 1] > height[left] and height[left] < height[x]
+#                   (diff point -> point where water can stack on both sides of this point)
+test5 = [5, 5, 4, 7, 8, 2, 6, 9, 4, 5]
+test5_out = 10
+print(rain_water(test5))
+
+test6 = [5, 5, 4, 7, 7, 8, 2, 6, 9, 4, 5]
+test6_out = 10
+print(rain_water(test6))
