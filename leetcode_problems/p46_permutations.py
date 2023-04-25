26a3eccc77c8942124b32636083f53101059401d
Pampam <piankovpe@gmail.com>
2023-04-25 21:57:14 +0600

    add working_sol (63.90%, 49.5%) -> (46ms, 14.1mb)

--- leetcode_problems/p46_permutations.py
+++ leetcode_problems/p46_permutations.py
@@ -1,8 +1,10 @@
 # Given an array nums of distinct integers, return all the possible permutations.
 # You can return the answer in any order.
+import math
 
 
 def permute(nums: list[int]) -> list[list[int]]:
+    # working_sol (63.90%, 49.5%) -> (46ms, 14.1mb)  time: O(n!) | space: O(n!)
     permutes = []
     origin = nums
 
@@ -17,10 +19,14 @@ def permute(nums: list[int]) -> list[list[int]]:
     rec_permute(origin)
     return permutes
 
+# Time complexity: O(n*n!) -> calling recursion for n elements in given input,
+#                             and for each element recursion for n! times.
+# Space complexity: O(n!) -> creating lists with n! lists in it, n! <- all combinations of input list.
 
 # Backtracking almost like in p22 with parentheses, but we're removing element not adding it,
 # and switching every index on every position from ! 0-len()-1 ! until we run out of them | start_ind == len(to_check)
-# After switching, always rebuilding list to_check in a pre_recursion call state.
+# After switching, always rebuilding list to_check into pre_recursion state.
+
 
 test1 = [1, 2, 3]
 test1_out = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
@@ -45,3 +51,7 @@ for _ in test3_out:
     test = permute(test3)
     assert len(test3_out) == len(test)
     assert _ in test
+
+test4 = [1, 2, 3, 4, 5, 6, 7]
+print(math.factorial(len(test4)))
+print(len(permute(test4)))
