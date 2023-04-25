f71d56931d278b59dfa2eb081d5ed8863bf2c57c
Pampam <piankovpe@gmail.com>
2023-04-25 21:14:16 +0600

    initial

---
+++ leetcode_problems/p46_permutations.py
@@ -0,0 +1,16 @@
+# Given an array nums of distinct integers, return all the possible permutations.
+# You can return the answer in any order.
+
+
+def permute(nums: list[int]) -> list[list[int]]:
+    pass
+
+
+test1 = [1, 2, 3]
+test1_out = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+test2 = [0, 1]
+test2_out = [[0, 1], [1, 0]]
+
+test3 = [1]
+test3_out = [[1]]
