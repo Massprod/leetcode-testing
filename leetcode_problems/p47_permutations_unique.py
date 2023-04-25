162ee471faffcdb9d1dd275a7bb18f245df3e799
Pampam <piankovpe@gmail.com>
2023-04-25 22:01:40 +0600

    initial

---
+++ leetcode_problems/p47_permutations_unique.py
@@ -0,0 +1,25 @@
+# Given a collection of numbers, nums, that might contain duplicates,
+# return all possible unique permutations in any order.
+
+
+def permute_unique(nums: list[int]) -> list[list[int]]:
+    pass
+
+
+test1 = [1, 1, 2]
+test1_out = [
+    [1, 1, 2],
+    [1, 2, 1],
+    [2, 1, 1]
+]
+
+test2 = [1, 2, 3]
+
+test2_out = [
+    [1, 2, 3],
+    [1, 3, 2],
+    [2, 1, 3],
+    [2, 3, 1],
+    [3, 1, 2],
+    [3, 2, 1]
+]
