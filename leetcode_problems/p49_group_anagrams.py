bc154a8ba7d8632d7c2b0a408d9034b63a0fd9c2
Pampam <piankovpe@gmail.com>
2023-04-26 14:16:03 +0600

    pre commit

--- leetcode_problems/p49_group_anagrams.py
+++ leetcode_problems/p49_group_anagrams.py
@@ -4,14 +4,36 @@
 # typically using all the original letters exactly once.
 
 def group_anagrams(strs: list[str]) -> list[list[str]]:
-    pass
+    ords = {}
+    anagrams = []
+    for word in strs:
+        summ = 0
+        for letter in word:
+            summ += ord(letter)
+        if summ in ords.keys():
+            ords[summ].append(word)
+        else:
+            ords[summ] = []
+            ords[summ].append(word)
+    for key, value in ords.items():
+        anagrams.append(value)
+    return anagrams
 
+
+# strs[i] consists of lowercase English letters.
+# Let's just try to count ascii of whole word and filter with dict.
+# What I can be missing? What about upper cases?
+# Ohh -> ! strs[i] consists of lowercase English letters. !
+# If there were uppercase's it could be a fail, cuz A and a different ascii but same letter for the anagram.
 
 test1 = ["eat", "tea", "tan", "ate", "nat", "bat"]
 test1_out = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+print(group_anagrams(test1))
 
 test2 = [""]
 test2_out = [[""]]
+print(group_anagrams(test2))
 
 test3 = ["a"]
 test3_out = [["a"]]
+print(group_anagrams(test3))
