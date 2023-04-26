6e9a358bfb63e8999f9a29b1e20e848faba88cac
Pampam <piankovpe@gmail.com>
2023-04-26 16:01:32 +0600

    initial

---
+++ leetcode_problems/p50_power.py
@@ -0,0 +1,17 @@
+# Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+def my_power(x: float, n: int) -> float:
+    pass
+
+
+test1 = 2.00000
+test1_pow = 10
+test1_out = 1024.00000
+
+test2 = 2.10000
+test2_pow = 3
+test2_out = 9.26100
+
+test3 = 2.00000
+test3_pow = -2
+test3_out = 0.25000
