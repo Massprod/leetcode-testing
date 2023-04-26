e915397076e6b49827766a1c4b79905a9ea00cb3
Pampam <piankovpe@gmail.com>
2023-04-26 14:04:23 +0600

    initial

---
+++ leetcode_problems/p49_group_anagrams.py
@@ -0,0 +1,17 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+
+def group_anagrams(strs: list[str]) -> list[list[str]]:
+    pass
+
+
+test1 = ["eat", "tea", "tan", "ate", "nat", "bat"]
+test1_out = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+
+test2 = [""]
+test2_out = [[""]]
+
+test3 = ["a"]
+test3_out = [["a"]]
