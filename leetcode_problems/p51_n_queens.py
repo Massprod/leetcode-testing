ac974611f7ed2ae159fdb175a4b1d3cd55bcf7dc
Pampam <piankovpe@gmail.com>
2023-04-28 15:48:24 +0600

    scratch

--- leetcode_problems/p51_n_queens.py
+++ leetcode_problems/p51_n_queens.py
@@ -6,13 +6,76 @@
 #
 # Each solution contains a distinct board configuration of the n-queens' placement,
 # where 'Q' and '.' both indicate a queen and an empty space, respectively.
+from copy import deepcopy
+
 
 def place_n_queens(n: int) -> list[list[str]]:
-    pass
+    ch_board = [["." for _ in range(n)] for _ in range(n)]
+    row_placements = {}
+    busy_box = {}
+
+    def check_pos(pos_y: int, pos_x: int) -> bool:
+        if not busy_box[(pos_y, pos_x)]:
+            for _ in range(n):
+                busy_box[(pos_y, _)] = True
+            for _ in range(n):
+                busy_box[(_, pos_x)] = True
+            try:
+                for _ in range(1, n):
+                    busy_box[(pos_y + _, pos_x + _)] = True
+            except KeyError:
+                pass
+            try:
+                for _ in range(1, n):
+                    busy_box[(pos_y + _, pos_x - _)] = True
+            except KeyError:
+                pass
+            return True
+        else:
+            return False
+
+    all_placements = []
+    starting_x = 0
+    starting_y = 0
+    while starting_x < n:
+        copy = deepcopy(ch_board)
+        q_count = 0
+        for free_y in range(n):
+            for free_x in range(n):
+                busy_box[(free_y, free_x)] = False
+        if q_count == 0:
+            for _ in busy_box:
+                busy_box[_] = False
+        if check_pos(starting_y, starting_x):
+            q_count += 1
+            row_placements[starting_y] = copy[starting_y]
+            row_placements[starting_y][starting_x] = "Q"
+            for y in range(n):
+                for x in range(n):
+                    if check_pos(y, x):
+                        q_count += 1
+                        row_placements[y] = copy[y]
+                        row_placements[y][x] = "Q"
+                        break
+        if q_count == 4:
+            variants = len(all_placements)
+            all_placements.append([])
+            for _ in range(n):
+                all_placements[variants].append("".join(row_placements[_]))
+        starting_x += 1
+    return all_placements
+
+
+# 1 -> there's always one Queen at any row.
+# 2 -> always going from top to bottom -> we can ignore top coordinates and check only: (y+1)(x+1) | (y+1)(x-1)
 
 
 test1 = 4
 test1_out = [[".Q..", "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]]
+test = place_n_queens(test1)
+print(test)
+# for _ in test:
+#     print(_)
 
 test2 = 1
 test2_out = [["Q"]]
