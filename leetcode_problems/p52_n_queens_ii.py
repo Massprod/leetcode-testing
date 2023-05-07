073b351709d848cb04a6ef0395299563a628c036
Pampam <piankovpe@gmail.com>
2023-05-07 18:34:53 +0600

    initial

---
+++ leetcode_problems/p52_n_queens_ii.py
@@ -0,0 +1,15 @@
+# The n-queens puzzle is the problem of placing n queens on an n x n chessboard
+# such that no two queens attack each other.
+#
+# Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+
+def queen_variants(n: int) -> int:
+    pass
+
+
+test1 = 4
+test1_out = 2
+
+test2 = 1
+test2_out = 1
