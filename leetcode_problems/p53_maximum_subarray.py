27fbc42588aaaab82c234497e1505ed4591ac6c2
Pampam <piankovpe@gmail.com>
2023-05-07 21:33:30 +0600

    add working_sol (94.96%, 12.97%) -> (660ms, 30.8mb)

--- leetcode_problems/p53_maximum_subarray.py
+++ leetcode_problems/p53_maximum_subarray.py
@@ -1,43 +1,28 @@
 # Given an integer array nums, find the subarray
 # with the largest sum, and return its sum.
-# ^^^^
-# Already been solving tasks with max_sum but for 3 nums, there's used all nums in some range.
-# Because of that, we can't skip values and need to check every possible index.
-# 100% sure I need to use conquer and divide but with recursion or just while loop?
-# Time limit or not, first going to try loop.
+
+# The idea of Kadane’s algorithm is to maintain a variable max_ending_here that stores the maximum sum
+# contiguous subarray ending at current index and a variable max_so_far stores the maximum sum of
+# contiguous subarray found so far, Everytime there is a positive-sum value in max_ending_here compare
+# it with max_so_far and update max_so_far if it is greater than max_so_far.
+# ^^
 
 def max_sub_array(nums: list[int]) -> int:
+    # working_sol (94.96%, 12.97%) -> (660ms, 30.8mb)  time: O(n) | space: O(1)
     if len(nums) == 1:
         return nums[0]
-    left_cursor = 0
-    right_cursor = len(nums) - 1
-    max_sum = 0
-    while left_cursor <= right_cursor:
-        temp_sum = sum(nums[left_cursor: right_cursor + 1])
-        if max_sum == 0 and temp_sum != 0:
-            max_sum = temp_sum
-        if temp_sum > max_sum:
-            max_sum = temp_sum
-        if left_cursor == right_cursor:
-            break
-        if nums[left_cursor] == 0:
-            left_cursor += 1
-            continue
-        if nums[right_cursor] == 0:
-            right_cursor -= 1
-            continue
-        left_step = nums[left_cursor] + nums[left_cursor + 1]
-        right_step = nums[right_cursor] + nums[right_cursor - 1]
-        if left_step > right_step:
-            right_cursor -= 1
-        if left_step < right_step:
-            left_cursor += 1
-        if left_step == right_step:
-            if nums[left_cursor + 1] > nums[right_cursor - 1]:
-                left_cursor += 1
-            else:
-                right_cursor -= 1
-    return max_sum
+    max_sub_sum: int = -10001  # -10**4 <= nums[i] <= 10**4
+    max_step_sum: int = 0
+    for x in range(0, len(nums)):
+        max_step_sum += nums[x]
+        if max_step_sum > max_sub_sum:
+            max_sub_sum = max_step_sum
+        if max_step_sum < 0:
+            max_step_sum = 0
+    return max_sub_sum
+
+# Time complexity: O(n) -> one whole loop through input array.
+# Space complexity: O(1) -> only input used, no extras.
 
 
 test1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
@@ -55,20 +40,16 @@ test3_out = 23
 assert test3_out == max_sub_array(test3)
 print(max_sub_array(test3))
 
-# test4 - failed -> I made default max_sum as 0, either I should have used minimum value -10^4 or ???
 test4 = [-2, -1]
 test4_out = -1
 assert test4_out == max_sub_array(test4)
 print(max_sub_array(test4))
 
-# test5 - failed -> max_value on one side which I wanted to skip with checking (-+1) indexes, but it's not working.
 test5 = [1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4]
 test5_out = 6
 assert test5_out == max_sub_array(test5)
 print(max_sub_array(test5))
 
-# test6 - failed -> I'm just calculating better step values, guess it's not what we need, or skip zeros??
-#                   cuz zeros doesn't make any changes, and we can skip them??
 test6 = [0, 0, -3, 1]
 test6_out = 1
 assert test6_out == max_sub_array(test6)
