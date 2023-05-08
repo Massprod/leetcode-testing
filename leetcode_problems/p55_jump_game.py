b6b4414e7b17e90ef2dd8d135636b2c3b91b2c4c
Pampam <piankovpe@gmail.com>
2023-05-08 22:28:52 +0600

    initial

---
+++ leetcode_problems/p55_jump_game.py
@@ -0,0 +1,14 @@
+# You are given an integer array nums. You are initially positioned at the array's first index,
+# and each element in the array represents your maximum jump length at that position.
+#
+# Return true if you can reach the last index, or false otherwise.
+
+def can_jump(nums: list[int]) -> bool:
+    pass
+
+
+test1 = [2, 3, 1, 1, 4]
+test1_out = True
+
+test2 = [3, 2, 1, 0, 4]
+test2_out = False
