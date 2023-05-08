bb31fc4d3f9fd9fe8cee22abe522fe50f3d4c717
Pampam <piankovpe@gmail.com>
2023-05-08 23:30:46 +0600

    add working_sol (37.98%, 5.2%) -> (507ms, 28.3mb)

--- leetcode_problems/p55_jump_game.py
+++ leetcode_problems/p55_jump_game.py
@@ -4,27 +4,37 @@
 # Return true if you can reach the last index, or false otherwise.
 
 def can_jump(nums: list[int]) -> bool:
+    # working_sol (37.98%, 5.2%) -> (507ms, 28.3mb)  time: O(n * log n) | space: O(n)
     end = len(nums) - 1
     if len(nums) == 1:
         return True
     if nums[0] >= end:
         return True
 
-    def can_reach(pool: list[int], start_ind: int = 0) -> bool:
-        if pool[start_ind] == 0:
+    def best_jump(pool: list[int], start_ind: int = 0):
+        if start_ind != end and pool[start_ind] == 0:
             return False
+        best_land_index = -1
+        best_start = -1
         for x in range(start_ind + 1, start_ind + pool[start_ind] + 1):
             land_index = pool[x] + x
             if land_index >= end:
                 return True
-            recur = can_reach(pool, land_index)
-            if recur:
-                return True
-            if not recur and (x == start_ind + pool[start_ind]):
-                return False
-    return can_reach(nums)
+            if land_index >= best_land_index:
+                best_land_index = land_index
+                best_start = x
+                continue
+        return best_jump(pool, best_start)
 
+    return best_jump(nums)
 
+# Time complexity: O(n * log n) -> we're looping through whole input array and checking available
+#                                  jump indexes for every index, worst case checking 3/4 extra.
+# Space complexity: O(n) -> only constants and input array for recursion stack.
+
+# ---------------------------
+# Failed by trying to STRIP my p45 version and make it work faster, without checking every jump, which isn't correct.
+# Could have avoided fails and just use it from the start, or just rebuild.
 # ---------------------------
 # p45 <- already solved similar but counting JUMPS.
 #        How we can't reach the end??
@@ -32,6 +42,8 @@ def can_jump(nums: list[int]) -> bool:
 #                                    and only if there's 0 we stop. Correct logic??
 #                                    Don't see any way we could stop jumping from START to END
 #                                    if there's no 0 on the way.
+#                                    ^^correct logic but I tricked myself to thinking about ONE_JUMP_ROUTE
+#                                      not considering any other jump_positions :) failed test5 cuz of this...
 
 
 test1 = [2, 3, 1, 1, 4]
@@ -65,3 +77,7 @@ test6 = [3, 0, 2, 2, 0, 1, 0, 1]
 test6_out = False
 assert can_jump(test6) == test6_out
 print(can_jump(test6))
+
+test7 = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]
+test7_out = True
+print(can_jump(test7))
