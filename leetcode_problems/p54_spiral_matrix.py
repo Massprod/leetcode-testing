b79a45b5180b9855c1e4978305bdf4e17a04d593
Pampam <piankovpe@gmail.com>
2023-05-08 21:00:26 +0600

    initial

---
+++ leetcode_problems/p54_spiral_matrix.py
@@ -0,0 +1,11 @@
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+
+def spiral_read(matrix: list[list[int]]) -> list[int]:
+    pass
+
+
+test1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+test1_out = [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+test2 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+test2_out = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
