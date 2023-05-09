3e003f5e8816598b9d5092424b34f224037f2a02
Pampam <piankovpe@gmail.com>
2023-05-09 19:58:49 +0600

    tests

--- leetcode_problems/p56_merge_intervals.py
+++ leetcode_problems/p56_merge_intervals.py
@@ -2,27 +2,28 @@
 # and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 def merge(intervals: list[list[int]]) -> list[list[int]]:
-    intervals.sort()
 
     def can_be_merged(merges: list[list[int]], start: int, end: int, start_index: int) -> dict:
-        merge_indexes = []
-        for y in range(start_index, len(merges)):
+        merge_values = []
+        for y in range(len(merges)):
+            if y == start_index:
+                continue
             check_start = merges[y][0]
             check_end = merges[y][1]
             if start <= check_start <= end <= check_end:
-                merge_indexes.append(y)
+                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
             elif check_start <= start <= check_end <= end:
-                merge_indexes.append(y)
+                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
             elif start <= check_start <= check_end <= end:
-                merge_indexes.append(y)
+                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
         merge_data = {
-            "indexes": merge_indexes,
+            "to_remove": merge_values,
             "new_limits": [start, end]
         }
         return merge_data
@@ -31,17 +32,22 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
         try:
             start_val = intervals[x][0]
             end_val = intervals[x][1]
-            if result := can_be_merged(intervals, start_val, end_val, x + 1):
-                to_remove = []
-                for index in result["indexes"]:
-                    to_remove.append(intervals[index])
-                for _ in to_remove:
-                    intervals.remove(_)
+            if result := can_be_merged(intervals, start_val, end_val, x):
                 intervals[x] = result["new_limits"]
+                for value in result["to_remove"]:
+                    intervals.remove(value)
         except IndexError:
             break
     return intervals
 
+
+# Changed to check every index in range(0, len(intervals)) -> now we don't need to sort and waste time on it.
+# Also changed replacing new_limits before we delete, to avoid missing indexes to replace with.
+# -------------------------------------
+# Hit a Time_Limit, but with 170/170 test and there's no info about this test_case...
+# Without rebuilding, I can remove sorting which is taking O(log n) from start, but then we can encounter
+# descending ordering and fail. W.e just fail to see this test and ordering.
+# -------------------------------------
 # If we assume there's only intervals input with ascending order, then it's working correctly.
 # But if it's not I need to rebuild or just sort it, cuz we're not double_checking merges after removing elements.
 # -------------------------------------
@@ -84,3 +90,10 @@ test4_out = [[0, 100]]
 for _ in merge(test4):
     assert _ in test4_out
 print(merge(test4))
+
+# test5 - failed -> exactly as I was expecting DESCENDING order..
+test5 = [[1, 4], [0, 5]]
+test5_out = [[0, 5]]
+for _ in merge(test5):
+    assert _ in test5_out
+print(merge(test5))
