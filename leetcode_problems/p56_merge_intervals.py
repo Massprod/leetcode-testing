875973dc038f21782c3c155cee913a21a6d973de
Pampam <piankovpe@gmail.com>
2023-05-09 18:02:00 +0600

    initial

---
+++ leetcode_problems/p56_merge_intervals.py
@@ -0,0 +1,12 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+# and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+def merge(intervals: list[list[int]]) -> list[list[int]]:
+    pass
+
+
+test1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
+test1_out = [[1, 6], [8, 10], [15, 18]]
+
+test2 = [[1, 4], [4, 5]]
+test2_out = [[1, 5]]
