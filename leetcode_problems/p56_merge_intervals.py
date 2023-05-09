287b942a7ccb872e16fe6f4ecadbdf6057f9272b
Pampam <piankovpe@gmail.com>
2023-05-09 19:27:42 +0600

    tests

--- leetcode_problems/p56_merge_intervals.py
+++ leetcode_problems/p56_merge_intervals.py
@@ -15,6 +15,10 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
                 merge_indexes.append(y)
                 start = min(start, check_start)
                 end = max(end, check_end)
+            elif start <= check_start <= check_end <= end:
+                merge_indexes.append(y)
+                start = min(start, check_start)
+                end = max(end, check_end)
         merge_data = {
             "indexes": merge_indexes,
             "new_limits": [start, end]
@@ -26,13 +30,22 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
             start_val = intervals[x][0]
             end_val = intervals[x][1]
             if result := can_be_merged(intervals, start_val, end_val, x + 1):
+                to_remove = []
                 for index in result["indexes"]:
-                    intervals.pop(index)
+                    to_remove.append(intervals[index])
+                for _ in to_remove:
+                    intervals.remove(_)
                 intervals[x] = result["new_limits"]
         except IndexError:
-            pass
+            break
     return intervals
 
+# Working for value in ascending order, but if there's -> [1, 5] [100, 100], [5, 100]
+#  descending after we already merged [1, 5] + [5, 100] -> [100, 100] will be just ignored.
+#  either loop more than one time, which isn't good, or I need to make smth.
+# -------------------------------------
+# ! try, except <- not pretty solution, but I was trying to solve it without creating new array. !
+# -------------------------------------
 # [1, 3] [2, 6]
 # start1 = 1, end1 = 3, start2 = 2, end2 = 6
 # start2 should be in range of (start1 - end1) -> start2 >= start1 and start2 <= end1
@@ -45,8 +58,18 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
 
 test1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
 test1_out = [[1, 6], [8, 10], [15, 18]]
+assert test1_out == merge(test1)
 print(merge(test1))
 
 test2 = [[1, 4], [4, 5]]
 test2_out = [[1, 5]]
+assert test2_out == merge(test2)
 print(merge(test2))
+
+test3 = [[1, 5], [4, 9], [9, 18], [5, 6], [2, 3], [4, 4], [0, 0]]
+test3_out = [[1, 18], [0, 0]]
+assert test3_out == merge(test3)
+print(merge(test3))
+
+test4 = [[0, 0], [0, 5], [4, 9], [4, 4], [100, 100], [5, 100]]
+print(merge(test4))
