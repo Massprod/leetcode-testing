095d5f66c83df08ff710b9ac14add4a14c66253f
Pampam <piankovpe@gmail.com>
2023-05-09 22:27:39 +0600

    time_limit

--- leetcode_problems/p56_merge_intervals.py
+++ leetcode_problems/p56_merge_intervals.py
@@ -2,76 +2,62 @@
 # and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 def merge(intervals: list[list[int]]) -> list[list[int]]:
-    new_intervals = []
-    new_intervals = [_ for _ in intervals if _ not in new_intervals]
 
-    def can_be_merged(merges: list[list[int]], start: int, end: int, start_index: int) -> dict:
-        merge_values: list = []
-        for y in range(len(merges)):
+    def can_merge(to_merge: list[list[int]], start_index: int) -> None:
+        start: int = to_merge[start_index][0]
+        end: int = to_merge[start_index][1]
+        y = 0
+        while y < len(to_merge):
             if y == start_index:
+                y += 1
                 continue
-            check_start = merges[y][0]
-            check_end = merges[y][1]
+            check_start = to_merge[y][0]
+            check_end = to_merge[y][1]
             if start <= check_start <= end <= check_end:
-                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
+                to_merge.pop(y)
+                to_merge[start_index] = [start, end]
+                if y > start_index:
+                    y = 0
+                continue
             elif check_start <= start <= check_end <= end:
-                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
+                to_merge.pop(y)
+                to_merge[start_index] = [start, end]
+                if y > start_index:
+                    y = 0
+                continue
             elif start <= check_start <= check_end <= end:
-                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
+                to_merge.pop(y)
+                to_merge[start_index] = [start, end]
+                if y > start_index:
+                    y = 0
+                continue
             elif check_start <= start <= end <= check_end:
-                merge_values.append(merges[y])
                 start = min(start, check_start)
                 end = max(end, check_end)
-        merge_data: dict = {
-            "to_remove": merge_values,
-            "new_limits": [start, end]
-        }
-        return merge_data
+                to_merge.pop(y)
+                to_merge[start_index] = [start, end]
+                if y > start_index:
+                    y = 0
+                continue
+            y += 1
 
     for x in range(len(intervals)):
         try:
-            start_val: int = intervals[x][0]
-            end_val: int = intervals[x][1]
-            if result := can_be_merged(intervals, start_val, end_val, x):
-                intervals[x] = result["new_limits"]
-                [intervals.remove(_) for _ in result["to_remove"] if _ != result["new_limits"]]
+            can_merge(intervals, x)
         except IndexError:
             break
     return intervals
 
-
-# Changed delete route, now we're deleting every value in "to_remove", including duplicates.
-# -------------------------------------
-# Changed to check every index in range(0, len(intervals)) -> now we don't need to sort and waste time on it.
-# Also changed replacing new_limits before we delete, to avoid missing indexes to replace with.
-# -------------------------------------
-# Hit a Time_Limit, but with 170/170 test and there's no info about this test_case...
-# Without rebuilding, I can remove sorting which is taking O(log n) from start, but then we can encounter
-# descending ordering and fail. W.e just fail to see this test and ordering.
-# -------------------------------------
-# If we assume there's only intervals input with ascending order, then it's working correctly.
-# But if it's not I need to rebuild or just sort it, cuz we're not double_checking merges after removing elements.
-# -------------------------------------
-# Working for value in ascending order, but if there's -> [1, 5] [100, 100], [5, 100]
-#  descending after we already merged [1, 5] + [5, 100] -> [100, 100] will be just ignored.
-#  either loop more than one time, which isn't good, or I need to make something.
-# -------------------------------------
-# ! try, except <- not pretty solution, but I was trying to solve it without creating new array. !
-# -------------------------------------
-# [1, 3] [2, 6]
-# start1 = 1, end1 = 3, start2 = 2, end2 = 6
-# start2 should be in range of (start1 - end1) -> start2 >= start1 and start2 <= end1
-# end2 should be more than end1 -> end2 >= end1
-# if reversed [2, 6] [1, 3]
-# start1 = 2, end1 = 3, start2 = 1, end2 = 3
-# start2 <= start1 and start2 <= end1
-# end2 >= start1 and end2 <= end1
+# 167/170 <- again Time_limit, rebuild, but we're still scrolling over whole input.
+#            Guess there's no way to make this work without sorting, cuz only with some ordering,
+#            we can walk from left_to_right and collect everything until we hit some higher value,
+#            and change our start, end on this value.
 
 
 test1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
@@ -98,15 +84,12 @@ for _ in merge(test4):
     assert _ in test4_out
 print(merge(test4))
 
-# test5 - failed -> exactly as I was expecting DESCENDING order..
 test5 = [[1, 4], [0, 5]]
 test5_out = [[0, 5]]
 for _ in merge(test5):
     assert _ in test5_out
 print(merge(test5))
 
-# test6 - failed -> Im removing elements, and didn't consider they can have DUPLICATES [1, 2] and [1, 2]
-#                   one of them removed and another is still here.
 test6 = [[0, 0], [1, 2], [5, 5], [2, 4], [3, 3], [5, 6], [5, 6], [4, 6], [0, 0], [1, 2], [0, 2], [4, 5]]
 test6_out = [[0, 6]]
 print(merge(test6))
