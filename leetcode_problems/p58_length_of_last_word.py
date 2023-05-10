ff9e0dc88033ef5f538fa9ac544297636a8c62f7
Pampam <piankovpe@gmail.com>
2023-05-10 15:55:53 +0600

    add working_sol (6.2%, 17.44%) -> (51ms, 16.2mb)

--- leetcode_problems/p58_length_of_last_word.py
+++ leetcode_problems/p58_length_of_last_word.py
@@ -4,13 +4,20 @@
 # 1 <= s.length <= 104 and There will be at least one word in S
 
 def length_of_last_word(s: str) -> int:
-    count: int = 0
+    # working_sol (6.2%, 17.44%) -> (51ms, 16.2mb)  time: O(n)  | space: O(1)
+    length: int = 0
     for x in range(len(s) - 1, -1, -1):
-        if s[x] == " " and count > 0:
+        if s[x] == " " and length > 0:
             break
         if s[x] != " ":
-            count += 1
-    return count
+            length += 1
+    return length
+
+# Time complexity: O(n) -> By the task description there's always will be word and spaces (at least 1 of each)
+#                          Worst case is we're going to loop through whole input -> case like this "word   "
+#                  Θ(log n) -> best of all cases -> "word   word" (only part of input will be searched)
+#                  Ω(1) -> best case like this -> "word" (always at least one word, no info about spaces)
+# Space complexity: O(1) -> one constant for counting length.
 
 # Most simple way to solve it is STRIP(), but I guess it's not a correct way to use build_in methods.
 # So let's try to loop and count every symbol from the end.
@@ -18,12 +25,15 @@ def length_of_last_word(s: str) -> int:
 
 test1 = "Hello World"
 test1_out = 5
+assert test1_out == length_of_last_word(test1)
 print(length_of_last_word(test1))
 
 test2 = "   fly me   to   the moon  "
 test2_out = 4
+assert test2_out == length_of_last_word(test2)
 print(length_of_last_word(test2))
 
 test3 = "luffy is still joyboy"
 test3_out = 6
+assert test3_out == length_of_last_word(test3)
 print(length_of_last_word(test3))
