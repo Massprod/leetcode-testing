ab8f544c9afc788c37d52fcc933ca7002404df6e
Pampam <piankovpe@gmail.com>
2023-05-10 15:44:38 +0600

    pre_commit

--- leetcode_problems/p58_length_of_last_word.py
+++ leetcode_problems/p58_length_of_last_word.py
@@ -4,14 +4,26 @@
 # 1 <= s.length <= 104 and There will be at least one word in S
 
 def length_of_last_word(s: str) -> int:
-    pass
+    count: int = 0
+    for x in range(len(s) - 1, -1, -1):
+        if s[x] == " " and count > 0:
+            break
+        if s[x] != " ":
+            count += 1
+    return count
+
+# Most simple way to solve it is STRIP(), but I guess it's not a correct way to use build_in methods.
+# So let's try to loop and count every symbol from the end.
 
 
 test1 = "Hello World"
 test1_out = 5
+print(length_of_last_word(test1))
 
 test2 = "   fly me   to   the moon  "
 test2_out = 4
+print(length_of_last_word(test2))
 
 test3 = "luffy is still joyboy"
 test3_out = 6
+print(length_of_last_word(test3))
