b1c714f25bc0ab4a056e64d4e20d71be48c316be
Pampam <piankovpe@gmail.com>
2023-05-10 15:33:52 +0600

    initial

---
+++ leetcode_problems/p58_length_of_last_word.py
@@ -0,0 +1,17 @@
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+#
+# A word is a maximal substring consisting of non-space characters only.
+# 1 <= s.length <= 104 and There will be at least one word in S
+
+def length_of_last_word(s: str) -> int:
+    pass
+
+
+test1 = "Hello World"
+test1_out = 5
+
+test2 = "   fly me   to   the moon  "
+test2_out = 4
+
+test3 = "luffy is still joyboy"
+test3_out = 6
