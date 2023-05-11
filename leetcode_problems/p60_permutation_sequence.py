310d222b25c26607a4aeb1113155fffbfd786331
Pampam <piankovpe@gmail.com>
2023-05-12 00:50:08 +0600

    tests

--- leetcode_problems/p60_permutation_sequence.py
+++ leetcode_problems/p60_permutation_sequence.py
@@ -8,36 +8,42 @@
 # "321"
 # Given n and k, return the kth permutation sequence.
 # 1 <= n <= 9 | 1 <= k <= n!
+import math
+
 
 def get_permutation(n: int, k: int) -> str:
-    to_permute: list[int] = [_ for _ in range(1, n + 1)]
-    permutes: list[list[int]] = []
+    numbers = [_ for _ in range(1, n + 1)]
+    permutation = ''
+    k -= 1  # for 0 - based indexing
+    for x in range(n):
+        n -= 1
+        fact = math.factorial(n)
+        index = int(k / fact)
+        k = k % fact
+        permutation += str(numbers[index])
+        numbers.pop(index)
+    return permutation
 
-    def rec_permute(to_check: list[int], start_ind: int = 0):
-        if start_ind == len(to_check):
-            permutes.append(to_check.copy())
-            return
-        for x in range(start_ind, len(to_check)):
-            if to_check[x] == to_check[start_ind] and start_ind != x:
-                continue
-            to_check[x], to_check[start_ind] = to_check[start_ind], to_check[x]
-            rec_permute(to_check, start_ind + 1)
-            to_check[x], to_check[start_ind] = to_check[start_ind], to_check[x]
-    rec_permute(to_permute, 0)
-    permutes.sort()
-    k_permute = ""
-    for _ in permutes[k - 1]:
-        k_permute += str(_)
-    return k_permute
-
-# Used p47 solution to create unique permutations, but there was used unordered set() and now using list -> slower.
-# --------------------------------
-# This descriptions...
-# ! The set [1, 2, 3, ..., n] contains a total of n! unique permutations !
-# Where's this set??
-# Should I create it?? For what??
-# Can I use list with 1 - n values??
-# I'm only given n -> which is last number of sequence from 1 to n.
+# Ok. Brute solution is fine, but there's no way to make it faster without rebuild.
+# Google_time:
+# The first position of an n length sequence is occupied by each of the numbers from 1 to n
+# exactly n! / n that is (n-1)! number of times and in ascending order.
+# So the first position of the kth sequence will be occupied by the number present at
+#   index = k / (n-1)!(according to 1-based indexing).
+#   index = (k - 1) / (n - 1)! (according to 0-based indexing) <- using this one.
+# The currently found number can not occur again, so it is removed from the original n numbers
+# and now the problem reduces to finding the ( k % (n-1)! )th permutation sequence of the remaining n-1 numbers.
+# This process can be repeated until we have only one number left
+# which will be placed in the first position of the last 1-length sequence.
+# The factorial values involved here can be very large as compared to k.
+# So, the trick used to avoid the full computation of such large factorials
+# is that as soon as the product n * (n-1) * … becomes greater than k,
+# we no longer need to find the actual factorial value.
+# ----------------------------------------
+# There's just no way to solve it for me without knowing this ^^
+# Especially:
+#   ! The first position of an n length sequence is occupied by each of the numbers from 1 to n
+#     exactly n! / n that is (n-1)! number of times and in ascending order. !
 
 
 test1_n = 3
@@ -64,27 +70,20 @@ test4_out = "123456789"
 assert test4_out == get_permutation(test4_n, test4_k)
 print(get_permutation(test4_n, test4_k))
 
-# test5 - failed -> most certain, because I was not sorting list, and wanted to just stop search at K permutation.
-#               ! By listing and labeling all the permutations in order, we get the following sequence !
-#               ^^ it can be solved like this, but then I need to find every possible permutation, and 100% sure
-#                  time limit is here.
 test5_n = 3
 test5_k = 5
 test5_out = "312"
 assert test5_out == get_permutation(test5_n, test5_k)
 print(get_permutation(test5_n, test5_k))
 
-# test6 - failed -> Yep. Time_limit, cuz there's no way it's just repeating of p47, and I need to find way to stop
-#                   recursion at some moment. Already tried to simple count of adding unique_permutes and stop at k.
 test6_n = 9
 test6_k = 206490
 test6_out = "619754832"
+assert test6_out == get_permutation(test6_n, test6_k)
 print(get_permutation(test6_n, test6_k))
 
-# test7 - failed -> Time_limit, if I stop permutations at some point my solution isn't going to work,
-#                   cuz there's either other solution with correct order of permutations or IDK.
-#                   Because it's obviously going to work with full list of all_unique_permutations in ascending order.
 test7_n = 9
 test7_k = 219601
 test7_out = "647123589"
+assert test7_out == get_permutation(test7_n, test7_k)
 print(get_permutation(test7_n, test7_k))
