74f3ad86c25b03a2a760bb8591ed834e71d347dc
Pampam <piankovpe@gmail.com>
2023-05-11 20:50:06 +0600

    initial

---
+++ leetcode_problems/p59_spiral_matrix_ii.py
@@ -0,0 +1,12 @@
+# Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+def generate_matrix(n: int) -> list[list[int]]:
+    pass
+
+
+test1 = 3
+test1_out = [[1,2,3],[8,9,4],[7,6,5]]
+
+
+test2 = 1
+test2_out = [[1]]
