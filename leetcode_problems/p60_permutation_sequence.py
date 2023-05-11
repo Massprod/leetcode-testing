8b69e00e23daba613e23e17c589ee8fdca1f0d9c
Pampam <piankovpe@gmail.com>
2023-05-11 21:21:51 +0600

    initial

---
+++ leetcode_problems/p60_permutation_sequence.py
@@ -0,0 +1,27 @@
+# The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+# By listing and labeling all the permutations in order, we get the following sequence for n = 3:
+# "123"
+# "132"
+# "213"
+# "231"
+# "312"
+# "321"
+# Given n and k, return the kth permutation sequence.
+# 1 <= n <= 9 | 1 <= k <= n!
+
+def get_permutation(n: int, k: int) -> str:
+    pass
+
+
+test1_n = 3
+test1_k = 3
+test1_out = "213"
+
+test2_n = 4
+test2_k = 9
+test2_out = "2314"
+
+test3_n = 3
+test3_k = 1
+test3_out = "123"
+
