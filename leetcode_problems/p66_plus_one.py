82221370ff2d1f90e2f3bc9a903498b262689f6e
Pampam <piankovpe@gmail.com>
2023-05-13 21:03:11 +0600

    initial

---
+++ leetcode_problems/p66_plus_one.py
@@ -0,0 +1,22 @@
+# You are given a large integer represented as an integer array digits,
+# where each digit[i] is the ith digit of the integer.
+# The digits are ordered from most significant to the least significant in left-to-right order.
+# The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+# 1 <= digits.length <= 100  ,  0 <= digits[i] <= 9  ,  digits does not contain any leading 0's.
+
+
+def plus_one(digits: list[int]) -> list[int]:
+    pass
+
+
+test1 = [1, 2, 3]
+test1_out = [1, 2, 4]
+
+test2 = [4, 3, 2, 1]
+test2_out = [4, 3, 2, 2]
+
+test3 = [9]
+test3_out = [1, 0]
