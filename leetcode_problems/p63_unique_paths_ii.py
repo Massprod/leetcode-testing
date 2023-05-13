edb2814b35b89baf999501b4ddd66ea9836cb5ce
Pampam <piankovpe@gmail.com>
2023-05-13 20:07:16 +0600

    tests

--- leetcode_problems/p63_unique_paths_ii.py
+++ leetcode_problems/p63_unique_paths_ii.py
@@ -12,7 +12,7 @@
 # 1 <= m, n <= 100  ,  obstacleGrid[i][j] is 0 or 1.
 
 def unique_paths_obstacles(obstacleGrid: list[list[int]]) -> int:
-    if obstacleGrid[0][0] == 1:
+    if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1]:
         return 0
     length: int = len(obstacleGrid[0])
     height: int = len(obstacleGrid)
@@ -35,17 +35,41 @@ def unique_paths_obstacles(obstacleGrid: list[list[int]]) -> int:
     return obstacleGrid[-1][-1]
 
 
+# Same approach but, if we along the way encounter obstacle we're making this cell unapproachable by giving value of 0.
+# Which is 0 path to enter this cell, and we can summ num_of_paths like before.
+# ------------------------------
 # Mirror of p62, but we need to skip some index_cells with already placed value == 1.
 
 
 test1 = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
 test1_out = 2
-print(unique_paths_obstacles(test1))
+test = unique_paths_obstacles(test1)
+print(test)
+assert test == test1_out
+del test
 
 test2 = [[0, 1], [0, 0]]
 test2_out = 1
-print(unique_paths_obstacles(test2))
+test = unique_paths_obstacles(test2)
+print(test)
+assert test == test2_out
+del test
 
 test3 = [[0, 1, 0], [1, 0, 0], [0, 0, 0]]
 test3_out = 0
-print(unique_paths_obstacles(test3))
+test = unique_paths_obstacles(test3)
+print(test)
+assert test == test3_out
+del test
+
+# test4 - failed -> considered that [0][0] can be obstacle, but forgot about [-1][-1] can be obstacle as well...
+test4 = [
+    [0], [1], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [1],
+    [0], [0], [0], [0], [1], [0], [0], [0], [0], [0], [0], [0], [0],
+    [0], [0], [1], [1], [0], [1], [0], [0], [1], [0], [0], [0], [0], [1]
+]
+test4_out = 0
+test = unique_paths_obstacles(test4)
+print(test)
+assert test == test4_out
+del test
