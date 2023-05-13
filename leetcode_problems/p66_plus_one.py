53ff691f73dec0330b1e5371f5ba9d3d2fab0317
Pampam <piankovpe@gmail.com>
2023-05-13 21:12:49 +0600

    pre_commit

--- leetcode_problems/p66_plus_one.py
+++ leetcode_problems/p66_plus_one.py
@@ -9,14 +9,37 @@
 
 
 def plus_one(digits: list[int]) -> list[int]:
-    pass
+    num: str = ""
+    for _ in digits:
+        num += str(_)
+    integer: int = int(num)
+    integer += 1
+    num = str(integer)
+    incremented: list[int] = []
+    for _ in num:
+        incremented.append(int(_))
+    return incremented
+
+# Most basic way is: make str from list -> make int from string -> increment by 1 -> make str from int -> list string
+# But it's a lot of transformations and if there's time_limit, than I will rebuild but try this one.
 
 
 test1 = [1, 2, 3]
 test1_out = [1, 2, 4]
+print(plus_one(test1))
+for _ in range(len(test1_out)):
+    assert test1_out[_] == plus_one(test1)[_]
 
 test2 = [4, 3, 2, 1]
 test2_out = [4, 3, 2, 2]
+print(plus_one(test2))
+for _ in range(len(test2_out)):
+    assert test2_out[_] == plus_one(test2)[_]
 
 test3 = [9]
 test3_out = [1, 0]
+print(plus_one(test3))
+for _ in range(len(test3_out)):
+    assert test3_out[_] == plus_one(test3)[_]
+
+print(plus_one([-1]))
