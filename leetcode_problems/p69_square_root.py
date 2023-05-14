2d21e4698b37088ace0d1a00aace9f1b1b1e3341
Pampam <piankovpe@gmail.com>
2023-05-14 19:58:15 +0600

    initial

---
+++ leetcode_problems/p69_square_root.py
@@ -0,0 +1,15 @@
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+# The returned integer should be non-negative as well.
+# 0 <= x <= 231 - 1
+# You must not use any built-in exponent function or operator.
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+def my_sqrt(x: int) -> int:
+    pass
+
+
+test1 = 4
+test1_out = 2
+
+test2 = 8
+test2_out = 2
