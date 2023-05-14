7b67a78c974c0901c57b099c40871f055d4675e4
Pampam <piankovpe@gmail.com>
2023-05-14 18:38:49 +0600

    initial

---
+++ leetcode_problems/p67_add_binary.py
@@ -0,0 +1,17 @@
+# Given two binary strings a and b, return their sum as a binary string.
+
+# 1 <= a.length, b.length <= 104
+# a and b consist only of '0' or '1' characters.
+# Each string does not contain leading zeros except for the zero itself.
+
+def add_binary(a: str, b: str) -> str:
+    pass
+
+
+test1_a = "11"
+test1_b = "1"
+test1_out = "100"
+
+test2_a = "1010"
+test2_b = "1011"
+test2_out = "10101"
