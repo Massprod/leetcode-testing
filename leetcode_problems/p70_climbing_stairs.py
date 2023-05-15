888fa73c08c915925b51a68999eb7ceb066a74bb
Pampam <piankovpe@gmail.com>
2023-05-15 19:53:54 +0600

    pre_commit

--- leetcode_problems/p70_climbing_stairs.py
+++ leetcode_problems/p70_climbing_stairs.py
@@ -3,11 +3,23 @@
 # 1 <= n <= 45
 
 def climb_stairs(n: int) -> int:
-    pass
+    def climbing(step: int = 0) -> int | None:
+        if step == n:
+            return 1
+        if step > n:
+            return 0
+        return climbing(step + 1) + climbing(step + 2)
+    return climbing()
 
 
 test1 = 2
 test1_out = 2
+print(climb_stairs(test1))
 
 test2 = 3
 test2_out = 3
+print(climb_stairs(test2))
+
+test3 = 10
+test3_out = 89
+print(climb_stairs(test3))
