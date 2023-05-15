7b48e38d4a8de98b9fdcee6aa15a793c09194ce1
Pampam <piankovpe@gmail.com>
2023-05-15 19:48:15 +0600

    initial

---
+++ leetcode_problems/p70_climbing_stairs.py
@@ -0,0 +1,13 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# 1 <= n <= 45
+
+def climb_stairs(n: int) -> int:
+    pass
+
+
+test1 = 2
+test1_out = 2
+
+test2 = 3
+test2_out = 3
