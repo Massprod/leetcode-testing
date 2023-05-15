2af3bb9eada55b7a4451e37f3dd26fe7c164ef2e
Pampam <piankovpe@gmail.com>
2023-05-15 21:17:49 +0600

    initial

---
+++ leetcode_problems/p73_set_matrix_zeroes.py
@@ -0,0 +1,19 @@
+# Given an m x n integer matrix, if an element is 0, set its entire row and column to 0's.
+# You must do it in place.
+# m == matrix.length  ,  n == matrix[0].length
+# 1 <= m, n <= 200  ,  -231 <= matrix[i][j] <= 231 - 1
+
+# Follow up:
+#   A straightforward solution using O(mn) space is probably a bad idea.
+#   A simple improvement uses O(m + n) space, but still not the best solution.
+#   Could you devise a constant space solution?
+
+def set_zeroes(matrix: list[list[int]]) -> None:
+    pass
+
+
+test1 = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+test1_out = [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
+
+test2 = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+test2_out = [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
