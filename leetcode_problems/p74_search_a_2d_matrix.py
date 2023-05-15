1575b57c878550979b1bcbed4007125c0701043e
Pampam <piankovpe@gmail.com>
2023-05-16 00:28:00 +0600

    add working_sol (33.54%, 15.5%) -> (53ms, 16.9mb)

--- leetcode_problems/p74_search_a_2d_matrix.py
+++ leetcode_problems/p74_search_a_2d_matrix.py
@@ -9,26 +9,45 @@
 
 
 def search_2d_matrix(matrix: list[list[int]], target: int) -> bool:
-    if matrix[0][0] > target:
+    # working_sol (33.54%, 15.5%) -> (53ms, 16.9mb)  time: O(m + n) | space: O(1)
+    if target > matrix[-1][-1]:
         return False
-    if matrix[-1][-1] < target:
+    if target < matrix[0][0]:
         return False
     row_length: int = len(matrix[0])
     for y in range(len(matrix)):
         if row_length == 1:
             if matrix[y][0] == target:
                 return True
-        if matrix[y][0] <= target <= matrix[y][1]:
-            return True
-    return False
+        if matrix[y][0] <= target <= matrix[y][-1]:
+            for x in range(row_length):
+                if matrix[y][x] == target:
+                    return True
+            return False
 
-# All we need to check is first_column and last_column.
-# But it's too easy and task is medium, tricky moments I know:
+# Time complexity: O(m + n) -> worst case, looping whole height of input_matrix => O(m) ->
+#                                   -> looping once through row of input_matrix length => O(n) ->
+#                                   -> O(m + n)
+#                  ! Not sure about this ^^, cuz it's nested loop, but it's triggering once and returning after it,
+#                    totally need to learn how to calculate it with formulas. !
+#                  -----------
+#                  Ω(m) or Ω(n) -> best case, 1row or 1 column and looping once through.
+#                  -----------
+#                  Θ(log(m + n)) -> median case, looping only part of height => Θ(log(m) ->
+#                                   -> and part of a row with size of input_matrix length == n => Θ(log(n)) ->
+#                                   -> Θ(log(m + n)
+# Space complexity: O(1) -> no extras and 1 constant.
+
+# Brain-lag, I was thinking that we need to return value in range(row(first_index), row(last_index)).
+# But I need to find particular number, not just being in range.
+# --------------------------------
+# Tricky moments I know:
 #   target > matrix[-1][-1] => target > max_value
 #   target < matrix[0][0] => target < min_value
+#   matrix is one row, or column
 # ! The first integer of each row is greater than the last integer of the previous row. !
-# But there's no info about, whole matrix, all values in ascending order or just in a particular row?
-# Because if values only in ascending order in a row, and not whole matrix, than we can't skip any rows.
+# Totally need to take a breaks, before rushing solution, cuz there's definition ^^ of ascending matrix.
+# every [y][0] < [y][-1] and [y][-1] < [y + 1][0] <- it's in a description...
 
 
 test1 = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
@@ -46,3 +65,8 @@ test3 = [[1]]
 test3_target = 1
 test3_out = True
 print(search_2d_matrix(test3, test3_target))
+
+test4 = [[1, 2, 3]]
+test4_target = 3
+test4_out = True
+print(search_2d_matrix(test4, test4_target))
