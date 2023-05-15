d867fca44c5f79c69438b593ef8feb7686edfe63
Pampam <piankovpe@gmail.com>
2023-05-15 21:49:27 +0600

    pre_commit

--- leetcode_problems/p73_set_matrix_zeroes.py
+++ leetcode_problems/p73_set_matrix_zeroes.py
@@ -8,12 +8,39 @@
 #   A simple improvement uses O(m + n) space, but still not the best solution.
 #   Could you devise a constant space solution?
 
+
 def set_zeroes(matrix: list[list[int]]) -> None:
-    pass
+    def zeroing(y: int, x: int) -> None:
+        for g in range(len(matrix[0])):
+            if matrix[y][g] == 0:
+                continue
+            matrix[y][g] = 0
+            used.add((y, g))
+        for h in range(len(matrix)):
+            if matrix[h][x] == 0:
+                continue
+            matrix[h][x] = 0
+            used.add((h, x))
+    used: set = set()
+    for _y in range(len(matrix)):
+        for _x in range(len(matrix[0])):
+            if (_y, _x) in used:
+                continue
+            if matrix[_y][_x] == 0:
+                used.add((_y, _x))
+                zeroing(_y, _x)
 
 
 test1 = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 test1_out = [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
+set_zeroes(test1)
+print(test1)
+for _ in test1_out:
+    assert _ in test1
 
 test2 = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
 test2_out = [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
+set_zeroes(test2)
+print(test2)
+for _ in test2_out:
+    assert _ in test2
