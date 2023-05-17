164032e6cf5c119fd70f6c4fca81aaa93b85df5a
Pampam <piankovpe@gmail.com>
2023-05-17 18:04:59 +0600

    pre_commit

--- leetcode_problems/p75_sort_colors.py
+++ leetcode_problems/p75_sort_colors.py
@@ -10,12 +10,38 @@
 # ! Follow up: Could you come up with a one-pass algorithm using only constant extra space? !
 
 def sort_colors(nums: list[int]) -> None:
-    pass
+    left: int = 0
+    right: int = len(nums) - 1
+    while left <= right:
+        for x in range(len(nums)):
+            if nums[x] == 2 and x <= right:
+                nums[x], nums[right] = nums[right], nums[x]
+                right -= 1
+            if nums[x] == 0 and x >= left:
+                nums[x], nums[left] = nums[left], nums[x]
+                left += 1
+                while nums[x] == 2:
+                    nums[x], nums[right] = nums[right], nums[x]
+                    right -= 1
+                    if nums[x] == 0:
+                        left += 1
+        break
+
+
+# It's not actually one-pass solution. Because we're looping once but there's neste loop inside to change blue color.
+# But maybe it's one-pass cuz there's no definition of that, and we're looping once through whole input.
 
 
 test1 = [2, 0, 2, 1, 1, 0]
 test1_out = [0, 0, 1, 1, 2, 2]
-print(sort_colors(test1))
+sort_colors(test1)
+print(test1)
 
 test2 = [2, 0, 1]
 test2_out = [0, 1, 2]
+sort_colors(test2)
+print(test2)
+
+test3 = [2, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2, 0, 2]
+sort_colors(test3)
+print(test3)
