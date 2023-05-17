b2b7c8eae4eb6dbe085bb7578120ac94e120d403
Pampam <piankovpe@gmail.com>
2023-05-17 19:08:16 +0600

    initial

---
+++ leetcode_problems/p77_combinations.py
@@ -0,0 +1,15 @@
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+# 1 <= n <= 20  ,  1 <= k <= n
+# You may return the answer in any order.
+
+def combine(n: int, k: int) -> list[list[int]]:
+    pass
+
+
+test1_n = 4
+test1_k = 2
+test1_out = [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+
+test2_n = 1
+test2_k = 1
+test2_out = [[1]]
