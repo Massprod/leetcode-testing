b5999493480e613284b7183e4a709b5dad16168c
Pampam <piankovpe@gmail.com>
2023-05-19 01:28:09 +0600

    tests

--- leetcode_problems/p84_largest_rectangle_in_histogram.py
+++ leetcode_problems/p84_largest_rectangle_in_histogram.py
@@ -4,40 +4,32 @@
 # 1 <= heights.length <= 105  ,  0 <= heights[i] <= 104
 
 def largest_rectangle(heights: list[int]) -> int:
-    bars_on_route: dict[int] = {}
-    min_height: int = heights[0]
-    length: int = 0
     max_area: int = 0
-    break_point: int = 0
     for index in range(len(heights)):
-        if heights[index] != 0:
-            bars_on_route[index] = 1
-        else:
-            bars_on_route[index] = 0
-        if heights[index] != 0:
-            length += 1
-        if heights[index] < min_height and heights[index] != 0:
-            min_height = heights[index]
         if heights[index] == 0:
-            min_plato = min_height * length
-            max_area = max(max_area, min_plato)
-            if index != (len(heights) - 1):
-                min_height = heights[index + 1]
-            length = 0
-        if heights[index] >= heights[index - 1] and index > 0 and heights[index] != 0:
-            for key in bars_on_route.keys():
-                if break_point <= key < index:
-                    if heights[key] != 0:
-                        bars_on_route[key] += 1
-        if (heights[index] < heights[index - 1] and index > 0) or (index == (len(heights) - 1)):
-            for key in bars_on_route.keys():
-                if key >= break_point:
-                    bar_area: int = heights[key] * bars_on_route[key]
-                    max_area = max(max_area, bar_area)
-            break_point = index
-    min_plato: int = min_height * length
-    return max(max_area, min_plato)
+            continue
+        length: int = 1
+        height: int = heights[index]
+        for x in range(index - 1, -1, -1):
+            if heights[x] < height:
+                break
+            length += 1
+        for y in range(index + 1, len(heights)):
+            if heights[y] < height:
+                break
+            length += 1
+        area: int = length * height
+        max_area = max(max_area, area)
+    return max_area
 
+
+# Ok. I'm done with trying to make this work, and will simply rebuild it in first_dump I thought about.
+# Just count every bar one by one until I hit any value lower than bar, so we can't put this bar inside.
+# Very slow, but still not burned enough to google.
+# ------------------------
+# No idea how to make prev_solution work with 2 ways, cuz I made it working with ascending or equal values,
+# with no intervals with only descending like: test8.
+# ------------------------
 # min_plato => value of bottom rectangle with min_height and whole length of histogram,
 #              breaking length only when encounter 0, and starting a new one.
 # ------------------------
