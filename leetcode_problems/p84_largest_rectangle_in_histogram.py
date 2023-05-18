e2674f8f28cf7d6b9f1872f32253122e78cb5785
Pampam <piankovpe@gmail.com>
2023-05-18 21:50:53 +0600

    initial

---
+++ leetcode_problems/p84_largest_rectangle_in_histogram.py
@@ -0,0 +1,14 @@
+# Given an array of integers heights representing the histogram's bar height
+# where the width of each bar is 1,
+# return the area of the largest rectangle in the histogram.
+# 1 <= heights.length <= 105  ,  0 <= heights[i] <= 104
+
+def largest_rectangle(heights: list[int]) -> int:
+    pass
+
+
+test1 = [2, 1, 5, 6, 2, 3]
+test1_out = 10
+
+test2 = [2, 4]
+test2_out = 4
