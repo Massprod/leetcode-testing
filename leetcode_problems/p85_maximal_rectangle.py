dfb28999281ea9960b1d5955dc307cf6122db349
Pampam <piankovpe@gmail.com>
2023-05-19 18:32:13 +0600

    add working_sol (23.12%, 13.14%) -> (347ms, 17.7mb)

--- leetcode_problems/p85_maximal_rectangle.py
+++ leetcode_problems/p85_maximal_rectangle.py
@@ -4,7 +4,7 @@
 # 1 <= row, cols <= 200  ,  matrix[i][j] is '0' or '1'.
 
 def maximal_rectangle(matrix: list[list[str | int]]) -> int:
-    first_row: list[str | int] = matrix[0]
+    # working_sol (23.12%, 13.14%) -> (347ms, 17.7mb)  time: O(n * m) | space: O(m)
     max_area: int = 0
 
     def stack_calc(hist: list[str]):
@@ -20,17 +20,23 @@ def maximal_rectangle(matrix: list[list[str | int]]) -> int:
             stack.append(index)
         hist.pop()
         return hist_max_area
-
-    max_area = max(max_area, stack_calc(first_row))
-    if len(matrix) == 1:
-        return max_area
-    for y in range(1, len(matrix)):
-        for x in range(len(matrix[y])):
-            if int(matrix[y][x]) == 1:
-                matrix[y][x] = int(matrix[y][x]) + int(matrix[y-1][x])
+    for y in range(len(matrix)):
+        if y >= 1:
+            for x in range(len(matrix[y])):
+                if int(matrix[y][x]) == 1:
+                    matrix[y][x] = int(matrix[y][x]) + int(matrix[y-1][x])
         max_area = max(max_area, stack_calc(matrix[y]))
     return max_area
 
+# Time complexity: O(n * m) -> worst_case, traversing once through all indexes in a row => O(m)
+# m - lengths of a row ^^      -> calling stack_calc() and traversing this row twice with a stack => O(2m) ->
+# n - lengths of a matrix ^^   -> repeating for every row in a matrix => O(n * m)
+#
+# Space complexity: O(m) -> creating stack of matrix_row(hist) size => O(m) -> and max_area constant => O(1)
+
+# Using this monstrosity with converting str -> int, cuz I don't want to make extra loop to change all cells to int.
+# It's going to be slower than this ^^. But it's very bad looking and error_prone.
+# Will change and see diff in speed after first correct commit.
 # -------------------
 # After failing with p84 and googling for a method, get a glimpse of STACK problems, and this is one of them.
 # We can just assume that this matrix rows as histograms,
@@ -44,12 +50,24 @@ test1 = [
     ["1", "0", "0", "1", "0"],
 ]
 test1_out = 6
-print(maximal_rectangle(test1))
+test = maximal_rectangle(test1)
+assert test1_out == test
 for _ in test1:
     print(_)
+del test
 
 test2 = [["0"]]
 test2_out = 0
+test = maximal_rectangle(test2)
+assert test2_out == test
+for _ in test2:
+    print(_)
+del test
 
 test3 = [["1"]]
 test3_out = 1
+test = maximal_rectangle(test3)
+assert test3_out == test
+for _ in test3:
+    print(_)
+del test
