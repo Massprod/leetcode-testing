7595946c6676556b744e9e07d90916b68af58154
Pampam <piankovpe@gmail.com>
2023-05-19 19:07:39 +0600

    pre_commit

--- leetcode_problems/p86_partition_list.py
+++ leetcode_problems/p86_partition_list.py
@@ -16,14 +16,54 @@ class ListNode:
         return f"{self.val} -> {self.next}"
 
 
+def create_linked(to_link: list[int]) -> ListNode:
+    tempo = link = ListNode()
+    for index in range(len(to_link)):
+        tempo.val = to_link[index]
+        if index != (len(to_link) - 1):
+            tempo.next = ListNode()
+            tempo = tempo.next
+    return link
+
+
 def partition(head: ListNode, x: int) -> ListNode:
-    pass
+    lower: list[int] = []
+    higher: list[int] = []
+    tempo: ListNode = head
+    while tempo:
+        if tempo.val >= x:
+            higher.append(tempo.val)
+        if tempo.val < x:
+            lower.append(tempo.val)
+        tempo = tempo.next
+    whole: list[int] = lower + higher
+    print(whole)
+    tempo = new_link = ListNode()
+    for index in range(len(whole)):
+        tempo.val = whole[index]
+        if index != (len(whole) - 1):
+            tempo.next = ListNode()
+            tempo = tempo.next
+    return new_link
+
+
+# ---------------------------
+# Most basic way and slowest (guess), is just recreate linked list from scratch.
+# Loop once to take all values inside nodes ->
+# -> create 2 lists of values: first => all values lower than x
+#                              second => all values higher or equal than x
+# first + second -> correct list of values -> creating a new linked_list with values from this list.
+# There's no word about doing this in_place, so I will try this one first.
 
 
 test1 = [1, 4, 3, 2, 5, 2]
 test1_x = 3
 test1_out = [1, 2, 2, 4, 3, 5]
+linked = create_linked(test1)
+print(partition(linked, test1_x))
 
 test2 = [2, 1]
 test2_x = 2
 test2_out = [1, 2]
+linked = create_linked(test2)
+print(partition(linked, test2_x))
