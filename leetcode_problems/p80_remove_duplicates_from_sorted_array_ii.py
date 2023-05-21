492e511dd39a04e36fc05373cfa87baa71ffb78f
Pampam <piankovpe@gmail.com>
2023-05-22 01:26:01 +0600

    initial

---
+++ leetcode_problems/p80_remove_duplicates_from_sorted_array_ii.py
@@ -0,0 +1,34 @@
+# Given an integer array nums sorted in non-decreasing order,
+# remove some duplicates in-place such that each unique element appears at most twice.
+# The relative order of the elements should be kept the same.
+#
+# Since it is impossible to change the length of the array in some languages,
+# you must instead have the result be placed in the first part of the array nums.
+# More formally, if there are k elements after removing the duplicates,
+# then the first k elements of nums should hold the final result.
+# It does not matter what you leave beyond the first k elements.
+#
+# Return k after placing the final result in the first k slots of nums.
+#
+# Do not allocate extra space for another array.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+# -----------------------
+# 1 <= nums.length <= 3 * 104  ,  -104 <= nums[i] <= 104
+# nums is sorted in non-decreasing order.
+
+
+def remove_duplicates(nums: list[int]) -> int:
+    pass
+
+
+test1 = [1, 1, 1, 2, 2, 3]
+test1_k = 5
+test1_out = [1, 1, 2, 2, 3, "_"]
+for _ in range(test1_k):
+    assert test1_out[_] == test1[_]
+
+test2 = [0, 0, 1, 1, 1, 1, 2, 3, 3]
+test2_k = 7
+test2_out = [0, 0, 1, 1, 2, 3, 3, "_", "_"]
+for _ in range(test2_k):
+    assert test2_out[_] == test2[_]
