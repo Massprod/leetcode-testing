cbc3d5a53df93ad55dc2ad40d7683f489a29f411
Pampam <piankovpe@gmail.com>
2023-05-24 02:16:44 +0600

    pre_commit

--- leetcode_problems/p93_restore_ip_adresses.py
+++ leetcode_problems/p93_restore_ip_adresses.py
@@ -11,14 +11,61 @@
 
 
 def restore_ip(s: str) -> list[str]:
-    pass
+    validated: list[str] = []
+    if not 4 <= len(s) <= 12:
+        return validated
+
+    def recurs_slice(sliced: str, tempo_ip: list[str]) -> None:
+        if len(sliced) == 0 and len(tempo_ip) == 4:
+            validated.append("".join(tempo_ip))
+            return
+        if len(tempo_ip) == 4 and len(sliced) != 0:
+            return
+        if len(tempo_ip) != 4 and len(sliced) == 0:
+            return
+        if 0 <= int(sliced[0]) <= 9:
+            if len(tempo_ip) == 3:
+                tempo_ip.append(sliced[0])
+            if len(tempo_ip) < 3:
+                tempo_ip.append(f"{sliced[0]}.")
+            recurs_slice(sliced[1:], tempo_ip)
+            tempo_ip.pop()
+        if len(sliced) > 1 and int(sliced[0]) != 0 and 10 <= int(sliced[0] + sliced[1]) <= 99:
+            if len(tempo_ip) == 3:
+                tempo_ip.append(sliced[0] + sliced[1])
+            if len(tempo_ip) < 3:
+                tempo_ip.append(f"{sliced[0]}{sliced[1]}.")
+            recurs_slice(sliced[2:], tempo_ip)
+            tempo_ip.pop()
+        if len(sliced) > 2 and int(sliced[0]) != 0:
+            if 100 <= int(sliced[0] + sliced[1] + sliced[2]) <= 255:
+                if len(tempo_ip) == 3:
+                    tempo_ip.append(sliced[0] + sliced[1] + sliced[2])
+                if len(tempo_ip) < 3:
+                    tempo_ip.append(f"{sliced[0]}{sliced[1]}{sliced[2]}.")
+                recurs_slice(sliced[3:], tempo_ip)
+                tempo_ip.pop()
+
+    recurs_slice(s, [])
+    return validated
+
+# First of all this basic: 1 <= s.length <= 20.
+# Ip address can be at low limit of length 4 and max limit of length 12, and in p91 I have been hitting time_limit.
+# When tried to use every possible slice, but in this case it's limited to 12 and should be enough ok with speed.
+# Otherwise, I need to find a way to cull some recursion calls.
 
 
 test1 = "25525511135"
 test1_out = ["255.255.11.135", "255.255.111.35"]
+print(restore_ip(test1))
+assert test1_out == restore_ip(test1)
 
 test2 = "0000"
 test2_out = ["0.0.0.0"]
+print(restore_ip(test2))
+assert test2_out == restore_ip(test2)
 
 test3 = "101023"
 test3_out = ["1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3"]
+print(restore_ip(test3))
+assert test3_out == restore_ip(test3)
