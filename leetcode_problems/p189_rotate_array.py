0cc56ec479f9d490d82b9b20fde9e9720434e811
Pampam <piankovpe@gmail.com>
2023-05-26 01:18:20 +0600

    tests

--- leetcode_problems/p189_rotate_array.py
+++ leetcode_problems/p189_rotate_array.py
@@ -7,58 +7,66 @@
 # Could you do it in-place with O(1) extra space?
 
 
-def rotate(nums: list[int], k: int) -> None:
+def rotate1(nums: list[int], k: int) -> None:
+    # working_time_limit O(k * n)
+    k = k % len(nums)
+    turn: int = 0
+    while turn < k:
+        prev: int = nums[turn]
+        for x in range(turn + 1, len(nums)):
+            nums[x], prev = prev, nums[x]
+            if (x + 1) >= len(nums):
+                nums[turn + (len(nums) - (x + 1))] = prev
+        turn += 1
+    point_1: int = 0
+    point_2: int = k - 1
+    while point_1 < point_2:
+        nums[point_1], nums[point_2] = nums[point_2], nums[point_1]
+        point_1 += 1
+        point_2 -= 1
+
+
+def rotate2(nums: list[int], k: int) -> None:
     k = k % len(nums)
     if k == 0:
         return
-    for x in range(k):
-        next_index: int = x + k
-        if next_index >= len(nums):
-            next_index = next_index - len(nums)
-        prev: int = nums[next_index]
-        nums[x], nums[next_index] = nums[next_index], nums[x]
-        step_index: int = next_index + k
-        if step_index >= len(nums):
-            step_index = step_index - len(nums)
-        while step_index <= x:
-            nums[step_index], prev = prev, nums[step_index]
-            step_index += k
-            if step_index >= len(nums):
-                step_index = step_index - len(nums)
-        nums[x] = prev
+    nums[:] = nums[-k:] + nums[:-k]
 
+
+
+#
+# Was correct about slicing, but forget about reversing parts.
+# This one is working. But I wanted to make working solution with switching indexes around k-point, and failed...
+# In the end. 2 solutions, one working but time_limited -> one_by_one switch.
+# Second simple slicing and merge.
+# --------------------
 # Done with trying to make this with switching indexes.
 # --------------------
 # Time limit, because I made one_by_one_solution.
 # Just wanted to make it work after failing to turn indexes for k insta.
 # But here's time limit for that.
-# --------------------
-# Already done similar but with linked_list in p61.
-# Three different ways:
-#   1) We're just slicing at [:x + 1] and [x + 1:] and returning => [x + 1:] + [:x + 1] => O(2n)
-#      ^^not in place solution, but there's no mentioning three in_place solutions.
-#   2) Same solution as 1 but, we're recreating new list from indexes not slices, not actually a different one.
-#   3) Changing every index 1 by 1 for k times, in_place. But it will be O(k * n),
-#      because we're only rotating by one at a time.
-#      ^^did this in p61, if I recall correctly.
-#   3+) 99% sure we can rotate every index by k in one walk, but how?
 
 
-# test1 = [1, 2, 3, 4, 5, 6, 7]
-# test1_k = 3
-# test1_out = [5, 6, 7, 1, 2, 3, 4]
-# rotate(test1, test1_k)
-# print(test1)
-# assert test1_out == test1
+test1 = [1, 2, 3, 4, 5, 6, 7]
+test1_k = 3
+test1_out = [5, 6, 7, 1, 2, 3, 4]
+rotate1(test1, test1_k)
+# rotate2(test1, test1_k)
+print(test1)
+assert test1_out == test1
 
+test2 = [-1, -100, 3, 99]
+test2_k = 2
+test2_out = [3, 99, -1, -100]
+rotate1(test2, test2_k)
+# rotate2(test2, test2_k)
+print(test2)
+assert test2_out == test2
 
-# test2 = [-1, -100, 3, 99]
-# test2_k = 2
-# test2_out = [3, 99, -1, -100]
-# rotate(test2, test2_k)
-# print(test2)
-# assert test2_out == test2
-
-test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-rotate(test3, 4)
+test3 = [1, 2, 3]
+test3_k = 2
+test3_out = [2, 3, 1]
+rotate1(test3, test3_k)
+# rotate2(test3, 2)
 print(test3)
+assert test3_out == test3
