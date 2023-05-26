cd947261beff3de8e56db6f88d40b3a7cc1d4bae
Pampam <piankovpe@gmail.com>
2023-05-27 00:47:52 +0600

    initial

--- leetcode_problems/p149_max_point_on_a_line.py
+++ leetcode_problems/p149_max_point_on_a_line.py
@@ -9,6 +9,16 @@ def max_pointa(points: list[list[int]]) -> int:
     pass
 
 
+# If we don't care about speed we can check every index one by one and count their lines,
+# but it's going to be O(n ** n) and I fear with 300 points it will hit time_limit.
+# How we can avoid it?
+# 1) Delete every index with 0? If we check point and find there's no other points on her lines.
+# Ok. But still need's to be checked and if there's none like this?
+# 2) Remember lines? We check x = 2, for every index, and if we met another point with x == 2
+# just ignore it or at least ignore horizontal calc.
+# Ok. 2 might be enough, don't see other options for now.
+
+
 test1 = [[1, 1], [2, 2], [3, 3]]
 test1_out = 3
 
