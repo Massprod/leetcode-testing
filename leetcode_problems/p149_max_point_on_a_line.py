58ee828600951077e6b112c14d545083b1247071
Pampam <piankovpe@gmail.com>
2023-05-27 00:42:50 +0600

    initial

---
+++ leetcode_problems/p149_max_point_on_a_line.py
@@ -0,0 +1,16 @@
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane,
+# return the maximum number of points that lie on the same straight line.
+# -----------------------
+# 1 <= points.length <= 300  ,  points[i].length == 2  ,  -104 <= xi, yi <= 104
+# All the points are unique.
+
+
+def max_pointa(points: list[list[int]]) -> int:
+    pass
+
+
+test1 = [[1, 1], [2, 2], [3, 3]]
+test1_out = 3
+
+test2 = [[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]
+test2_out = 4
