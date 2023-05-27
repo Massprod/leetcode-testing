d2eb2273a760469c8a369f21b06d8ef7d463d31d
Pampam <piankovpe@gmail.com>
2023-05-27 22:07:57 +0600

    initial

---
+++ leetcode_problems/p151_reverse_words_in_a_string.py
@@ -0,0 +1,23 @@
+# Given an input string s, reverse the order of the words.
+#  A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+# Return a string of the words in reverse order concatenated by a single space.
+#  Note that s may contain leading or trailing spaces or multiple spaces between two words.
+#  The returned string should only have a single space separating the words. Do not include any extra spaces.
+# -------------------
+# 1 <= s.length <= 104
+# s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+# There is at least one word in s.
+
+
+def reverse_words(s: str) -> str:
+    pass
+
+
+test1 = "the sky is blue"
+test1_out = "blue is sky the"
+
+test2 = "  hello world  "
+test2_out = "world hello"
+
+test3 = "a good   example"
+test3_out = "example good a"
