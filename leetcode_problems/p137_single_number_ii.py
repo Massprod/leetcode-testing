e86d15763a19fef38352e12b36883775e7565734
Pampam <piankovpe@gmail.com>
2023-05-29 01:27:30 +0600

    initial

---
+++ leetcode_problems/p137_single_number_ii.py
@@ -0,0 +1,19 @@
+# Given an integer array nums where every element appears three times except for one,
+# which appears exactly once.
+# Find the single element and return it.
+# -----------------------
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+# -----------------------
+# 1 <= nums.length <= 3 * 104  ,  -231 <= nums[i] <= 231 - 1
+# Each element in nums appears exactly three times except for one element which appears once.
+
+
+def single_number(nums: list[int]) -> int:
+    pass
+
+
+test1 = [2,2,3,2]
+test1_out = 3
+
+test2 = [0,1,0,1,0,1,99]
+test2_out = 99
