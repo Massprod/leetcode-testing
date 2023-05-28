bc0cf3b2c5568e6b550d3aa1044b58e1e564299c
Pampam <piankovpe@gmail.com>
2023-05-28 21:56:46 +0600

    pre_commit

--- leetcode_problems/p152_maximum_product_subarray.py
+++ leetcode_problems/p152_maximum_product_subarray.py
@@ -6,11 +6,50 @@
 
 
 def max_product(nums: list[int]) -> int:
-    pass
+    if len(nums) == 1:
+        return nums[0]
+    max_val: int = nums[0]
+    for x in range(len(nums)):
+        prod: int = nums[x]
+        max_val = max(max_val, nums[x])
+        for y in range(x + 1, len(nums)):
+            prod = prod * nums[y]
+            max_val = max(max_val, prod)
+        prod = nums[x]
+        for z in range(x - 1, -1, -1):
+            prod = prod * nums[z]
+            max_val = max(max_val, prod)
+    return max_val
+
+
+# Maybe sorting? Well it could cull everything, because we're just going to calculate
+# values from negative, starting from some index which will give us EVEN number of negative,
+# like: [-100, -50, -25, -10, 1, 50, 60, 70, 80, 90]
+# all we need after sorting is take [0] -> [3] => giving us positive product with the highest values,
+# and multiply one by one [1, 50, 60, 70, 80, 90] => most highest value.
+# If we're allowing to do this it might be working solution, because I doubt it can pass with just O(n * (n + n).
+# But let's fail first.
+# -------------------
+# !
+# A subarray is a contiguous non-empty sequence of elements within an array. !
+# Don't know how to make it faster than O(n * (n + n)) at least for now, and I didn't know what time_limit is.
+# Let's first try to make it like this and after hitting limit change,
+# because I don't see any ways to skip calculations.
+# We should always go from index and check every side of it, from 0 - index, and index - end.
+# Always compare it to current max, and just return this max. Like I see it for now.
 
 
 test1 = [2, 3, -2, 4]
 test1_out = 6
+print(max_product(test1))
+assert test1_out == max_product(test1)
 
 test2 = [-2, 0, -1]
 test2_out = 0
+print(max_product(test2))
+assert test2_out == max_product(test2)
+
+test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+test3_out = 3628800
+print(max_product(test3))
+assert test3_out == max_product(test3)
