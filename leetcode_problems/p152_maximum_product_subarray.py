960e9d99db433c20cf1b06f3f8421f868e20ef5d
Pampam <piankovpe@gmail.com>
2023-05-28 21:36:20 +0600

    initial

---
+++ leetcode_problems/p152_maximum_product_subarray.py
@@ -0,0 +1,16 @@
+# Given an integer array nums, find a subarray that has the largest product, and return the product.
+# The test cases are generated so that the answer will fit in a 32-bit integer.
+# -------------------
+# 1 <= nums.length <= 2 * 104  ,  -10 <= nums[i] <= 10
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+
+def max_product(nums: list[int]) -> int:
+    pass
+
+
+test1 = [2, 3, -2, 4]
+test1_out = 6
+
+test2 = [-2, 0, -1]
+test2_out = 0
