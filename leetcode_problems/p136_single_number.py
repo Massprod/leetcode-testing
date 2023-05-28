d136e2c771e5fc10a6d37e16a3da6551c654b36b
Pampam <piankovpe@gmail.com>
2023-05-28 23:33:34 +0600

    initial

---
+++ leetcode_problems/p136_single_number.py
@@ -0,0 +1,20 @@
+# Given a non-empty array of integers nums, every element appears twice except for one.
+# Find that single one.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+# --------------------------
+# 1 <= nums.length <= 3 * 104  ,  -3 * 104 <= nums[i] <= 3 * 104
+# Each element in the array appears twice except for one element which appears only once.
+
+
+def single_number(nums: list[int]) -> int:
+    pass
+
+
+test1 = [2, 2, 1]
+test1_out = 1
+
+test2 = [4, 1, 2, 1, 2]
+test2_out = 4
+
+test3 = [1]
+test3_out = 1
