833ce2e9e333d855448dd5fa961d7790c5578c6b
Pampam <piankovpe@gmail.com>
2023-05-29 17:52:26 +0600

    initial

---
+++ leetcode_problems/p135_candy.py
@@ -0,0 +1,19 @@
+# There are n children standing in a line.
+# Each child is assigned a rating value given in the integer array ratings.
+# You are giving candies to these children subjected to the following requirements:
+#   1) Each child must have at least one candy.
+#   2) Children with a higher rating get more candies than their neighbors.
+# Return the minimum number of candies you need to have to distribute the candies to the children.
+# ---------------------
+# n == ratings.length  ,  1 <= n <= 2 * 104  ,  0 <= ratings[i] <= 2 * 104
+
+
+def candy(ratings: list[int]) -> int:
+    pass
+
+
+test1 = [1, 0, 2]
+test1_out = 5
+
+test2 = [1, 2, 2]
+test2_out = 4
