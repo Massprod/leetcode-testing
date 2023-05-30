aed0495152bd7b6d6cb17e74d9eb2e5790cfbb52
Pampam <piankovpe@gmail.com>
2023-05-30 17:24:38 +0600

    initial

--- leetcode_problems/p128_longest_consecutive_sequence.py
+++ leetcode_problems/p128_longest_consecutive_sequence.py
@@ -9,6 +9,22 @@ def longest_consecutive(nums: list[int]) -> int:
     pass
 
 
+# Yeah. That's it, we use HASH operations which always take O(1) time,
+# and just checking every value as I wanted but not from an original list.
+# Ok. Guess for the future use => if we use some HASH table we can call searching in it O(1).
+# -----------------------
+# Time -> O(n) -> How?
+# Obvious basic solution is to check every number left_right sides for values of num-1, num+1, but it's (n * n).
+# With while or recursion for like I did in word_search_matrix, encounter some num-1 and start search from it with
+# lowest_value - 1, highest_value + 1.
+# Way I see to speed this, is sort, than we could just walk from lowest_highest and record only -+1 changes.
+# But sorting is at least O(n * (log n)).
+# Either I don't know some method for this like bit_stuff, or it's same as p148.
+# Where's recursion without saving a result was counted as O(1) space, when it shouldn't.
+# No reasons to waste time for trying to solve it on my own. Because it's can't be done in O(n) without some rules,
+# like in p148, or it's task with bit operations which I have so little experience and don't see it.
+
+
 test1 = [100, 4, 200, 1, 3, 2]
 test1_out = 4
 
