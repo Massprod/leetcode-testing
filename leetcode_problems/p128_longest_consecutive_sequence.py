4805f90d1c534d9585332688ca36b4582f60beb3
Pampam <piankovpe@gmail.com>
2023-05-30 17:12:26 +0600

    initial

---
+++ leetcode_problems/p128_longest_consecutive_sequence.py
@@ -0,0 +1,16 @@
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+# !
+# You must write an algorithm that runs in O(n) time.!
+# -----------------------
+# 0 <= nums.length <= 105  ,  -109 <= nums[i] <= 109
+
+
+def longest_consecutive(nums: list[int]) -> int:
+    pass
+
+
+test1 = [100, 4, 200, 1, 3, 2]
+test1_out = 4
+
+test2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+test2_out = 9
