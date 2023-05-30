6892461f8cd66cb2e8f8e234b42c73b111ff6124
Pampam <piankovpe@gmail.com>
2023-05-30 17:45:29 +0600

    add working_sol (53.27%, 11,1%) -> (458ms, 34mb)

--- leetcode_problems/p128_longest_consecutive_sequence.py
+++ leetcode_problems/p128_longest_consecutive_sequence.py
@@ -6,9 +6,40 @@
 
 
 def longest_consecutive(nums: list[int]) -> int:
-    pass
+    # working_sol (53.27%, 11,1%) -> (458ms, 34mb)  time: O(n) | space: O(n)
+    original: dict[int] = {}
+    for num in nums:
+        original[num] = False
+    consecutive: int = 0
+    for key in original.keys():
+        if original[key] is False:
+            original[key] = True
+            value: int = key - 1
+            current_streak: int = 1
+            while (value in original) and (original[value] is False):
+                original[value] = True
+                current_streak += 1
+                value -= 1
+            value = key + 1
+            while (value in original) and (original[value] is False):
+                original[value] = True
+                current_streak += 1
+                value += 1
+            consecutive = max(consecutive, current_streak)
+    return consecutive
 
 
+# Time complexity: O(n) -> traversing once to create dictionary with all values from original list => O(n) ->
+# n - len of input_list^^ -> assuming that every search in dictionary is hash operation, and we can call that => O(1) ->
+#                         -> leading us to O(n)
+# Space complexity: O(n) -> extra space to store dictionary with same values as original list => O(n)
+# -----------------------
+# Still don't think that is correct to call dict_search as O(1) if we're searching multiple values,
+# because we're still doing N - operations just with O(1) time for every single one of them,
+# but all I could see is either HASH searching or sorting and counting after that.
+# So I was correct on thinking about solution that we can't solve it in O(n) without some extra rule,
+# like this HASH search. But w.e still correct idea just need to know this rule for a future use.
+# -----------------------
 # Yeah. That's it, we use HASH operations which always take O(1) time,
 # and just checking every value as I wanted but not from an original list.
 # Ok. Guess for the future use => if we use some HASH table we can call searching in it O(1).
@@ -27,6 +58,10 @@ def longest_consecutive(nums: list[int]) -> int:
 
 test1 = [100, 4, 200, 1, 3, 2]
 test1_out = 4
+print(longest_consecutive(test1))
+assert test1_out == longest_consecutive(test1)
 
 test2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
 test2_out = 9
+print(longest_consecutive(test2))
+assert test2_out == longest_consecutive(test2)
