f5e5f4f83401f9325bc316c1883709dae6f784be
Pampam <piankovpe@gmail.com>
2023-05-31 16:13:45 +0600

    initial

--- leetcode_problems/p125_valid_palindrome.py
+++ leetcode_problems/p125_valid_palindrome.py
@@ -12,6 +12,20 @@ def is_palindrome(s: str) -> bool:
     pass
 
 
+# -------------------
+# !
+# Alphanumeric characters are made up of the 26 letters of the alphabet (A through Z)
+# and the digits 0 through 9. So, 1, 2, q, f, m, p, and 10 are all examples of letters and numbers.
+# Alphanumeric characters also include characters like *, &, and @. !
+# !
+# s consists only of printable ASCII characters -> 20 - 127numbers. !
+# Hmm. How we should consider *, &, @ and others into reading of a palindrome?
+# !
+# Alphanumeric characters include letters and numbers. !
+# I will stick to this for now, if I fail because there's some other signs. Not my fault, because this description...
+# ASCII for nums and lowercase_letters in this case: 30 - 39, 97 - 122 (inclusive)
+
+
 test1 = "A man, a plan, a canal: Panama"
 test1_out = True
 
