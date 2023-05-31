bafc3fd61c435df081d7b5626353d866bf45a327
Pampam <piankovpe@gmail.com>
2023-05-31 20:31:50 +0600

    add working_sol (91.82%, 33.87%) -> (777ms, 29.1mb)

--- leetcode_problems/p1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
+++ leetcode_problems/p1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
@@ -6,17 +6,49 @@
 
 
 def num_subseq(nums: list[int], target: int) -> int:
-    pass
+    # working_sol (91.82%, 33.87%) -> (777ms, 29.1mb)  time: O(n * (log n) + n) | space: O(1)
+    nums.sort()
+    subs: int = 0
+    min_point: int = 0
+    max_point: int = len(nums) - 1
+    while min_point < max_point:
+        if nums[min_point] + nums[max_point] <= target:
+            options: int = max_point - min_point
+            subs += 1 << options
+            min_point += 1
+            continue
+        max_point -= 1
+    if nums[min_point] << 1 <= target:
+        subs += 1
+    return subs % (10 ** 9 + 7)
+
+
+# Time complexity: O(n * (log n) + n) -> sorting input_list with built_in => O(n * (log n)) ->
+# n - len of input_list^^| -> traversing through whole input_list, checking every index as min or max points => O(n) ->
+#                          -> O(n * (log n) + n)
+# Space complexity: O(1) -> python built_in sort() => O(1) -> 4 extra constants, doesn't depend on input => O(1)
+# --------------------
+# Total subs for len == n ->  nC0 + nC1 + nC2 + … nCn = 2 ** n.
+# For a future use -> always remember that bit_shift is faster than using ** or *,
+#                     in this case is faster to do left_shift for 1 to option times,
+#                     2 ** n => 1 << n, because left_shit is equal to (any * (2 ** n)) but it's faster.
+#                     ^^took a peak on 780ms after mine 7900 ms.
 
 
 test1 = [3, 5, 6, 7]
 test1_target = 9
 test1_out = 4
+print(num_subseq(test1, test1_target))
+assert test1_out == num_subseq(test1, test1_target)
 
 test2 = [3, 3, 6, 8]
 test2_target = 10
 test2_out = 6
+print(num_subseq(test2, test2_target))
+assert test2_out == num_subseq(test2, test2_target)
 
 test3 = [2, 3, 3, 4, 6, 7]
 test3_target = 12
 test3_out = 61
+print(num_subseq(test3, test3_target))
+assert test3_out == num_subseq(test3, test3_target)
