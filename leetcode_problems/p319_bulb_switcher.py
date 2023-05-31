5073746f7030eaa7a8e8de141fc50c91b37b1190
Pampam <piankovpe@gmail.com>
2023-05-31 23:35:46 +0600

    initial

---
+++ leetcode_problems/p319_bulb_switcher.py
@@ -0,0 +1,23 @@
+# There are n bulbs that are initially off. You first turn on all the bulbs,
+# then you turn off every second bulb.
+#
+# On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on).
+# For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
+#
+# Return the number of bulbs that are on after n rounds.
+# -----------------------
+# 0 <= n <= 109
+
+
+def bulb_switch(n: int) -> int:
+    pass
+
+
+test1 = 3
+test1_out = 1
+
+test2 = 0
+test2_out = 0
+
+test3 = 1
+test3_out = 1
