98a706247902b2261ededa2320ddb3dae67e95a9
Pampam <piankovpe@gmail.com>
2023-05-31 17:45:26 +0600

    add working_sol (16.33%, 40.61%) -> (750ms, 16.8mb)

--- leetcode_problems/p2215_find_the_difference_of_two_arrays.py
+++ leetcode_problems/p2215_find_the_difference_of_two_arrays.py
@@ -8,18 +8,32 @@
 
 
 def find_difference(nums1: list[int], nums2: list[int]) -> list[list[int]]:
+    # working_sol (16.33%, 40.61%) -> (750ms, 16.8mb)  time: O(max(n, m)) | space: O(2*(n + m))
     answer: list[list[int]] = [[], []]
     used: set[int] = set()
-    for _ in nums1:
-        if (_ not in used) and (_ not in nums2):
-            answer[0].append(_)
-            used.add(_)
-    for _ in nums2:
-        if (_ not in used) and (_ not in nums1):
-            answer[1].append(_)
-            used.add(_)
+    for x in range(max(len(nums1), len(nums2))):
+        if (x < len(nums1)) and (nums1[x] not in used) and (nums1[x] not in nums2):
+            answer[0].append(nums1[x])
+            used.add(nums1[x])
+        if (x < len(nums2)) and (nums2[x] not in used) and (nums2[x] not in nums1):
+            answer[1].append(nums2[x])
+            used.add(nums2[x])
     return answer
 
+
+# Time complexity: O(max(n, m)) -> checking every index in nums1 and nums2 once, but looping for max_len of 2 inputs =>
+# n - len of nums1^^| => O(max(n, m)) or O(n) - where's n is len of longest input_list.
+# m - len of nums2^^|
+# Space complexity: O((n + m) + (n + m)) -> in the worst case, creating lists with all values from both inputs,
+#                                           because there's no values presents in counterpart => O(n + m) ->
+#                                        -> and there's duplicate for every value in input_lists,
+#                                           creating set of size (n + m) => O(n + m) ->
+#                                        -> leading us to => O((n + m) + (n + m)) => O(2(n + m))
+# -----------------------
+# Or I could just use set() to remove duplicates and scroll created sets after it.
+# Should be faster, because otherwise, we're checking every duplicate being in used every time,
+# and if we're creating sets without duplicates we're going to skip this check.
+# -----------------------
 # Yep. I was correct they're not equal length.
 # -----------------------
 # If we knew that len(nums1) == len(nums2), than I could do this with 1 loop.
