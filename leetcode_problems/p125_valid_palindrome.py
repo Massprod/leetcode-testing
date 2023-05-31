4eaa20e39920e1e52f9b8025067c88aa035fb3c8
Pampam <piankovpe@gmail.com>
2023-05-31 16:03:34 +0600

    initial

---
+++ leetcode_problems/p125_valid_palindrome.py
@@ -0,0 +1,22 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+# and removing all non-alphanumeric characters, it reads the same forward and backward.
+# Alphanumeric characters include letters and numbers.
+#
+# Given a string s, return true if it is a palindrome, or false otherwise.
+# -------------------
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+
+def is_palindrome(s: str) -> bool:
+    pass
+
+
+test1 = "A man, a plan, a canal: Panama"
+test1_out = True
+
+test2 = "race a car"
+test2_out = False
+
+test3 = " "
+test3_out = True
