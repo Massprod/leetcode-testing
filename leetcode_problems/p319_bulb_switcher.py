7b7f3b7f5f822d733cbe2350035dae4055f91dd2
Pampam <piankovpe@gmail.com>
2023-06-01 00:08:03 +0600

    pre_commit

--- leetcode_problems/p319_bulb_switcher.py
+++ leetcode_problems/p319_bulb_switcher.py
@@ -10,14 +10,45 @@
 
 
 def bulb_switch(n: int) -> int:
-    pass
+    bulbs_on: int = 0
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    bulbs: dict[int] = {}
+    for _ in range(1, n + 1):
+        if _ % 2 == 0:
+            bulbs[_] = False
+            continue
+        bulbs[_] = True
+    round_num: int = 3
+    while round_num < n:
+        for x in range(round_num, n + 1, round_num):
+            bulbs[x] = not bulbs[x]
+        round_num += 1
+    bulbs[round_num] = not bulbs[round_num]
+    for bulb in bulbs:
+        if bulbs[bulb] is True:
+            bulbs_on += 1
+    return bulbs_on
 
 
 test1 = 3
 test1_out = 1
+print(bulb_switch(test1))
 
 test2 = 0
 test2_out = 0
+print(bulb_switch(test2))
 
 test3 = 1
 test3_out = 1
+print(bulb_switch(test3))
+
+test4 = 13
+print(bulb_switch(test4))
+
+test5 = 2
+print(bulb_switch(test5))
