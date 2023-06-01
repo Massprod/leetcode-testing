7fa5a2f50968355b55b5ece2275144a170960664
Pampam <piankovpe@gmail.com>
2023-06-01 20:36:28 +0600

    rebuild

--- leetcode_problems/p2140_solving_questions_with_brainpower.py
+++ leetcode_problems/p2140_solving_questions_with_brainpower.py
@@ -19,10 +19,9 @@ def most_points(questions: list[list[int]]) -> int:
     def count_points(start: int, cur_max_points: int = 0) -> int:
         cur_max_points += questions[start][0]
         for g in range(start + questions[start][1] + 1, len(questions)):
-            if g not in used or questions[x][0] > used_values[g]:
-                path.append(count_points(g, cur_max_points))
-                used.add(g)
-                used_values[g] = questions[start][0]
+            path.append(count_points(g, cur_max_points))
+            used.add(g)
+            used_values[g] = questions[start][0]
         return cur_max_points
 
     for x in range(len(questions)):
@@ -32,9 +31,14 @@ def most_points(questions: list[list[int]]) -> int:
                 path.append(count_points(y, cur_points))
                 used.add(y)
                 used_values[y] = cur_points
-    return max(path)
+    if len(path) != 0:
+        return max(path)
+    return max(questions)[0]
 
 
+# Ok. Made this with skipping second step values if we already started from them  with values higher this current step.
+# But how to skip all other with including previous?
+# -------------------------
 # Correct solution, but time_limit what can I cull?
 # -------------------------
 # Recursion like with counting best steps/jumps?
@@ -58,3 +62,6 @@ test4 = [
     [1, 3], [60, 2], [63, 3], [79, 3], [19, 1], [7, 1], [35, 1], [55, 4], [1, 4], [41, 1], [58, 5],
 ]
 print(most_points(test4))
+
+test5 = [[12, 46], [78, 19], [63, 15], [79, 62], [13, 10]]
+print(most_points(test5))
