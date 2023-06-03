18a28a81fc5899853566d25452289857407533d9
Pampam <piankovpe@gmail.com>
2023-06-03 20:15:21 +0600

    initial

---
+++ leetcode_problems/p347_top_k_frequent_elements.py
@@ -0,0 +1,19 @@
+# Given an integer array nums and an integer k, return the k most frequent elements.
+# You may return the answer in any order.
+# --------------------------
+# 1 <= nums.length <= 10 ** 5  ,  -10 ** 4 <= nums[i] <= 10 ** 4
+# k is in the range [1, the number of unique elements in the array].
+# It is guaranteed that the answer is unique.
+
+
+def top_k_frequent(nums: list[int], k: int) -> list[int]:
+    pass
+
+
+test1 = [1, 1, 1, 2, 2, 3]
+test1_k = 2
+test1_out = [1, 2]
+
+test2 = [1]
+test2_k = 1
+test2_out = [1]
