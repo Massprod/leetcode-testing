d554e6ed24f0c9b5aa5a684e664c79c2e6e965be
Pampam <piankovpe@gmail.com>
2023-06-03 21:03:25 +0600

    add working_sol (5.2%, 64.58%) -> (264ms, 21.1mb)

--- leetcode_problems/p347_top_k_frequent_elements.py
+++ leetcode_problems/p347_top_k_frequent_elements.py
@@ -9,6 +9,7 @@
 
 
 def top_k_frequent(nums: list[int], k: int) -> list[int]:
+    # working_sol (5.2%, 64.58%) -> (264ms, 21.1mb)  time: O(n) | space: O(n + k)
     doubles: dict[int] = {}
     for num in nums:
         if num not in doubles:
@@ -20,7 +21,7 @@ def top_k_frequent(nums: list[int], k: int) -> list[int]:
     step: int = 0
     while step != k:
         for key in doubles:
-            if not max_key:
+            if max_key is None:
                 max_key = key
                 continue
             if doubles[max_key] < doubles[key]:
@@ -32,6 +33,15 @@ def top_k_frequent(nums: list[int], k: int) -> list[int]:
     return top_values
 
 
+# Time complexity: O(n) -> traversing input_list once to recreate dict_counter for every value => O(n) ->
+# n - len of input_list^^| -> checking every value in dictionary for K times to get K_max_values => O(1 * k) ->
+#                          -> appending this values into top_values => O(1) -> O(n)
+# ^^It's correct if we assume that all HASH operations with dictionaries is O(1).
+# But I think it's O(n * k + n), because I'm still not convicted that dict operations even if there's N of them is O(1).
+# --------------------------
+# Auxiliary space: O(n + k) -> creating dict with all unique values from nums, worst case - no duplicates => O(n) ->
+#                               -> creating top_values list with K number of values in it => O(k) -> O(n + k)
+# --------------------------
 # Hmm. What about same number of duplicates?
 # !
 # It is guaranteed that the answer is unique. ! -> Ok. But duplicates is not the same value.
@@ -48,13 +58,24 @@ test1 = [1, 1, 1, 2, 2, 3]
 test1_k = 2
 test1_out = [1, 2]
 print(top_k_frequent(test1, test1_k))
+assert test1_out == top_k_frequent(test1, test1_k)
 
 test2 = [1]
 test2_k = 1
 test2_out = [1]
 print(top_k_frequent(test2, test2_k))
+assert test2_out == top_k_frequent(test2, test2_k)
 
 test3 = [1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 5, 5, 5, 5, 5, 5]
 test3_k = 3
-test3_out = [1, 3, 5]
+test3_out = [5, 2, 1]
 print(top_k_frequent(test3, test3_k))
+assert test3_out == top_k_frequent(test3, test3_k)
+
+# test4 - failed -> gosh, again im getting 0 at IF check and failing it because 0 is FALSE...
+#                   Always explicitly check for None!
+test4 = [3, 0, 1, 0]
+test4_k = 1
+test4_out = [0]
+print(top_k_frequent(test4, test4_k))
+assert test4_out == top_k_frequent(test4, test4_k)
