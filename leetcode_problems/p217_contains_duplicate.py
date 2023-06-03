5f629075cd167918772808308bb55d56d2a93468
Pampam <piankovpe@gmail.com>
2023-06-03 19:52:07 +0600

    initial

---
+++ leetcode_problems/p217_contains_duplicate.py
@@ -0,0 +1,18 @@
+# Given an integer array nums, return true if any value appears at least twice in the array,
+#   and return false if every element is distinct.
+# -----------------------
+# 1 <= nums.length <= 10 ** 5  ,  -10 ** 9 <= nums[i] <= 10 ** 9
+
+
+def contains_duplicate(nums: list[int]) -> bool:
+    pass
+
+
+test1 = [1, 2, 3, 1]
+test1_out = True
+
+test2 = [1, 2, 3, 4]
+test2_out = False
+
+test3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+test3_out = True
