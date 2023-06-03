119095ebe38e2f17b23556c41b53750d9ef97e41
Pampam <piankovpe@gmail.com>
2023-06-03 14:36:07 +0600

    pre commit

--- leetcode_problems/p139_word_break.py
+++ leetcode_problems/p139_word_break.py
@@ -8,17 +8,40 @@
 
 
 def word_break(s: str, wordDict: list[str]) -> bool:
-    pass
+    all_words: set[str] = set(wordDict)
+    path: list[str] = []
+
+    def check_start(start: int):
+        if len("".join(path)) == len(s):
+            return True
+        to_check: str = ""
+        for y in range(start, len(s)):
+            to_check += s[y]
+            if to_check in all_words:
+                path.append(to_check)
+                if check_start(y + 1):
+                    return True
+        return False
+
+    return check_start(0)
+
+
+# Seems working and I don't  see any tricky parts with constraints, so let's try to commit.
+# -----------------------------
+# Left to right walk with sample_string, which are we increasing for a 1 sign and checking to be present?
 
 
 test1 = "leetcode"
 test1_dict = ["leet", "code"]
 test1_out = True
+print(word_break(test1, test1_dict))
 
 test2 = "applepenapple"
 test2_dict = ["apple", "pen"]
 test2_out = True
+print(word_break(test2, test2_dict))
 
 test3 = "catsandog"
 test3_dict = ["cats", "dog", "sand", "and", "cat"]
 test3_out = False
+print(word_break(test3, test3_dict))
