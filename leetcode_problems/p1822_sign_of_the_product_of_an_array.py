8ee5d710b9493834a78a74e920e666cbf5b7be2e
Pampam <piankovpe@gmail.com>
2023-06-04 15:14:16 +0600

    add working_sol (80.22%, 45.83%) -> (70ms, 16.5mb)

--- leetcode_problems/p1822_sign_of_the_product_of_an_array.py
+++ leetcode_problems/p1822_sign_of_the_product_of_an_array.py
@@ -9,6 +9,7 @@
 
 
 def array_sign(nums: list[int]) -> int:
+    # working_sol (80.22%, 45.83%) -> (70ms, 16.5mb)  time: O(n) | space: O(1)
     negative: bool = False
     for num in nums:
         if num == 0:
@@ -20,14 +21,22 @@ def array_sign(nums: list[int]) -> int:
     return 1
 
 
+# Time complexity: O(n) -> traversing whole input_list once => O(n)
+# n - len of input_list^^|
+# Auxiliary space: O(1) -> only 1 constant doesn't depend on input => O(1)
+
+
 test1 = [-1, -2, -3, -4, 3, 2, 1]
 test1_out = 1
 print(array_sign(test1))
+assert test1_out == array_sign(test1)
 
 test2 = [1, 5, 0, 2, -3]
 test2_out = 0
 print(array_sign(test2))
+assert test2_out == array_sign(test2)
 
 test3 = [-1, 1, -1, 1, -1]
 test3_out = -1
 print(array_sign(test3))
+assert test3_out == array_sign(test3)
