0f1ecd413d7269388886ca55c468f8c6748f809b
Pampam <piankovpe@gmail.com>
2023-06-04 15:07:18 +0600

    initial

---
+++ leetcode_problems/p1822_sign_of_the_product_of_an_array.py
@@ -0,0 +1,22 @@
+# There is a function signFunc(x) that returns:
+#   1 if x is positive.
+#   -1 if x is negative.
+#   0 if x is equal to 0.
+# You are given an integer array nums. Let product be the product of all values in the array nums.
+# Return signFunc(product).
+# ----------------------------
+# 1 <= nums.length <= 1000  ,  -100 <= nums[i] <= 100
+
+
+def array_sign(nums: list[int]) -> int:
+    pass
+
+
+test1 = [-1, -2, -3, -4, 3, 2, 1]
+test1_out = 1
+
+test2 = [1, 5, 0, 2, -3]
+test2_out = 0
+
+test3 = [-1, 1, -1, 1, -1]
+test3_out = -1
