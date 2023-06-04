5448c14282c49fc9ca49ab512cc900b0d7929bcb
Pampam <piankovpe@gmail.com>
2023-06-04 15:18:44 +0600

    initial

---
+++ leetcode_problems/p121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,18 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy one stock
+#   and choosing a different day in the future to sell that stock.
+# Return the maximum profit you can achieve from this transaction.
+# If you cannot achieve any profit, return 0.
+# -----------------------
+# 1 <= prices.length <= 10 ** 5  ,  0 <= prices[i] <= 10 ** 4
+
+
+def max_profit(prices: list[int]) -> int:
+    pass
+
+
+test1 = [7, 1, 5, 3, 6, 4]
+test1_out = 5
+
+test2 = [7, 6, 4, 3, 1]
+test2_out = 0
