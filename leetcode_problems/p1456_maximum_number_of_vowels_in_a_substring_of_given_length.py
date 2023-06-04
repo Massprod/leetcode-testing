ce046fa23da49f8d6988cba927adf4876006ea05
Pampam <piankovpe@gmail.com>
2023-06-04 16:23:15 +0600

    pre_commit

--- leetcode_problems/p1456_maximum_number_of_vowels_in_a_substring_of_given_length.py
+++ leetcode_problems/p1456_maximum_number_of_vowels_in_a_substring_of_given_length.py
@@ -7,17 +7,56 @@
 
 
 def max_vowels(s: str, k: int) -> int:
-    pass
+    k_sub_indexes: set = set()
+    tempo: list[int] = []
+    vowels: dict[str] = {
+        "a": True,
+        "e": True,
+        "i": True,
+        "o": True,
+        "u": True,
+        "max": 0,
+    }
+
+    def new_combine(left_to_use: int, start: int = 0, end: int = len(s), ) -> None:
+        if left_to_use == 0:
+            k_sub_indexes.add(tuple(sorted(tempo)))
+            return
+        for num in range(start, end):
+            if num in tempo:
+                break
+            tempo.append(num)
+            new_combine(left_to_use - 1, start + 1, end)
+            tempo.clear()
+            return
+
+    for _ in range(len(s)):
+        new_combine(k, start=_)
+    for tup in k_sub_indexes:
+        current_vowels: int = 0
+        for index in tup:
+            if s[index] in vowels:
+                current_vowels += 1
+        if current_vowels > vowels["max"]:
+            vowels["max"] = current_vowels
+    return vowels["max"]
+
+
+# Bad part I was thinking we should use every SUBSET, and rebuild previously made recursion for that.
+# But after than I see that only substrings...W.e still made this working, but I will need to cull some parts.
 
 
 test1 = "abciiidef"
 test1_k = 3
 test1_out = 3
+print(max_vowels(test1, test1_k))
 
 test2 = "aeiou"
 test2_k = 2
 test2_out = 2
+print(max_vowels(test2, test2_k))
 
 test3 = "leetcode"
 test3_k = 3
 test3_out = 2
+print(max_vowels(test3, test3_k))
