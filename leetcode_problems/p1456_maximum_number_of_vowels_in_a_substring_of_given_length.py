e1eb8a7c134853c28dfb8128a707c9a978c7e333
Pampam <piankovpe@gmail.com>
2023-06-04 15:57:00 +0600

    initial

---
+++ leetcode_problems/p1456_maximum_number_of_vowels_in_a_substring_of_given_length.py
@@ -0,0 +1,23 @@
+# Given a string s and an integer k, return the maximum number
+#        of vowel letters in any substring of s with length k.
+# Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+# --------------------------
+# 1 <= s.length <= 10 ** 5  ,  1 <= k <= s.length
+# s consists of lowercase English letters.
+
+
+def max_vowels(s: str, k: int) -> int:
+    pass
+
+
+test1 = "abciiidef"
+test1_k = 3
+test1_out = 3
+
+test2 = "aeiou"
+test2_k = 2
+test2_out = 2
+
+test3 = "leetcode"
+test3_k = 3
+test3_out = 2
