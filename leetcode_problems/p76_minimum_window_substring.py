aee8228bd22c397a1288839ee9dcb69a94fb13dc
Pampam <piankovpe@gmail.com>
2023-06-05 00:48:09 +0600

    add working_sol

--- leetcode_problems/p76_minimum_window_substring.py
+++ leetcode_problems/p76_minimum_window_substring.py
@@ -10,19 +10,22 @@
 
 
 def min_window(s: str, t: str) -> str:
+    if len(t) > len(s):
+        return ""
     chars: dict[str] = {}
     cur_chars: dict[str] = {}
+    extras: dict[str] = {}
     for char in t:
         if char not in chars:
             chars[char] = 1
             cur_chars[char] = 0
+            extras[char] = 0
             continue
         chars[char] += 1
     left: int = 0
     right: int = 0
     min_sub: str = ""
     whole: bool = False
-    extras: list[str] = []
     while right != len(s) + 1 and left < len(s):
         for key in cur_chars:
             if cur_chars[key] >= chars[key]:
@@ -34,19 +37,21 @@ def min_window(s: str, t: str) -> str:
         if not whole:
             for key in cur_chars:
                 if cur_chars[key] > chars[key]:
-                    if (cur_chars[key] - chars[key]) != extras.count(key):
-                        extras.append(key)
+                    if (cur_chars[key] - chars[key]) != extras[key]:
+                        extras[key] += 1
         if whole:
             if len(min_sub) > len(s[left: right]) or len(min_sub) == 0:
                 min_sub = s[left: right]
+                if len(min_sub) == len(t):
+                    return min_sub
             if s[left] in cur_chars:
                 cur_chars[s[left]] -= 1
             left += 1
             continue
-        while left < len(s) and ((s[left] not in cur_chars) or (s[left] in extras)):
+        while left < len(s) and ((s[left] not in cur_chars) or (extras[s[left]] != 0)):
             if s[left] in extras:
                 cur_chars[s[left]] -= 1
-                extras.remove(s[left])
+                extras[s[left]] -= 1
             left += 1
         if (right < len(s)) and (s[right] in cur_chars):
             cur_chars[s[right]] += 1
@@ -54,8 +59,26 @@ def min_window(s: str, t: str) -> str:
     return min_sub
 
 
+# Sheesh. Hard without extra info and only using HINT as guidance, evolving.
+# Dunno about O(n + m), but at least I made a correctly working solution.
+# Actually we're making it in ONE_WAY walk O(n), but with every step we're checking too much data
+# in dictionaries, so if we can't call dictionary operations as O(1), in this case I doubt that.
+# Because search in dictionary is O(1) but we're checking every KEY. Plus there's top solutions with <200ms,
+# when mine is 801ms. So if they're O(n + m) mine is something O(n + m * n) ->
+# -> we're checking every index once, but every KEY in dictionaries will be checked, on every index step.
+# --------------------------
+# Don't think changing extras from list to dict is going to change anything, but if there's
+# a 100 extra it's going to be faster. Well let's try this one as well.
+# Otherwise, I need to find how to check all_symbols used or not faster.
+# Well well well, working in this 266 test_case, 1 to go :)
+# --------------------------
 # Sadly 265/267 cases passed, now it's TimeGate, but at least I made working solution,
 # without extra info(google/gpt). There's a lot of extra checks, and maybe I will find how to cull them.
+# What about length?
+# While doing heavy_lifting I forgot that we can just cull any input with incorrect length.
+# Nah, not the case, but still good to implement.
+# Another one I see is that we can insta return if we found min_sub with equal length to t,
+# because t == min_sub is IDEAL option, no reasons to search anything else.
 # --------------------------
 # Ok. I need to fail, because I'm kinda stuck with thinking that's correct need more test_cases.
 # --------------------------
