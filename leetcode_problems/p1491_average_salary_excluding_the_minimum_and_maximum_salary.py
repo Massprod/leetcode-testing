c4ca2cee4bc927f5043e60475388d27718319f19
Pampam <piankovpe@gmail.com>
2023-06-05 16:00:43 +0600

    initial

---
+++ leetcode_problems/p1491_average_salary_excluding_the_minimum_and_maximum_salary.py
@@ -0,0 +1,17 @@
+# You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+# Return the average salary of employees excluding the minimum and maximum salary.
+# Answers within 10 ** -5 of the actual answer will be accepted.
+# -----------------------------
+# 3 <= salary.length <= 100  ,  1000 <= salary[i] <= 106
+# All the integers of salary are unique.
+
+
+def average(salary: list[int]) -> float:
+    pass
+
+
+test1 = [4000, 3000, 1000, 2000]
+test1_out = 2500.00000
+
+test2 = [1000, 2000, 3000]
+test2_out = 2000.00000
