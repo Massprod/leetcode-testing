05a14784995e64d83c9d85ec98599391ca7d1085
Pampam <piankovpe@gmail.com>
2023-06-06 16:51:27 +0600

    initial

---
+++ leetcode_problems/p704_binary_search.py
@@ -0,0 +1,21 @@
+# Given an array of integers nums which is sorted in ascending order, and an integer target,
+# write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+# --------------------
+# 1 <= nums.length <= 10 ** 4  ,  -10 ** 4 < nums[i], target < 10 ** 4
+# All the integers in nums are unique.
+# nums is sorted in ascending order.
+
+
+def binary_search(nums: list[int], target: int) -> int:
+    pass
+
+
+test1 = [-1, 0, 3, 5, 9, 12]
+test1_t = 9
+test1_out = 4
+
+test2 = [-1, 0, 3, 5, 9, 12]
+test2_t = 2
+test2_out = -1
