bbcdd7ab10ebd35dfad2a87aaa534a552e843a1d
Pampam <piankovpe@gmail.com>
2023-06-06 08:17:16 +0600

    initial

---
+++ leetcode_problems/p258_add_digits.py
@@ -0,0 +1,16 @@
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+# --------------------
+# 0 <= num <= 2 ** 31 - 1
+# --------------------
+# Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+
+def add_digits(num: int) -> int:
+    pass
+
+
+test1 = 38
+test1_out = 2
+
+test2 = 0
+test2_out = 2
