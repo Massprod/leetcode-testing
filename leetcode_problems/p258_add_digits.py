e9acfae0285357c7f2115f413fb1a108671cf55f
Pampam <piankovpe@gmail.com>
2023-06-06 08:25:34 +0600

    pre_commit

--- leetcode_problems/p258_add_digits.py
+++ leetcode_problems/p258_add_digits.py
@@ -6,11 +6,27 @@
 
 
 def add_digits(num: int) -> int:
-    pass
+    if num == 0:
+        return 0
+    return 1 + (num - 1) % 9
+
+
+# For doing this as a loop is simple -> taking num % 10 until we hit something lower than 10 -> summarize everything ->
+# repeat -> until num < 10 and if num == 10, num % 10 will be 0 and return 1 in this case.
+# For a O(1) no idea, so I'm taking a Hint, and it's extra_math. ! https://en.wikipedia.org/wiki/Digital_root !
+# DRb(n) = 1 + ((num - 1) % (base - 1)), if n != 0
+# DRb(n) = 0, if n == 0
+# In out case base == 10, so DR10(num) = 1 + ((num - 1) % 9) for n > 0
 
 
 test1 = 38
 test1_out = 2
+print(add_digits(test1))
 
 test2 = 0
 test2_out = 2
+print(add_digits(test2))
+
+test3 = 10
+test3_out = 1
+print(add_digits(test3))
