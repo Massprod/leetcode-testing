663e7805fd90ff0c9d6388c7cd5a850ad9a3e5bb
Pampam <piankovpe@gmail.com>
2023-06-08 15:59:50 +0600

    initial

---
+++ leetcode_problems/p169_majority_element.py
@@ -0,0 +1,21 @@
+# Given an array nums of size n, return the majority element.
+#
+# The majority element is the element that appears more than ⌊n / 2⌋ times.
+# You may assume that the majority element always exists in the array.
+# ----------------------
+# n == nums.length
+# 1 <= n <= 5 * 10 ** 4
+# -10 ** 9 <= nums[i] <= 10 ** 9
+# ----------------------
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
+def majority_element(nums: list[int]) -> int:
+    pass
+
+
+test1 = [3, 2, 3]
+test1_out = 3
+
+test2 = [2, 2, 1, 1, 1, 2, 2]
+test2_out = 2
