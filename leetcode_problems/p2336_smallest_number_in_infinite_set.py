c1eb9416e0ee31557b70fe0ac600935846d5c139
Pampam <piankovpe@gmail.com>
2023-06-08 11:09:22 +0600

    add working_sol (43.9%, 9.48%) -> (137ms, 17.1mb)

--- leetcode_problems/p2336_smallest_number_in_infinite_set.py
+++ leetcode_problems/p2336_smallest_number_in_infinite_set.py
@@ -11,6 +11,7 @@ import heapq
 
 
 class SmallestInfiniteSet:
+    # working_sol (43.9%, 9.48%) -> (137ms, 17.1mb)  time: below | space: O(m + (n - m))
 
     def __init__(self):
         self.original: heapq = [num for num in range(1, 1001)]
@@ -25,8 +26,20 @@ class SmallestInfiniteSet:
     def addBack(self, num: int) -> None:
         if num in self.removed:
             heapq.heappush(self.original, num)
+            self.removed.remove(num)
 
 
+# Time complexity:  initial = creating original list with size of n => O(n) ->
+# n = 1000^^|              -> crating heapq from this list => O(n) == O(n).
+#                   popSmallest = removing the smallest element from a heapq => O(log m) ->
+# m - values in heapq^^|   -> adding element into a set() => O(1) == O(log m).
+#                   addBack = getting item from a set() => O(1) -> if it's present pushing value into a heapq =>
+#                          => O(log m) -> removing item from a set() => O(1).
+# Space complexity: O(m + (n - m)) -> extra space to store heapq wih initial size and changes => O(m) ->
+#                                     -> extra space to store removed values in a set() => O(n - m).
+# ---------------------------
+# Failed because forgot to remove newly_added values from removed. Shame.
+# ---------------------------
 # Using heap to maintain order and pop() most left, otherwise I don't know how to maintain correct smallest value.
 # Because if we just use set or list, we can't normally maintain smallest, like -> remove 1 - 10 values, add 1,
 # smallest will become 1, after that delete 1, and how do we get smallest = 11? Only with min(), or other O(n) method.
