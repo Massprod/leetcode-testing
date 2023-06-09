310069193c39db6e7e63322555308fefcce3fe5a
Pampam <piankovpe@gmail.com>
2023-06-09 09:52:04 +0600

    initial

---
+++ leetcode_problems/p1492_the_kth_factor_of_n.py
@@ -0,0 +1,25 @@
+# You are given two positive integers n and k. A factor of an integer n is defined
+#       as an integer i where n % i == 0.
+# Consider a list of all factors of n sorted in ascending order,
+#       return the kth factor in this list or return -1 if n has less than k factors.
+# --------------------
+# 1 <= k <= n <= 1000
+# Follow up:
+# Could you solve this problem in less than O(n) complexity?
+
+
+def kth_factor(n: int, k: int) -> int:
+    pass
+
+
+test1 = 12
+test1_k = 3
+test1_out = 3
+
+test2 = 7
+test2_k = 2
+test2_out = 7
+
+test3 = 4
+test3_k = 4
+test3_out = -1
