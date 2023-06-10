0c4145a8d475bb70f9f7696cc4848f47e2ec67e8
Pampam <piankovpe@gmail.com>
2023-06-10 10:39:06 +0600

    initial

---
+++ leetcode_problems/p202_happy_number.py
@@ -0,0 +1,23 @@
+# Write an algorithm to determine if a number n is happy.
+# A happy number is a number defined by the following process:
+#   - Starting with any positive integer, replace the number by the sum of the squares of its digits.
+#   - Repeat the process until the number equals 1 (where it will stay),
+#        or it loops endlessly in a cycle which does not include 1.
+#   - Those numbers for which this process ends in 1 are happy.
+# Return true if n is a happy number, and false if not.
+# -----------------------
+# 1 <= n <= 2 ** 31 - 1
+
+
+def is_happy(n: int) -> bool:
+    pass
+
+
+test1 = 19
+test1_out = True
+
+test2 = 2
+test2_out = False
+
+test3 = 2 ** 31 - 1
+# test3_out = ???
