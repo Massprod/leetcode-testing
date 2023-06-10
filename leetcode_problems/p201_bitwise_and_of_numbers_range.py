0675385014e1737c09efe93b79f1c4acbeb7353e
Pampam <piankovpe@gmail.com>
2023-06-10 09:09:05 +0600

    initial

---
+++ leetcode_problems/p201_bitwise_and_of_numbers_range.py
@@ -0,0 +1,21 @@
+# Given two integers left and right that represent the range [left, right],
+#           return the bitwise AND of all numbers in this range, inclusive.
+# -------------------
+# 0 <= left <= right <= 2 ** 31 - 1
+
+
+def range_bitwise_and(left: int, right: int) -> int:
+    pass
+
+
+test1 = 5
+test1_right = 7
+test1_out = 4
+
+test2 = 0
+test2_right = 0
+test2_out = 0
+
+test3 = 1
+test3_right = 2147483647
+test3_out = 0
