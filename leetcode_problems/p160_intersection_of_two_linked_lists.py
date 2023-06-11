764880eda18b38af14f4abf97db2da4394d78a1f
Pampam <piankovpe@gmail.com>
2023-06-11 08:30:25 +0600

    add working_sol (76.21%, 40.7) -> (163ms, 31.6mb)  time: O(n + m) | space: O(1)

--- leetcode_problems/p160_intersection_of_two_linked_lists.py
+++ leetcode_problems/p160_intersection_of_two_linked_lists.py
@@ -29,21 +29,36 @@ def create_linked(to_link: list[int]) -> ListNode:
 
 
 def get_intersection_node(headA: ListNode, headB: ListNode) -> ListNode:
-    # working_sol (73.7%, 7.71%) -> (164ms, 32.4mb)  time: O(n + m) | space: O(n)
-    met: set[ListNode] = set()
-    while headA:
-        met.add(headA)
-        headA = headA.next
-    while headB:
-        if headB in met:
-            return headB
-        headB = headB.next
+    # working_sol (76.21%, 40.7) -> (163ms, 31.6mb)  time: O(n + m) | space: O(1)
+    pointer_1: ListNode = headA
+    pointer_2: ListNode = headB
+    switched_A: bool = False
+    switched_B: bool = False
+    while pointer_1 != pointer_2:
+        pointer_1, pointer_2 = pointer_1.next, pointer_2.next
+        if pointer_1 == pointer_2:
+            return pointer_2
+        if pointer_1 is None and not switched_A:
+            pointer_1 = headB
+        if pointer_2 is None and not switched_B:
+            pointer_2 = headA
+    return pointer_2
 
 
-# Time complexity: O(n + m) -> traversing both input_lists once => O(n + m).
-# n - nodes in headA^^|
-# m - nodes in headB^^|
-# Auxiliary space: O(n) -> only node links from headA will be saved into a set() => O(n)
+# Time complexity: O(n + m) -> in the worst case we're traversing whole headA + inter, headB + inter,
+# n - nodes of headA^^|        inter(inside headB) + inter(inside headA) -> O((n + m) + (log n + log  m)) ->
+# m - nodes of headB^^|        -> we can ignore this (log n + log m) part => O(n + m).
+# Auxiliary space: O(1) -> nothing extra that depends on input_lists => O(1)
+
+# ---------------
+# Ok. There's no extra math, just some logic that we can find collision point with extra travel.
+# If we pass whole headA + intersection_node and after that start to move from headB once again,
+# we're going to travel same distance as another pointer which started at headB
+# and did the same transfer to headA -> x - headA, y - inter_node, z - headB ->
+# x + y + z - distance of 1 pointer
+# z + y + x - distance of 2 pointer
+# x + y + z = z + y + x
+# y = y + z + x - x - z -> y = y -> so we're hitting same distance after extra switch_travel.
 # ---------------
 # No idea how to make this in O(1) memory, but we can make this in O(n + m) and O(n) memory.
 # Guess there's something like tasks with Floyd_algorithm, if you don't know it you just can't do this.
@@ -103,3 +118,14 @@ while tempo:
     tempo = tempo.next
 test3_out = test3_both
 assert test3_out == get_intersection_node(test3_A, test3_B)
+
+test4_B = create_linked([2])
+test4_both = create_linked([3])
+tempo = test4_B
+while tempo:
+    if tempo.next is None:
+        tempo.next = test4_both
+        break
+    tempo = tempo.next
+test4_out = test4_both
+assert test4_out == get_intersection_node(test4_both, test4_B)
