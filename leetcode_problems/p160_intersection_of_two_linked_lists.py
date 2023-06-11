1ae35b38a866c32f888406dd79248b0b28a60469
Pampam <piankovpe@gmail.com>
2023-06-11 07:53:31 +0600

    add working_sol (73.7%, 7.71%) -> (164ms, 32.4mb)  time: O(n + m) | space: O(n)

--- leetcode_problems/p160_intersection_of_two_linked_lists.py
+++ leetcode_problems/p160_intersection_of_two_linked_lists.py
@@ -29,9 +29,27 @@ def create_linked(to_link: list[int]) -> ListNode:
 
 
 def get_intersection_node(headA: ListNode, headB: ListNode) -> ListNode:
-    pass
+    # working_sol (73.7%, 7.71%) -> (164ms, 32.4mb)  time: O(n + m) | space: O(n)
+    met: set[ListNode] = set()
+    while headA:
+        met.add(headA)
+        headA = headA.next
+    while headB:
+        if headB in met:
+            return headB
+        headB = headB.next
 
 
+# Time complexity: O(n + m) -> traversing both input_lists once => O(n + m).
+# n - nodes in headA^^|
+# m - nodes in headB^^|
+# Auxiliary space: O(n) -> only node links from headA will be saved into a set() => O(n)
+# ---------------
+# No idea how to make this in O(1) memory, but we can make this in O(n + m) and O(n) memory.
+# Guess there's something like tasks with Floyd_algorithm, if you don't know it you just can't do this.
+# First commit with O(n) and search for extra_info after.
+
+# Could change it into a function, but w.e. Easy task and no reasons to.
 test1_A = create_linked([4, 1])
 test1_B = create_linked([5, 6, 1])
 test1_both = create_linked([8, 4, 5])
@@ -48,6 +66,7 @@ while tempo:
         break
     tempo = tempo.next
 test1_out = test1_both
+assert test1_out == get_intersection_node(test1_A, test1_B)
 
 test2_A = create_linked([1, 9, 1])
 test2_B = create_linked([3])
@@ -64,7 +83,8 @@ while tempo:
         tempo.next = test2_both
         break
     tempo = tempo.next
-test1_out = test2_both
+test2_out = test2_both
+assert test2_out == get_intersection_node(test2_A, test2_B)
 
 test3_A = create_linked([2, 6, 4])
 test3_B = create_linked([1, 5])
@@ -81,4 +101,5 @@ while tempo:
         tempo.next = test3_both
         break
     tempo = tempo.next
-test1_out = test3_both
+test3_out = test3_both
+assert test3_out == get_intersection_node(test3_A, test3_B)
