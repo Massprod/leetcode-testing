a6ddc3aac556053be3242103f2906fe8192a89c6
Pampam <piankovpe@gmail.com>
2023-06-11 07:34:46 +0600

    add working_sol (53.4%, 39.5%) -> (45ms, 16.2mb)

--- leetcode_problems/p191_number_of_1_bits.py
+++ leetcode_problems/p191_number_of_1_bits.py
@@ -7,17 +7,32 @@
 
 
 def hamming_weight(n: int) -> int:
-    pass
+    # working_sol (53.4%, 39.5%) -> (45ms, 16.2mb)  time: O(n) | space: O(1)
+    mask: int = 1
+    count: int = 0
+    while n:
+        count += n & mask
+        n = n >> 1
+    return count
+
+# Time complexity: O(n) -> looping until we delete every less_signi_bit and n becomes 0 => O(n).
+# Auxiliary space: O(1) -> 2 extra INTs, 1 can be deleted, doesn't depend on input => O(1)
+# --------------------
+# Basic bit manipulation with deleting lsb and counting this 1 bit value.
 
 
 test1 = 0o0000000000000000000000000001011
 test1_out = 3
-print(test1)
+print(hamming_weight(test1))
+assert test1_out == hamming_weight(test1)
 
 test2 = 0o0000000000000000000000010000000
 test2_out = 1
 print(test2)
+print(hamming_weight(test2))
+assert test2_out == hamming_weight(test2)
 
-test3 = 11111111111111111111111111111101
+test3 = 0o11111111111111111111111111111101
 test3_out = 31
-print(test3)
+print(hamming_weight(test3))
+assert test3_out == hamming_weight(test3)
