742674161de70ba42cc2e0969b0809a626a51525
Pampam <piankovpe@gmail.com>
2023-06-11 21:04:23 +0600

    initial

---
+++ leetcode_problems/p204_count_primes.py
@@ -0,0 +1,20 @@
+# Given an integer n, return the number of prime numbers that are strictly less than n.
+# --------------
+# 0 <= n <= 5 * 10 ** 6
+
+
+def count_primes(n: int) -> int:
+    pass
+
+
+test1 = 10
+test1_out = 4
+
+test2 = 0
+test2_out = 0
+
+test3 = 1
+test3_out = 0
+
+test4 = 4000001
+test4_out = 283146
