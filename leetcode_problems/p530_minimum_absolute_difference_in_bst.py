b560b85b22d49ace56a8784340734651bee8f74e
Pampam <piankovpe@gmail.com>
2023-06-14 10:54:11 +0600

    add working_sol (50.61%, 15.28%) -> (71ms, 18.9mb)  time: O(n * (log n)) | space: O(n)

--- leetcode_problems/p530_minimum_absolute_difference_in_bst.py
+++ leetcode_problems/p530_minimum_absolute_difference_in_bst.py
@@ -13,9 +13,10 @@ class TreeNode:
         self.right = right
 
 
-def get_minimum_diff(root: TreeNode) -> int:
+def get_minimum_diff(root: TreeNode) -> int | None:
+    # working_sol (50.61%, 15.28%) -> (71ms, 18.9mb)  time: O(n * (log n)) | space: O(n)
     if not root:
-        return root
+        return None
     all_values: list[int] = []
 
     def search_tree(node: TreeNode) -> None:
@@ -26,17 +27,34 @@ def get_minimum_diff(root: TreeNode) -> int:
             search_tree(node.right)
 
     search_tree(root)
+    all_values.sort()
     min_diff: int | None = None
-    for x in range(len(all_values) - 1):
-        for y in range(x + 1, len(all_values)):
-            diff: int = abs(all_values[x] - all_values[y])
-            if min_diff:
-                min_diff = min(diff, min_diff)
-                continue
-            min_diff = diff
+    for x in range(1, len(all_values)):
+        diff: int = abs(all_values[x] - all_values[x - 1])
+        if min_diff:
+            min_diff = min(diff, min_diff)
+            continue
+        min_diff = diff
     return min_diff
 
 
+# Time complexity: O(n * (log n)) -> traversing whole BT once to store all node values from it => O(n) ->
+# n - num of nodes in BT^^|  -> sorting this values after => O(n * (log n)) -> traversing sorted all_values
+#                            to get minimal difference between values inside => O(n) -> O(n * (log n)).
+# Auxiliary space: O(n) -> using recursion for every node in BT, stack will be a size of summ of every node => O(n) ->
+#                          -> storing every value from these nodes in a list => O(n) -> O(2n) => O(n).
+# ---------------
+# Still fine to fail at this rate, because it's a first time dealing with BinaryTress, because I skipped them before.
+# But didn't want to break daily_task. Totally need to return and complete all basic BT tasks, which I planned.
+# ---------------
+# Ok. It's all correct except for finding difference is too slow.
+# Which is why I wanted to see 189/189 test_case, one that gives TLE,
+# but they don't give it and I assumed that it's None.
+# Because of that I was focused on different problem, and didn't see that it's just slow diff_calc.
+# Which can be faster just checking values after sorting, because ever value will be checked with their
+# most lowest neighbor.
+# [5, 3, 2, 1, 4] -> sorted[1, 2, 3, 4, 5] -> every x + 1 is most_lowest check we can find for x.
+# ---------------
 # Failed first_commit, because didn't understand task correctly, and I was checking
 # diff between every 2 connected nodes, which was harder and I made it work...
 # Failed second_commit, no idea why, TimeLimit without actual test_case given ->
@@ -46,4 +64,4 @@ def get_minimum_diff(root: TreeNode) -> int:
 # ---------------
 # Not creating TreeNodes to test, and not using tests in this case.
 # Because I know how to search BTS, but if I made creation of BTS it's the same as reading it,
-# and I can't do this today. So just reading for now.
+# and I can't do this today (no time). So just reading for now.
