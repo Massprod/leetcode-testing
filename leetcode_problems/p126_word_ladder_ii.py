f468940cdb1c4ee32c6c028abba7828d72b21de9
Pampam <piankovpe@gmail.com>
2023-06-19 23:56:21 +0600

    tests

--- leetcode_problems/p126_word_ladder_ii.py
+++ leetcode_problems/p126_word_ladder_ii.py
@@ -19,23 +19,44 @@
 
 
 def find_ladders(beginWord: str, endWord: str, wordList: list[str]) -> list[list[str]]:
-    word_list: set[str] = set(wordList)
     if endWord not in wordList:
         return []
+    pairs: dict[str, set[str]] = {}
 
     def single_diff(word1: str, word2: str) -> bool:
+        if word1 in pairs:
+            if word2 in pairs[word1]:
+                return True
+        if word2 in pairs:
+            if word1 in pairs[word2]:
+                return True
         count: int = 0
-        for x in range(len(word1)):
+        for z in range(len(word1)):
             if count > 1:
                 return False
-            elif word1[x] != word2[x]:
+            elif word1[z] != word2[z]:
                 count += 1
         if count == 1:
+            if word1 not in pairs:
+                pairs[word1] = {word2}
+            else:
+                pairs[word1].add(word2)
+            if word2 not in pairs:
+                pairs[word2] = {word1}
+            else:
+                pairs[word2].add(word1)
             return True
         return False
 
     def ladder_search(path: list[str]) -> None | bool:
         if min_length[-1] - 1 < len(path):
+            path.append(endWord)
+            for g in range(1, len(path) - 1):
+                if path[g] not in correct_parts:
+                    correct_parts[path[g]] = [path[g + 1:]]
+                else:
+                    correct_parts[path[g]].append(path[g + 1:])
+            path.pop()
             return False
         if single_diff(path[-1], endWord):
             path.append(endWord)
@@ -45,7 +66,7 @@ def find_ladders(beginWord: str, endWord: str, wordList: list[str]) -> list[list
                 copy: list[str] = path.copy()
                 correct_paths.clear()
                 correct_paths.append(copy)
-                for g in range(len(path)):
+                for g in range(1, len(path) - 1):
                     if path[g] not in correct_parts:
                         correct_parts[path[g]] = [path[g + 1:]]
                     else:
@@ -59,14 +80,23 @@ def find_ladders(beginWord: str, endWord: str, wordList: list[str]) -> list[list
             elif min_length[-1] < path_length:
                 path.pop()
                 return
-        for word in word_list:
-            if (word not in used) and single_diff(path[-1], word):
+        for y in range(len(wordList)):
+            word: str = wordList[y]
+            if (word != endWord) and (word not in used) and single_diff(path[-1], word):
+                if word in correct_parts:
+                    for _ in correct_parts[word]:
+                        correct_path2: list[str] = path + [word] + _
+                        correct_length2: int = len(correct_path2)
+                        if correct_length2 < min_length[-1]:
+                            min_length[-1] = len(correct_path2)
+                            correct_paths.clear()
+                            correct_paths.append(correct_path2)
+                        elif correct_length2 == min_length[-1]:
+                            correct_paths.append(correct_path2)
+                    continue
                 used.add(word)
                 path.append(word)
-                if ladder_search(path) is False:
-                    path.pop()
-                    used.remove(word)
-                    return
+                ladder_search(path)
                 path.pop()
                 used.remove(word)
 
@@ -74,10 +104,11 @@ def find_ladders(beginWord: str, endWord: str, wordList: list[str]) -> list[list
         return [[beginWord, endWord]]
     correct_parts: dict[str, list[list[str]]] = {}
     used: set[str] = set()
-    min_length: list[int] = [len(word_list)]
+    min_length: list[int] = [len(wordList)]
     correct_paths: list[list[str]] = []
-    for check in word_list:
-        if single_diff(beginWord, check):
+    for x in range(len(wordList)):
+        check: str = wordList[x]
+        if (check != endWord) and single_diff(beginWord, check):
             if check in correct_parts:
                 for _ in correct_parts[check]:
                     correct_path: list[str] = [beginWord, check] + _
@@ -96,6 +127,12 @@ def find_ladders(beginWord: str, endWord: str, wordList: list[str]) -> list[list
     return correct_paths
 
 
+# Don't see what im missing, but I need to cull recursion checks, but how?
+# -------------------
+# Added remembering of already travelled paths, so now if we're going to check first word in
+# sequence, and it's already been used in recursion, we will reuse built paths.
+# But it's not enough and I need to cull recursion calls.
+# -------------------
 # Working but hits TimeLimit.
 # -------------------
 # Rushed commit and forgot about part with different lengths, if we meet something smaller
@@ -125,6 +162,7 @@ test2 = ["hot", "dot", "dog", "lot", "log"]
 test2_begin = "hit"
 test2_end = "cog"
 test2_out = []
+print(find_ladders(test2_begin, test2_end, test2))
 
 test3 = ["hot", "dot", "dog", "lot", "log", "cog", "mog"]
 test3_begin = "hit"
@@ -147,3 +185,39 @@ test5_begin = "leet"
 test5_end = "code"
 test5_out = [["leet", "lest", "lost", "lose", "lode", "code"]]
 print(find_ladders(test5_begin, test5_end, test5))
+
+test6 = ["kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs",
+         "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan",
+         "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui",
+         "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led",
+         "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply",
+         "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac",
+         "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob",
+         "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag",
+         "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe",
+         "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic",
+         "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod",
+         "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere",
+         "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and",
+         "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan",
+         "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao",
+         "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum",
+         "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw",
+         "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate",
+         "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex",
+         "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem",
+         "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin",
+         "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye",
+         "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug",
+         "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee",
+         "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio",
+         "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc",
+         "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton",
+         "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox",
+         "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con",
+         "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot",
+         "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie",
+         "aim"]
+test6_begin = "cet"
+test6_end = "ism"
+print(find_ladders(test6_begin, test6_end, test6))
