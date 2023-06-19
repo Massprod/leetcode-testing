a4bf60b11df3dc90d6661545917e4396226dc031
Pampam <piankovpe@gmail.com>
2023-06-19 13:37:50 +0600

    add working_sol (96.45%, 26.6%) -> (36ms, 16.3mb)  time: O(n) | space: O(1)

--- leetcode_problems/p1732_find_the_highest_altitude.py
+++ leetcode_problems/p1732_find_the_highest_altitude.py
@@ -9,11 +9,26 @@
 
 
 def largest_altitude(gain: list[int]) -> int:
-    pass
+    # working_sol (96.45%, 26.6%) -> (36ms, 16.3mb)  time: O(n) | space: O(1)
+    altitude: int = 0
+    max_altitude: int = 0
+    for _ in gain:
+        altitude += _
+        max_altitude = max(altitude, max_altitude)
+    return max_altitude
+
+
+# Time complexity: O(n) -> traversing whole input array once => O(n).
+# n - len of input_list^^|
+# Auxiliary space: O(1) -> 2 constant INTs both doesn't depend on input => O(1)
 
 
 test1 = [-5, 1, 5, 0, -7]
 test1_out = 1
+print(largest_altitude(test1))
+assert test1_out == largest_altitude(test1)
 
 test2 = [-4, -3, -2, -1, 4, 3, 2]
 test2_out = 0
+print(largest_altitude(test2))
+assert test2_out == largest_altitude(test2)
