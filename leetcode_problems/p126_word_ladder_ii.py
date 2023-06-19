3b7893653d8eb590e288dc57c2372fa443f7caec
Pampam <piankovpe@gmail.com>
2023-06-19 21:12:06 +0600

    pre_commit

--- leetcode_problems/p126_word_ladder_ii.py
+++ leetcode_problems/p126_word_ladder_ii.py
@@ -19,15 +19,91 @@
 
 
 def find_ladders(beginWord: str, endWord: str, wordList: list[str]) -> list[list[str]]:
-    pass
+    if endWord not in wordList:
+        return []
+
+    def single_diff(word1: str, word2: str) -> bool:
+        count: int = 0
+        for _ in word1:
+            if count > 1:
+                return False
+            elif _ not in word2:
+                count += 1
+        if count == 1:
+            return True
+        return False
+
+    def ladder_search(start_index: int, path: list[str]) -> None:
+        if min_length[-1] - 1 < len(path):
+            return
+        if single_diff(path[-1], endWord):
+            path.append(endWord)
+            path_length: int = len(path)
+            if min_length[-1] > path_length:
+                min_length[-1] = path_length
+                correct_paths.clear()
+                correct_paths.append(path.copy())
+                path.pop()
+                return
+            elif min_length[-1] == path_length:
+                correct_paths.append(path.copy())
+                path.pop()
+                return
+            elif min_length[-1] < path_length:
+                path.pop()
+                return
+        for x in range(start_index, len(wordList)):
+            word: str = wordList[x]
+            if single_diff(path[-1], word):
+                path.append(word)
+                ladder_search(x + 1, path)
+                path.pop()
+
+    if single_diff(beginWord, endWord):
+        return [[beginWord, endWord]]
+    min_length: list[int] = [len(wordList)]
+    correct_paths: list[list[str]] = []
+    for index in range(len(wordList)):
+        word_to_check: str = wordList[index]
+        if single_diff(beginWord, word_to_check):
+            ladder_search(index + 1, [beginWord, word_to_check])
+    return correct_paths
+
+
+# Rushed commit and forgot about part with different lengths, if we meet something smaller
+# we should fully clear correct_paths.
+# -------------------
+# What about case when beginWord is 1 letter changed endWord?
+# can we just return it => [beginWord, endWord]
+# Yeah, tested this case, it's correct assumption.
+# -------------------
+# No info on what position endWord will have, so I assume that it can be any position.
+# So we obliged to check every possible index to find this and build ladder using any other words.
+# We can ignore any starting words we already checked.
+# Like in test1 -> started with HOT there's ro reasons to check DOR + HOT after HOT ->
+# -> so it's only left to right walk to find ladder, everything we already started from can be ignored.
+# -------------------
+# No idea about TimeLimit, but I can solve it with recursion.
+# If I hit TLE than rebuild, but let's try recursion first.
 
 
 test1 = ["hot", "dot", "dog", "lot", "log", "cog"]
 test1_begin = "hit"
 test1_end = "cog"
 test1_out = [["hit", "hot", "dot", "dog", "cog"], ["hit", "hot", "lot", "log", "cog"]]
+print(find_ladders(test1_begin, test1_end, test1))
 
 test2 = ["hot", "dot", "dog", "lot", "log"]
 test2_begin = "hit"
 test2_end = "cog"
 test2_out = []
+
+test3 = ["hot", "dot", "dog", "lot", "log", "cog", "mog"]
+test3_begin = "hit"
+test3_end = "cog"
+test3_out = [["hit", "hot", "dot", "dog", "cog"], ["hit", "hot", "lot", "log", "cog"]]
+print(find_ladders(test3_begin, test3_end, test3))
+
+test4 = ["ted","tex","red","tax","tad","den","rex","pee"]
+test4_begin = "red"
+test4_end = "tax"
