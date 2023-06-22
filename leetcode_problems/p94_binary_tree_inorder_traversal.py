a88b7f0fb38832391ce445826a52c2e97f5826ea
Pampam <piankovpe@gmail.com>
2023-06-22 11:52:40 +0600

    add working_sol (74.53%,  70.47%) -> (43ms, 16.2mb)  time: O(n) | space: O(n)

--- leetcode_problems/p94_binary_tree_inorder_traversal.py
+++ leetcode_problems/p94_binary_tree_inorder_traversal.py
@@ -13,14 +13,30 @@ class TreeNode:
 
 
 def inorder_traversal(root: TreeNode) -> list[int]:
-    pass
+    # working_sol (74.53%,  70.47%) -> (43ms, 16.2mb)  time: O(n) | space: O(n)
+    if not root:
+        return root
+    inorder_traver: list[int] = []
+
+    def inorder(node: TreeNode) -> None:
+        if node.left is None:
+            inorder_traver.append(node.val)
+        if node.left:
+            inorder(node.left)
+            inorder_traver.append(node.val)
+        if node.right:
+            inorder(node.right)
+
+    inorder(root)
+    return inorder_traver
 
 
-test1 = [1, None, 2, 3]
-test1_out = [1, 3, 2]
-
-test2 = []
-test2_out = []
-
-test3 = [1]
-test3_out = []
+# Time complexity: O(n) -> traversing whole BT(binary tree) only once => O(n)
+# n - number of nodes in input_BT^^|
+# Auxiliary space: O(n) -> storing every node.val from original BT into list of these values with size of n => O(n)
+# ------------------
+# Next task is to recreate binary Tree, and then I will build it and made test function.
+# In this one I will just traverse InOrder.
+# !
+# An inorder traversal first visits the left child (including its entire subtree),
+# then visits the node, and finally visits the right child (including its entire subtree). !
