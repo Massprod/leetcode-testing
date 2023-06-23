b7ef52ba49bd282735a670f5082ad910a306770e
Pampam <piankovpe@gmail.com>
2023-06-23 14:42:00 +0600

    initial

---
+++ leetcode_problems/p1027_longest_arithmetic_subsequence.py
@@ -0,0 +1,22 @@
+# Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
+# Note that:
+#   A subsequence is an array that can be derived from another array by deleting some or no elements
+#       without changing the order of the remaining elements.
+#   A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+# ----------------------------
+# 2 <= nums.length <= 1000
+# 0 <= nums[i] <= 500
+
+
+def longest_arith_seq_length(nums: list[int]) -> int:
+    pass
+
+
+test1 = [3, 6, 9, 12]
+test1_out = 4
+
+test2 = [9, 4, 7, 2, 10]
+test2_out = 3
+
+test3 = [20, 1, 15, 3, 10, 5, 8]
+test3_out = 4
