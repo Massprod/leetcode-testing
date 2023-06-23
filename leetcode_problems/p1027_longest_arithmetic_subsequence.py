8faee342e453b1e138ad8600bba09f8281594c8a
Pampam <piankovpe@gmail.com>
2023-06-23 14:52:18 +0600

    pre commit

--- leetcode_problems/p1027_longest_arithmetic_subsequence.py
+++ leetcode_problems/p1027_longest_arithmetic_subsequence.py
@@ -9,14 +9,42 @@
 
 
 def longest_arith_seq_length(nums: list[int]) -> int:
-    pass
+    all_options: dict[int, list[int]] = {}
+    for x in range(1, len(nums)):
+        for y in range(0, x):
+            option: int = nums[x] - nums[y]
+            if option not in all_options:
+                all_options[option] = [nums[y], nums[x]]
+            else:
+                all_options[option].append(nums[x])
+    max_length: int = 0
+    for value in all_options.values():
+        max_length = max(max_length, len(value))
+    return max_length
+
+
+# Only question is, do I need to take absolute? Because absolute will be a distance, and we need this rule ->
+# -> A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+# Well what about cases like: 20 - 3 = 17 and 3 - 20 = -17, different sequences and with absolute values
+# they will be the same. Yeah, no need to use absolute values here.
+# ----------------------------
+# No idea how to make this fast, but I know how to make this in O(n * n) ->
+# -> just store every possible diff in dict and append them one by one.
+# No hints given and I have no idea about TimeLimit. Let's try to fail commit.
 
 
 test1 = [3, 6, 9, 12]
 test1_out = 4
+print(longest_arith_seq_length(test1))
 
 test2 = [9, 4, 7, 2, 10]
 test2_out = 3
+print(longest_arith_seq_length(test2))
 
 test3 = [20, 1, 15, 3, 10, 5, 8]
 test3_out = 4
+print(longest_arith_seq_length(test3))
+
+test4 = [1, 2]
+test4_out = 2
+print(longest_arith_seq_length(test4))
