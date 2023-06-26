319a151d6b94341720e26543ffdbe27b91c2df7e
Pampam <piankovpe@gmail.com>
2023-06-26 22:23:59 +0600

    pre commit

--- leetcode_problems/p2462_total_cost_to_hire_k_workers.py
+++ leetcode_problems/p2462_total_cost_to_hire_k_workers.py
@@ -16,23 +16,81 @@
 # 1 <= costs.length <= 10 ** 5
 # 1 <= costs[i] <= 10 ** 5
 # 1 <= k, candidates <= costs.length
+import heapq
 
 
 def total_cost(costs: list[int], k: int, candidates: int) -> int:
-    pass
+    left_index: int = 0
+    right_index: int = len(costs) - 1
+    left_list: heapq = []
+    heapq.heapify(left_list)
+    right_list: heapq = []
+    heapq.heapify(right_list)
+    cost_to_hire: int = 0
+    if len(costs) // 2 >= candidates:
+        while len(left_list) != candidates:
+            heapq.heappush(left_list, costs[left_index])
+            heapq.heappush(right_list, costs[right_index])
+            left_index += 1
+            right_index -= 1
+    else:
+        while len(left_list) != len(costs):
+            heapq.heappush(left_list, costs[left_index])
+            left_index += 1
+        while k:
+            cost_to_hire += heapq.heappop(left_list)
+            k -= 1
+        return cost_to_hire
+    while k and left_index <= right_index:
+        left_candi: int = left_list[0]
+        right_candi: int = right_list[0]
+        if left_candi < right_candi:
+            cost: int = heapq.heappop(left_list)
+            cost_to_hire += cost
+            k -= 1
+        elif right_candi < left_candi:
+            cost: int = heapq.heappop(right_list)
+            cost_to_hire += cost
+            k -= 1
+        elif left_candi == right_candi:
+            cost: int = heapq.heappop(left_list)
+            cost_to_hire += cost
+            k -= 1
+        if len(left_list) != candidates and left_index <= right_index:
+            heapq.heappush(left_list, costs[left_index])
+            left_index += 1
+        if len(right_list) != candidates and left_index <= right_index:
+            heapq.heappush(right_list, costs[right_index])
+            right_index -= 1
+    if k == 0:
+        return cost_to_hire
+    merged: iter = iter(heapq.merge(left_list, right_list))
+    while k:
+        cost_to_hire += next(merged)
+        k -= 1
+    return cost_to_hire
 
 
 test1 = [17, 12, 10, 2, 7, 2, 11, 20, 8]
 test1_k = 3
 test1_candidates = 4
 test1_out = 11
+print(total_cost(test1, test1_k, test1_candidates))
 
 test2 = [1, 2, 4, 1]
 test2_k = 3
 test2_candidates = 3
 test2_out = 4
+print(total_cost(test2, test2_k, test2_candidates))
 
 test3 = [7, 7, 7, 7, 7, 7, 7]
 test3_k = 7
 test3_candidates = 7
 test3_out = 49
+print(total_cost(test3, test3_k, test3_candidates))
+
+test4 = [1, 2, 2, 1]
+test4_k = 2
+test4_candidates = 2
+test4_out = 10
+print(total_cost(test4, test4_k, test4_candidates))
