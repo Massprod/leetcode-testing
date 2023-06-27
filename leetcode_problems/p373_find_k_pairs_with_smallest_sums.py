a3a325cc384b8e9b6ecf4d19b01c0d4a2b4c2480
Pampam <piankovpe@gmail.com>
2023-06-27 15:19:02 +0600

    initial

---
+++ leetcode_problems/p373_find_k_pairs_with_smallest_sums.py
@@ -0,0 +1,25 @@
+# You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+# Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+# Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+# ----------------
+# 1 <= nums1.length, nums2.length <= 10 ** 5
+# -10 ** 9 <= nums1[i], nums2[i] <= 10 ** 9
+# nums1 and nums2 both are sorted in ascending order.
+# 1 <= k <= 10 ** 4
+
+
+def k_smallest_pairs(nums1: list[int], nums2: list[int], k: int) -> list[list[int]]:
+    pass
+
+
+test1_1 = [1, 7, 11]
+test1_2 = [2, 4, 6]
+test1_k = 3
+
+test2_1 = [1, 1, 2]
+test2_2 = [1, 2, 3]
+test2_k = 2
+
+test3_1 = [1, 2]
+test3_2 = [3]
+test3_k = 3
