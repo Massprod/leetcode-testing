2c08201b5069c785a96808ad91ec4faaa343c6de
Pampam <piankovpe@gmail.com>
2023-07-03 17:33:14 +0600

    add working solution

--- leetcode_problems/p197_rising_temperature.py
+++ leetcode_problems/p197_rising_temperature.py
@@ -39,10 +39,24 @@ db.execute(
         "INSERT INTO weather(recordDate, temperature) "
         "VALUES "
         "('2015-01-01', 10),"
-        "('2015-01-03', 25),"
+        "('2015-01-02', 25),"
         "('2015-01-03', 20),"
         "('2015-01-04', 30);"
     )
 )
 db.commit()
 
+# Failed 2 times, because I was using PostgreSQL syntax and didn't know we need to use DATE_ADD() in MySQL.
+mysql_query: str = "SELECT e2.id " \
+                   "FROM weather AS e1 " \
+                   "JOIN weather AS e2 ON e1.recordDate = DATE_ADD(e2.recordDate, INTERVAL -1 day) " \
+                   "WHERE e2.temperature > e1.temperature;"
+
+# There's no DATE_ADD() in postgresql, but we can simply add or extract INTERVAL 'week', 'month' w.e we want.
+postgresql_query: str = "SELECT e2.id " \
+                        "FROM weather AS e1 " \
+                        "JOIN weather AS e2 ON e1.recordDate = e2.recordDate - INTERVAL '1 day' " \
+                        "WHERE e2.temperature > e1.temperature;"
+data: Result = db.execute(text(postgresql_query))
+for _ in data:
+    print(_)
