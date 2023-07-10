5397cd2fb17c0b1a672312270e1fb3db5649d290
Pampam <piankovpe@gmail.com>
2023-07-10 16:45:29 +0600

    initial

---
+++ leetcode_problems/p102_binary_tree_level_order_traversal.py
@@ -0,0 +1,18 @@
+# Given the root of a binary tree,
+#   return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+# ----------------
+# The number of nodes in the tree is in the range [0, 2000].
+# -1000 <= Node.val <= 1000
+
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def levelOrder(root: TreeNode) -> list[list[int]]:
+    pass
+
