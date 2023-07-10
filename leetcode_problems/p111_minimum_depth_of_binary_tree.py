2983b0bbe2532e592c09e9ec35e58a993c51b6bf
Pampam <piankovpe@gmail.com>
2023-07-10 14:28:01 +0600

    add working_sol (65.95%, 43.6%) -> (553ms, 57.2mb)  time: O(n) | space: O(n)

--- leetcode_problems/p111_minimum_depth_of_binary_tree.py
+++ leetcode_problems/p111_minimum_depth_of_binary_tree.py
@@ -15,8 +15,39 @@ class TreeNode:
 
 
 def minDepth(root: TreeNode) -> int:
-    pass
+    # working_sol (65.95%, 43.6%) -> (553ms, 57.2mb)  time: O(n) | space: O(n)
+    if not root:
+        return 0
+    min_depth: list[int | None] = [None]
+
+    def inorder(node: TreeNode, level: int = 0) -> None:
+        level += 1
+        if node.left is None and node.right is None:
+            if min_depth[0] is None:
+                min_depth[0] = level
+            else:
+                min_depth[0] = min(min_depth[0], level)
+            return
+        if node.left:
+            inorder(node.left, level)
+        if node.right:
+            inorder(node.right, level)
+
+    inorder(root)
+    return min_depth[0]
 
 
-
+# Time complexity: O(n) -> standard inorder traverse of the whole BT, once => O(n).
+# n - num of nodes in BT^^|
+# Auxiliary spae: O(n) -> using recursion, and for every call we're storing link_nodes in a stack,
+#                         we're not creating nodes, but still storing links to them, and they're depends on input_BT =>
+#                         => O(n).
+# ! Or we can call it O(1) <- because we're operating only with links to a nodes and one constant value min_depth[0] !
+# ------------------
+# What a joke. Literally tried to create EMPTY TREE with just [] used in test_case and didn't allow me.
+# And second test_case is empty tree as expected, and needs to return 0 not Null. Wow.
+# --------------------
 # Not building trees for this.
+# Only thing I don't understand is how to test case with length of tree 0.
+# If there's no Nodes this should return None, but maybe it's 0 depth?
+# Guess it's just a trap like always, fail to see.
