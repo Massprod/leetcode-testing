610e7770ca4d842d2da377d67a5fbc3d726d7d4d
Pampam <piankovpe@gmail.com>
2023-08-01 19:38:14 +0600

    add working_sol (39.07%, 97.59%) -> (410ms, 17.51mb)  time: O(n ** k) | space: O(nCk * k) | Revisit

--- leetcode_problems/p77_combinations.py
+++ leetcode_problems/p77_combinations.py
@@ -3,28 +3,35 @@
 # You may return the answer in any order.
 
 
-def combine(n: int, k: int) -> list[list[int]] | set:
-    # working_sol (8.43%, 6.27%) -> (607ms, 18.5mb)  time: O(n ** k) | space: O(nCk * k)
+def combine(n: int, k: int) -> set:
+    # working_sol (39.07%, 97.59%) -> (410ms, 17.51mb)  time: O(n ** k) | space: O(nCk * k)
     k_numbers: set = set()
+    # Temporary storage for combination.
     tempo: list[int] = []
 
-    def new_combine(start: int = 1, end: int = n + 1, left_to_use: int = k):
+    def new_combine(start: int = 1, end: int = n + 1, left_to_use: int = k) -> None:
         if left_to_use == 0:
             k_numbers.add(tuple(sorted(tempo)))
             return
+        # Creating combinations, by taking values from start -> end,
+        # one by one until we hit usage limit == k.
         for num in range(start, end):
+            # We don't need duplicates, so there's no reason
+            # to recall same values.
             if num in tempo:
                 break
             tempo.append(num)
             new_combine(start + 1, end, left_to_use - 1)
             tempo.pop()
+
     new_combine()
     return k_numbers
 
-# Time complexity: O(n ** k) -> recursion tree with n branches and k depths => O(n ** k)
-# Space complexity: O((n! / k! * (n-k)!) * k) or O(nCk * k) -> storing all combinations => O(nCk) ->
+
+# Time complexity: O(n ** k) -> recursion tree with n branches and k depths => O(n ** k).
+# Auxiliary space: O((n! / k! * (n-k)!) * k) or O(nCk * k) -> storing all combinations => O(nCk) ->
 #                   and size of combinations is k, for every combination we create tempo_list of k size ->
-#                   -> leaving us with a list or set with size of => O(nCk * k)
+#                   -> leaving us with a list or set with size of => O(nCk * k).
 #   !
 #   The number of combinations of n distinct objects, taken r at a time is:
 #                          nCr = n! / r! (n - r)!
@@ -41,8 +48,8 @@ test1_n = 4
 test1_k = 2
 test1_out = [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 test = list(combine(test1_n, test1_k))
-print(test)
 for _ in test1_out:
+    assert len(test1_out) == len(test)
     assert tuple(_) in test
 del test
 
@@ -50,8 +57,8 @@ test2_n = 1
 test2_k = 1
 test2_out = [[1]]
 test = list(combine(test2_n, test2_k))
-print(test)
 for _ in test2_out:
+    assert len(test2_out) == len(test)
     assert tuple(_) in test
 del test
 
@@ -62,8 +69,8 @@ test3_out = [
     [1, 4, 5], [2, 3, 4], [2, 3, 5], [2, 4, 5], [3, 4, 5],
 ]
 test = list(combine(test3_n, test3_k))
-print(test)
 for _ in test3_out:
+    assert len(test3_out) == len(test)
     assert tuple(_) in test
 del test
 
