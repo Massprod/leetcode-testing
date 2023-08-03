ee0618bc5b7ac3cbe0a7d49ba86ff4e87d1c5658
Pampam <piankovpe@gmail.com>
2023-08-03 21:57:00 +0600

    add working_sol (98.78%, 98.42%) -> (31ms, 16.1mb)  time: O(n ** k) | space: O(n ** k) | Revisit

--- leetcode_problems/p17_letter_combs_of_phone_number.py
+++ leetcode_problems/p17_letter_combs_of_phone_number.py
@@ -1,55 +1,20 @@
-# Given a string containing digits from 2-9 inclusive, return all possible letter combinations
-# that the number could represent. Return the answer in any order.
-#
+# Given a string containing digits from 2-9 inclusive,
+#   return all possible letter combinations that the number could represent.
+# Return the answer in any order.
 # A mapping of digits to letters (just like on the telephone buttons) is given below.
 # Note that 1 does not map to any letters.
-from itertools import combinations
-from numpy import matrix as mt
+# ---------------------
+# 0 <= digits.length <= 4
+# digits[i] is a digit in the range ['2', '9'].
 
 
-def total_combs(digits: str) -> list[str]:
-    # working_sol (23.18%, 19.89%) - brute is no Good.
-    # if len(digits) == 0:
-    #     return []
-    # options = {
-    #     "2": ["a", "b", "c"],
-    #     "3": ["d", "e", "f"],
-    #     "4": ["g", "h", "i"],
-    #     "5": ["j", "k", "l"],
-    #     "6": ["m", "n", "o"],
-    #     "7": ["p", "q", "r", "s"],
-    #     "8": ["t", "u", "v"],
-    #     "9": ["w", "x", "y", "z"],
-    # }
-    # if len(digits) == 1:
-    #     return options[digits[0]]
-    # combs = []
-    # to_use = []
-    # for digit in digits:
-    #     to_use.append(options[digit])
-    # for x in range(len(to_use[0])):
-    #     first = to_use[0][x]
-    #     for y in range(len(to_use[1])):
-    #         second = to_use[1][y]
-    #         if len(to_use) == 2:
-    #             to_add = first + second
-    #             combs.append(to_add)
-    #             continue
-    #         for z in range(len(to_use[2])):
-    #             third = to_use[2][z]
-    #             if len(to_use) == 3:
-    #                 to_add = first + second + third
-    #                 combs.append(to_add)
-    #                 continue
-    #             for g in range(len(to_use[3])):
-    #                 fourth = to_use[3][g]
-    #                 to_add = first + second + third + fourth
-    #                 combs.append(to_add)
-    # return combs
-    # working sol - googled (41.43% , 98.2%)
+def letter_combinations(digits: str) -> list[str]:
+    # working_sol (98.78%, 98.42%) -> (31ms, 16.1mb)  time: O(n ** k) | space: O(n ** k)
+    # Unique case with empty.
     if len(digits) == 0:
         return []
-    options = {
+    # All digits symbol options.
+    options: dict[str, list[str]] = {
         "2": ["a", "b", "c"],
         "3": ["d", "e", "f"],
         "4": ["g", "h", "i"],
@@ -59,49 +24,42 @@ def total_combs(digits: str) -> list[str]:
         "8": ["t", "u", "v"],
         "9": ["w", "x", "y", "z"],
     }
+    # Combining backwards, so we need to take Base first ->
+    # -> Last digit is a Base with who everything will be combined.
     if len(digits) == 1:
         return options[digits[0]]
-    added = total_combs(digits[:-1])  # recalling functions until we hit len == 1 to get our first option
-    to_add = options[digits[-1]]  # adding our first option to last digit in called recursion DIGITS
-    combs = [a + b for a in added for b in to_add]  # combining them into one list
-    return combs
+    # Every digit symbol can be used with every other digit symbol ->
+    # -> "77" -> "pp", "pq", "pr" etc.
+    # Duplicates are allowed, so we're just taking every symbol of last digit
+    # and building every combination with previous digits backwards.
+    already_added: list[str] = letter_combinations(digits[:-1])
+    new_options: list[str] = options[digits[-1]]
+    all_combs: list[str] = [alr + new for alr in already_added for new in new_options]
+    return all_combs
 
 
+# Time complexity: O(n ** k) -> basically in the worst case we're having 4 nested loops with 3 or 4 size ->
+# n -> longest of input_digit options^^| -> so it's something like n1 * n2 * n3 * n4 where n == len(options[digit]) ->
+# k -> len of input_digits^^| -> like "7777" we're just getting n ** 4, it's correct, but I don't know can we
+#                             just use n as longest, cuz it can be 3 or 4 -> w.e O() is worst case anyway =>
+#                             => O(n ** k).
+# Auxiliary space: O(n ** k) -> we're storing every combination and all combinations of 2 arrays is X * Y,
+#                               X <- elements of first array, Y <- elements of second array ->
+#                               -> we're combining all digits, and their equal to k => so it's k arrays with n size =>
+#                               => O(n ** k).
+# ---------------------
 # For the future - always go to recursion if there's 2+ for loops needed
 # Made only 2 tasks with recursion and failed to see it here.....
 
 
+test: str = "23"
+test_out: list[str] = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+assert test_out == letter_combinations(test)
 
-test1 = "23"
-test1_out = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-print(total_combs(test1))
-assert test1_out == total_combs(test1)
-# test2 = "23456789"
-# print(total_combs(test2))
-# combb = combinations(test2, r=2)
-# test3 = set()
-# for _ in combb:
-#     strs = _[0] + _[1]
-#     test3.add(strs)
-# for _ in test3:
-#     assert _ in total_combs(test2)
+test = ""
+test_out = []
+assert test_out == letter_combinations(test)
 
-# test3 = ""
-# print(total_combs(test3))
-# test4 = "2"
-# print(total_combs(test4))
-# test5 = "6"
-# print(total_combs(test5))
-# failed test5, cuz made a typo in dict m, m, o -> m, n, o  .......................nc
-test6 = "22"
-print(total_combs(test6))
-# failed tes6, cuz im using SET and instead of using sorted() on str I was brute checking more or less and forgot equal
-test7 = "234"
-test7_out = ["adg","adh","adi","aeg","aeh","aei","afg","afh","afi","bdg","bdh","bdi","beg","beh","bei","bfg","bfh","bfi","cdg","cdh","cdi","ceg","ceh","cei","cfg","cfh","cfi"]
-print(total_combs(test7))
-assert test7_out == total_combs(test7)
-# task limiting numbers from 0-4, so I can make brute options for 2, 3, 4 or ????
-# try to commit and not hit TLE with brute at least for 1 time
-# Brute is working 23.18% , 19.89%. But it's only for 0-4  it's a bad solution....
-test8 = "954"
-print(total_combs(test8))
+test = "2"
+test_out = ["a", "b", "c"]
+assert test_out == letter_combinations(test)
