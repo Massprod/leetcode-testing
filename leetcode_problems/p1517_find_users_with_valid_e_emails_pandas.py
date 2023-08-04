f71276da58e35a07a0730bad5467456eae2f73c0
Pampam <piankovpe@gmail.com>
2023-08-05 00:01:15 +0600

    add working solution with pandas

---
+++ leetcode_problems/p1517_find_users_with_valid_e_emails_pandas.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+# Write a solution to find the users who have valid emails.
+# A valid e-mail has a prefix name and a domain where:
+#   The prefix name is a string that may contain letters (upper or lower case),
+#     digits, underscore '_', period '.', and/or dash '-'.
+#   The prefix name must start with a letter.
+#   The domain is '@leetcode.com'.
+# Return the result table in any order.
+# +---------+-----------+-------------------------+
+# | user_id | name      | mail                    |
+# +---------+-----------+-------------------------+
+# | 1       | Winston   | winston@leetcode.com    |
+# | 2       | Jonathan  | jonathanisgreat         |
+# | 3       | Annabelle | bella-@leetcode.com     |
+# | 4       | Sally     | sally.come@leetcode.com |
+# | 5       | Marwan    | quarz#2020@leetcode.com |
+# | 6       | David     | david69@gmail.com       |
+# | 7       | Shapiro   | .shapo@leetcode.com     |
+# +---------+-----------+-------------------------+
+
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    users = users[users['mail'].str.match(pat='^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$')]
+    return users
