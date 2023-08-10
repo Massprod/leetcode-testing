5c285bad1a0b9c17a88b473edde2c0334f00db13
Pampam <piankovpe@gmail.com>
2023-08-10 17:28:45 +0600

    add working_sol (98.26%, 77.85%) -> (49ms, 16.8mb)  time: O(n) | space: O(1)

--- leetcode_problems/p81_search_in_rotated_sorted_array_ii.py
+++ leetcode_problems/p81_search_in_rotated_sorted_array_ii.py
@@ -1,57 +1,67 @@
 # There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
-#
 # Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length)
-# such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+#   such that the resulting array is [nums[k], nums[k+1],...,nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
 # For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
-#
 # Given the array nums after the rotation and an integer target,
-# return true if target is in nums, or false if it is not in nums.
+#   return true if target is in nums, or false if it is not in nums.
 #
 # You must decrease the overall operation steps as much as possible.
 # ---------------------------
-# 1 <= nums.length <= 5000  ,  -104 <= nums[i] <= 104  ,  -104 <= target <= 104
+# 1 <= nums.length <= 5000  ,  -10 ** 4 <= nums[i] <= 10 ** 4  ,  -10 ** 4 <= target <= 10 ** 4
 # nums is guaranteed to be rotated at some pivot.
 
 
 def search(nums: list[int], target: int) -> bool:
-    # working_sol (48.1%, 11.68%) -> (60ms, 16.9mb)  time: O(n) | space: O(1)
-    if len(nums) == 0:
-        return False
-    elif len(nums) == 1:
-        if nums[0] == target:
-            return True
-        else:
-            return False
-    # if rotated
-    elif nums[-1] <= nums[0]:
+    # working_sol (98.26%, 77.85%) -> (49ms, 16.8mb)  time: O(n) | space: O(1)
+    # If rotated.
+    if nums[-1] <= nums[0]:
+        # Not present.
         if nums[-1] < target < nums[0]:
             return False
+        # In the left_part.
         elif target >= nums[0]:
             for x in range(len(nums)):
                 if nums[x] == target:
                     return True
+                # Everything else, is going to be higher.
+                elif nums[x] > target:
+                    return False
+                # Breakpoint => start of the right_part.
+                # Only lower values will be presented.
                 elif nums[x] < nums[x - 1] and x > 0:
                     return False
+        # In the right_part.
         elif target <= nums[-1]:
             for x in range(len(nums) - 1, -1, -1):
                 if nums[x] == target:
                     return True
+                # Everything else, is going to be lower.
+                elif nums[x] < target:
+                    return False
+                # Breakpoint => end of the left_part.
+                # Only higher values will be presented.
                 elif nums[x] < nums[x - 1]:
                     return False
-    # if not rotated
+    # If not rotated.
+    # Higher than maximum, can't be present.
     elif target > nums[-1]:
         return False
+    # Lower than lowest, can't be present.
     elif target < nums[0]:
         return False
+    # Possible in range(min, max).
     elif nums[0] <= target <= nums[-1]:
         for x in range(len(nums)):
             if nums[x] == target:
                 return True
+            # Ascending order by default.
+            if nums[x] > target:
+                return False
     return False
 
 
 # Time complexity: O(n) -> without duplicates, it was O(log n) -> because we always could break at some part of array,
-# n -> length of input^^   now we can't break that easily and in the worst case going to check whole array => O(n)
+# n -> length of input^^|  now we can't break that easily and in the worst case going to check whole array => O(n)
 #                          ! case == [1, 1, 1, 1, 1, 1, 1, 1, 1] -> any part of array we're starting, we can't stop
 #                            until we reach lower or higher values, or simply out of indexes. !
 # Space complexity: O(1) -> no extra space.
@@ -76,28 +86,28 @@ def search(nums: list[int], target: int) -> bool:
 #   at least in this case.^^
 
 
-test1 = [2, 5, 6, 0, 0, 1, 2]
-test1_target = 0
-test1_out = True
-print(search(test1, test1_target))
+test: list[int] = [2, 5, 6, 0, 0, 1, 2]
+test_target: int = 0
+test_out: bool = True
+assert test_out == search(test, test_target)
 
-test2 = [2, 5, 6, 0, 0, 1, 2]
-test2_target = 3
-test2_out = False
-print(search(test2, test2_target))
+test = [2, 5, 6, 0, 0, 1, 2]
+test_target = 3
+test_out = False
+assert test_out == search(test, test_target)
 
-test3 = [1, 1, 1, 1, 1, 1, 1, 1, 0]
-test3_target = 2
-test3_out = False
-print(search(test3, test3_target))
+test = [1, 1, 1, 1, 1, 1, 1, 1, 0]
+test_target = 2
+test_out = False
+assert test_out == search(test, test_target)
 
 # test4 - failed -> failed to consider we can have not rotated list, but with same values on start and end,
 #                   main reason for that I didn't rebuild p33, and just made it work for same start_end values.
 #                   Problem with that, we had only uniques, so there was no way to encounter duplicate.
 #                   [1, 1] -> won't be possible and index[x + 1] will always be presented, if it's rotated.
-#                   Btw, bad practice to used for loops in [x + 1] way, but in this case, it's ok,
+#                   Btw, bad practice to use for loops in [x + 1] way, but in this case, it's ok,
 #                   but still can be changed to [x - 1] -> better to always do this.
-test4 = [1, 1]
-test4_target = 2
-test4_out = False
-print(search(test4, test4_target))
+test = [1, 1]
+test_target = 2
+test_out = False
+assert test_out == search(test, test_target)
