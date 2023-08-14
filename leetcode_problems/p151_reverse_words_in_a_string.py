ad8249b352a8d031dfbe1eefc821a31c77975397
Pampam <piankovpe@gmail.com>
2023-08-14 22:41:27 +0600

    add working_sol (74.89%, 84.85%) -> (42ms, 16.3mb)  time: O(n) | space: O(n) | Revisit

--- leetcode_problems/p151_reverse_words_in_a_string.py
+++ leetcode_problems/p151_reverse_words_in_a_string.py
@@ -1,10 +1,11 @@
 # Given an input string s, reverse the order of the words.
-#  A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+# A word is defined as a sequence of non-space characters.
+# The words in s will be separated by at least one space.
 # Return a string of the words in reverse order concatenated by a single space.
-#  Note that s may contain leading or trailing spaces or multiple spaces between two words.
-#  The returned string should only have a single space separating the words. Do not include any extra spaces.
+# Note that s may contain leading or trailing spaces or multiple spaces between two words.
+# The returned string should only have a single space separating the words. Do not include any extra spaces.
 # -------------------
-# 1 <= s.length <= 104
+# 1 <= s.length <= 10 ** 4
 # s contains English letters (upper-case and lower-case), digits, and spaces ' '.
 # There is at least one word in s.
 # -------------------
@@ -13,49 +14,46 @@
 
 
 def reverse_words_rebuild(s: str) -> str:
-    # working_sol (5.49%, 36.41%) -> (73ms, 16.5mb)  time: O(2n) | space: O(2n)
-    new_words: str = ""
-    new_word: str = ""
-    for x in range(len(s) - 1, -1, -1):
-        if (x != (len(s) - 1) and s[x] == " " and s[x + 1] != " ") or x == 0:
-            if x == 0 and s[0] != " ":
-                new_word += s[0]
-            if len(new_words) >= 1 and new_word != "":
-                new_words += " "
-            for y in range(len(new_word) - 1, -1, -1):
-                new_words += new_word[y]
-            new_word = ""
+    # working_sol (74.89%, 84.85%) -> (42ms, 16.3mb)  time: O(n) | space: O(n)
+    # Combine of all reverse words.
+    new_words: str = ''
+    # Words one by one.
+    new_word: str = ''
+    index: int = len(s) - 1
+    while index >= 0:
+        # Spaces added as extra and sliced after,
+        # so they can be ignored 24/7.
+        if s[index] == ' ':
+            index -= 1
             continue
-        elif x != 0 and s[x] != " " and s[x - 1] == " ":
-            new_word += f"{s[x]}"
-        elif s[x] != " ":
-            new_word += s[x]
-    return new_words
+        # Only correct words without spaces is needed.
+        while index >= 0 and s[index] != ' ':
+            new_word += s[index]
+            index -= 1
+        # Reverse and add correct words, with extra space ->
+        new_words += new_word[::-1] + ' '
+        new_word = ''
+    # -> slice extra space on last word.
+    return new_words[:-1]
 
 
-# Time complexity: O(2n) -> traversing once through whole input_string => O(n) ->
-# n - len of input_string^^ -> creating words along the way, on break point traversing this new_word
-#                              to reverse and append new_words(answer) => O(log n) <- on median, in the worst case ->
-#                           -> our whole input_string will be filled with only correct symbols => O(n) -> O(n) + O(n)
-# Space complexity: O(2n) -> same worst, case we're storing whole input_string into new_word
-#                            and reversing new_word into new_words(answer) => O(n) + O(n)
+# Time complexity: O(n) -> traversing once through whole input_string => O(n) -> reversing every word, and in the
+# n - len of input_string^^| worst case without empty spaces we will reverse actual input_string => O(2n) ->
+#                            -> extra slicing should be O(n) as well, so it's somewhat O(3n) => O(n) drop const anyway.
+# Space complexity: O(n) -> new_words with size of n, and new_word is always empty when we increment new_words => O(n).
 # -------------------
 # Basic rebuilding with O(2n) space is easy, so it's better to try O(1).
 # ! Strings are not mutable in Python. ! <- Sadly, we can't.
 
 
-test1 = "the sky is blue"
-print(test1)
-test1_out = "blue is sky the"
-print(reverse_words_rebuild(test1))
-assert test1_out == reverse_words_rebuild(test1)
+test: str = "the sky is blue"
+test_out: str = "blue is sky the"
+assert test_out == reverse_words_rebuild(test)
 
-test2 = "  hello world  "
-test2_out = "world hello"
-print(reverse_words_rebuild(test2))
-assert test2_out == reverse_words_rebuild(test2)
+test = "  hello world  "
+test_out = "world hello"
+assert test_out == reverse_words_rebuild(test)
 
-test3 = "a good   example"
-test3_out = "example good a"
-print(reverse_words_rebuild(test3))
-assert test3_out == reverse_words_rebuild(test3)
+test = "a good   example"
+test_out = "example good a"
+assert test_out == reverse_words_rebuild(test)
