7420fad669729f17342500bf5be316700ff82998
Pampam <piankovpe@gmail.com>
2023-08-15 17:58:28 +0600

    add working_sol(97.27%, 55.72%) -> (34ms, 16.38mb)  time: O(n) | space: O(n) | Revisit

--- leetcode_problems/p86_partition_list.py
+++ leetcode_problems/p86_partition_list.py
@@ -1,6 +1,5 @@
 # Given the head of a linked list and a value x,
-# partition it such that all nodes less than x come before nodes greater than or equal to x.
-#
+#   partition it such that all nodes less than x come before nodes greater than or equal to x.
 # You should preserve the original relative order of the nodes in each of the two partitions.
 # ------------------------
 # The number of nodes in the list is in the range [0, 200].
@@ -27,22 +26,28 @@ def create_linked(to_link: list[int]) -> ListNode:
 
 
 def partition(head: ListNode, x: int) -> ListNode:
-    # working_sol(38.83%, 13.38%) -> (44ms, 16.5mb)  time: O(n) | space: O(n)
+    # working_sol(97.27%, 55.72%) -> (34ms, 16.38mb)  time: O(n) | space: O(n)
+    # Store everything lower and higher than x.
     lower: list[int] = []
     higher: list[int] = []
+    # Empty list -> insta return.
     if not head:
         return head
     tempo: ListNode = head
     while tempo:
+        # Higher|Equal than x => right_side.
         if tempo.val >= x:
             higher.append(tempo.val)
+        # Lower than x => left_side.
         if tempo.val < x:
             lower.append(tempo.val)
         tempo = tempo.next
+    # Combine and recreate with correct order.
     whole: list[int] = lower + higher
     tempo = new_link = ListNode()
     for index in range(len(whole)):
         tempo.val = whole[index]
+        # Last index should always point to None.
         if index != (len(whole) - 1):
             tempo.next = ListNode()
             tempo = tempo.next
@@ -54,7 +59,6 @@ def partition(head: ListNode, x: int) -> ListNode:
 # Space complexity: O(n) -> creating lists with summary size of input linked_list ->
 #                           -> merging them together and getting list of input_n size => O(n)
 # n - number of values inside linked_list ^^
-
 # ---------------------------
 # Ok. I should have been considered incorrect input of None and make it return None.
 # I was correct with thinking this is through but, I didn't think about incorrect input of None.
@@ -75,47 +79,32 @@ def partition(head: ListNode, x: int) -> ListNode:
 # There's no word about doing this in_place, so I will try this one first.
 
 
-test1 = [1, 4, 3, 2, 5, 2]
-test1_x = 3
-test1_out = [1, 2, 2, 4, 3, 5]
-linked = create_linked(test1)
-test_tempo = test = partition(linked, test1_x)
-print(test)
-for _ in range(len(test1_out)):
-    assert test_tempo.val == test1_out[_]
+test: ListNode = create_linked([1, 4, 3, 2, 5, 2])
+test_x: int = 3
+test_out: list[int] = [1, 2, 2, 4, 3, 5]
+test_tempo: ListNode = partition(test, test_x)
+for _ in range(len(test_out)):
+    assert test_tempo.val == test_out[_]
     test_tempo = test_tempo.next
-    if _ == len(test1_out) == 0:
+    if _ == len(test_out) == 0:
         assert test.next is None
-del test
 
-test2 = [2, 1]
-test2_x = 2
-test2_out = [1, 2]
-linked = create_linked(test2)
-test_tempo = test = partition(linked, test2_x)
-print(test)
-for _ in range(len(test2_out)):
-    assert test_tempo.val == test2_out[_]
+test = create_linked([2, 1])
+test_x = 2
+test_out = [1, 2]
+test_tempo = partition(test, test_x)
+for _ in range(len(test_out)):
+    assert test_tempo.val == test_out[_]
     test_tempo = test_tempo.next
-    if _ == len(test2_out) == 0:
+    if _ == len(test_out) == 0:
         assert test.next is None
-del test
 
-test3 = [0]
-test3_x = 1
-test3_out = [0]
-linked = create_linked(test3)
-test_tempo = test = partition(linked, test3_x)
-print(test)
-for _ in range(len(test3_out)):
-    assert test_tempo.val == test3_out[_]
+test = create_linked([0])
+test_x = 1
+test_out = [0]
+test_tempo = partition(test, test_x)
+for _ in range(len(test_out)):
+    assert test_tempo.val == test_out[_]
     test_tempo = test_tempo.next
-    if _ == len(test3_out) == 0:
+    if _ == len(test_out) == 0:
         assert test.next is None
-del test
-
-test4 = []
-test4_x = 0
-test4_out = []
-test = partition(test4, test4_x)
-assert test == []
