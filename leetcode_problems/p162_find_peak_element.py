ca0e2f5275a7267ad56bc336175bbd9c0dae1e65
Pampam <piankovpe@gmail.com>
2023-08-22 18:10:52 +0600

    add working_sol (99.08%, 62.39%) -> (40ms, 16.4mb)  time: O(log n) | space: O(1) | Revisit

--- leetcode_problems/p162_find_peak_element.py
+++ leetcode_problems/p162_find_peak_element.py
@@ -1,9 +1,8 @@
 # A peak element is an element that is strictly greater than its neighbors.
-#   Given a 0-indexed integer array nums, find a peak element, and return its index.
-#   If the array contains multiple peaks, return the index to any of the peaks.
+# Given a 0-indexed integer array nums, find a peak element, and return its index.
+# If the array contains multiple peaks, return the index to any of the peaks.
 # You may imagine that nums[-1] = nums[n] = -∞.
 # In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
-#
 # You must write an algorithm that runs in O(log n) time.
 # ----------------------
 # 1 <= nums.length <= 1000  ,  -2 ** 31 <= nums[i] <= 2 ** 31 - 1
@@ -12,42 +11,44 @@ from random import randint
 
 
 def find_peak_element(nums: list[int]) -> int:
-    # working_sol (58.60%, 51.61%) -> (58ms, 16.4mb)  time: O(log n) | space: O(1)
+    # working_sol (99.08%, 62.39%) -> (40ms, 16.4mb)  time: O(log n) | space: O(1)
     if len(nums) == 1:
         return 0
-    # eliminate 2 unique cases
+    # Eliminate 2 unique cases.
+    # ! [0], [-1] ->  greater than a neighbor that is outside the array !
     if nums[0] > nums[1]:
         return 0
     if nums[-1] > nums[-2]:
         return len(nums) - 1
+    # ^^[0], [-1] already checked, skip them.
     left: int = 1
     right: int = len(nums) - 2
     while left < right:
-        # extra checks to reduce calls in some cases
+        # Left peak check.
         if nums[left - 1] < nums[left] > nums[left + 1]:
             return left
+        # Right peak check.
         if nums[right - 1] < nums[right] > nums[right + 1]:
             return right
         middle: int = int((right + left) / 2)
-        # peak check
+        # Middle peak check.
         if nums[middle - 1] < nums[middle] > nums[middle + 1]:
             return middle
-        # ascending half check
-        elif nums[middle - 1] < nums[middle] < nums[middle + 1]:
+        # ! nums[i] != nums[i + 1] for all valid i. !
+        # So it's always either ascending or descending.
+        # Ascending -> peak will be on right side.
+        elif nums[middle] < nums[middle + 1]:
             left = middle + 1
-        # descending half check
-        elif nums[middle + 1] < nums[middle] < nums[middle - 1]:
+        # Descending -> peak will be on left side.
+        elif nums[middle] < nums[middle - 1]:
             right = middle - 1
-        # duplicates or lowest, can be (right = middle - 1)
-        elif nums[middle - 1] >= nums[middle] <= nums[middle + 1]:
-            left = middle + 1
-    # in the end right == left, so we can return either
+    # In the end right == left, so we can return either
     return left
 
 
-# Time complexity: O(log n) -> default, binary search, but for breaking points not some number => O(log n)
+# Time complexity: O(log n) -> default, binary search for any of breaking points(peaks) in the input_list => O(log n)
 # n - len of input_list^^|
-# Space complexity: O(1) -> 3 extra constant INTs, doesn't depends on input => O(1)
+# Space complexity: O(1) -> 3 extra constant INTs, none of them depends on input => O(1)
 # ----------------------
 # Well in the end, did the same solution as I did in p154 but instead of recursion now it's iterative.
 # Actually if I ever revisit 153 I can rebuild it to be constant_space.
@@ -67,23 +68,20 @@ def find_peak_element(nums: list[int]) -> int:
 # which will give us another PEAK. Should work.
 
 
-test1 = [1, 2, 3, 1]
-test1_out = 2
-print(find_peak_element(test1))
-assert test1_out == find_peak_element(test1)
+test: list[int] = [1, 2, 3, 1]
+test_out: int = 2
+assert test_out == find_peak_element(test)
 
-test2 = [1, 2, 1, 3, 5, 6, 4]
-test2_out = 1
-print(find_peak_element(test2))
-assert test2_out == find_peak_element(test2)
+test = [1, 2, 1, 3, 5, 6, 4]
+test_out = 1
+assert test_out == find_peak_element(test)
 
 for _ in range(100):
-    test_case: list[int] = [-2600] + [randint(-2500, 2500) for num in range(1000)]
-    test_case = test_case + test_case.copy() + [-2600]  # eliminating unique cases
+    #  extra eliminate unique cases
+    test_case: list[int] = [-2600] + [randint(-2500, 2500) for num in range(1000)] + [-2600]
     test_out: set[int] = set()
     for x in range(1, (len(test_case) - 1)):
         if test_case[x - 1] < test_case[x] > test_case[x + 1]:
             test_out.add(x)
-    test = find_peak_element(test_case)
-    assert test in test_out
+    assert find_peak_element(test_case) in test_out
     test_out.clear()
