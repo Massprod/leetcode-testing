7f49b0f9350bf0a582c2ed1e0daabc7d1ffddc7e
Pampam <piankovpe@gmail.com>
2023-09-13 22:06:50 +0600

    add working_sol (77.68%, 91.55%) -> (140ms, 19.17mb)  time: O(n) | space: O(n) | Revisit

--- leetcode_problems/p135_candy.py
+++ leetcode_problems/p135_candy.py
@@ -5,42 +5,35 @@
 #   2) Children with a higher rating get more candies than their neighbors.
 # Return the minimum number of candies you need to have to distribute the candies to the children.
 # ---------------------
-# n == ratings.length  ,  1 <= n <= 2 * 104  ,  0 <= ratings[i] <= 2 * 104
+# n == ratings.length
+# 1 <= n <= 2 * 10 ** 4
+# 0 <= ratings[i] <= 2 * 10 ** 4
 
 
 def candy(ratings: list[int]) -> int:
-    # working_sol (47.62%, 47.31%) -> (174ms, 19.2mb)  time: O(2n) | space: O(n)
-    if len(ratings) == 0:
-        return 1
-    candies: int = 0
-    all_candies: list[int] = [1 for _ in range(len(ratings))]
-    for x in range(len(ratings) - 1):
-        if ratings[x + 1] > ratings[x]:
-            left_candies: int = all_candies[x] + 1
-            all_candies[x + 1] = left_candies
-    candies += all_candies[-1]
+    # working_sol (77.68%, 91.55%) -> (140ms, 19.17mb)  time: O(n) | space: O(n)
+    # Greedy approach.
+    # ! Each child must have at least one candy. !
+    # Give every child at least 1 candy.
+    all_childs: list[int] = [1 for _ in ratings]
+    # LEFT neighbours.
+    for x in range(1, len(ratings)):
+        # Higher rating -> more candies than LEFT neighbour.
+        if ratings[x] > ratings[x - 1]:
+            all_childs[x] = all_childs[x - 1] + 1
+    # RIGHT neighbours.
     for y in range(len(ratings) - 2, -1, -1):
         if ratings[y] > ratings[y + 1]:
-            current_candies: int = all_candies[y]
-            right_candies: int = all_candies[y + 1] + 1
-            all_candies[y] = max(current_candies, right_candies)
-        candies += all_candies[y]
-    return candies
+            all_childs[y] = max(       # Already:
+                all_childs[y],         # Have more than RIGHT neighbour.
+                all_childs[y + 1] + 1  # Have less than RIGHT neighbour.
+            )
+    return sum(all_childs)
 
 
-# Time complexity: O(2n) -> traversing input_list twice to count candies from left_neighbor and right_neighbor => O(2n)
-# n - len of input_list^^  ! can be changed to O(n) -> because input doesn't change number of loops,
-#                            but I prefer to show number of loops. !
-# Space complexity: O(n) -> extra list of input_list size to store all candy values for individual child => O(n)
-# ---------------------
-# Flow:
-#   1) -> adding every child 1 candy by default ->
-#   2) -> adding every child their (left neighbors candies + 1) if they have higher rating than this left neighbor ->
-#   3) -> adding last child candies into a candies, because we're checking right neighbors next,
-#                                                            and he doesn't have right neighbor ->
-#   4) -> adding every child their right neighbor candies if they have higher rating than this right neighbor ->
-#         -> but, ignoring child's who's already have more candies than their (right neighbor candies + 1).
-#   !) -> candies, can be replaced with just sum of all_candies.
+# Time complexity: O(n) -> traversing input_list twice to count candies from left_neighbor and right_neighbor => O(2n).
+# n - len of input_list^^|
+# Space complexity: O(n) -> extra list of input_list size to store all candy values for individual child => O(n).
 # ---------------------
 # !
 # Children with a higher rating get more candies than their neighbors. !
@@ -56,17 +49,14 @@ def candy(ratings: list[int]) -> int:
 #   But HARD to see this tricky_part.
 
 
-test1 = [1, 0, 2]
-test1_out = 5
-print(candy(test1))
-assert test1_out == candy(test1)
+test: list[int] = [1, 0, 2]
+test_out: int = 5
+assert test_out == candy(test)
 
-test2 = [1, 2, 2]
-test2_out = 4
-print(candy(test2))
-assert test2_out == candy(test2)
+test = [1, 2, 2]
+test_out = 4
+assert test_out == candy(test)
 
-test3 = [1, 2, 3, 4, 5, 2, 2, 9, 3, 9, 2, 1, 6, 2, 4, 0, 1, 0]
-test3_out = 35
-print(candy(test3))
-assert test3_out == candy(test3)
+test = [1, 2, 3, 4, 5, 2, 2, 9, 3, 9, 2, 1, 6, 2, 4, 0, 1, 0]
+test_out = 35
+assert test_out == candy(test)
