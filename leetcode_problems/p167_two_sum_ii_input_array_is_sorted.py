26f50a2e28c5bc1a03f520c3acb8ab1ca7b81bf2
Pampam <piankovpe@gmail.com>
2023-09-26 22:50:24 +0600

    add working_sol (69.93%, 85.26%) -> (117ms, 17.14mb)  time: O(n) | space: O(1) | Revisit

--- leetcode_problems/p167_two_sum_ii_input_array_is_sorted.py
+++ leetcode_problems/p167_two_sum_ii_input_array_is_sorted.py
@@ -13,11 +13,10 @@
 # numbers is sorted in non-decreasing order.
 # -1000 <= target <= 1000
 # The tests are generated such that there is exactly one solution.
-from random import randint
 
 
 def two_sum(numbers: list[int], target: int) -> list[int]:
-    # working_sol (79.84%, 94.99%) -> (136ms, 17.1mb)  time: O(n) | space: O(1)
+    # working_sol (69.93%, 85.26%) -> (117ms, 17.14mb)  time: O(n) | space: O(1)
     left: int = 0
     right: int = len(numbers) - 1
     while left < right:
@@ -25,8 +24,11 @@ def two_sum(numbers: list[int], target: int) -> list[int]:
         if cur_sum == target:
             # zero_indexed -> 1_indexed
             return [left + 1, right + 1]
+        # ! already sorted in non-decreasing order !
+        # Make second value lower.
         if cur_sum > target:
             right -= 1
+        # Make first value higher.
         if cur_sum < target:
             left += 1
 
@@ -34,44 +36,29 @@ def two_sum(numbers: list[int], target: int) -> list[int]:
 # Time complexity: O(n) -> in the worst_case answer is indexes: first = (len(numbers) // 2) and sec = first + 1 ->
 # n - len of input_array^^| -> then we need to travel whole input_array until we find it => O(n).
 # Auxiliary space: O(1) -> using 3 extra INTs, none of them depends on input => O(1).
-# -------------------
-# Took a peak into BIG_BOYS with 90% and I missed most simple way to do this.
-# This is just a closing_window problem, made it overcomplicated AF.
-# Rebuild.
-# -------------------
-# Rushed and didn't take a break after BT task, failed 2 commits and missed most basic parts.
-# Like middle and break when found index is same as x...
-# -------------------
-# Before I just stored every possible numbers[x] - target and just searched for this.
-# Now we can't use any storage, so it's should be binary search.
 
 
-test1 = [2, 7, 11, 15]
-test1_t = 9
-test1_out = [1, 2]
-print(two_sum(test1, test1_t))
-assert test1_out == two_sum(test1, test1_t)
+test: list[int] = [2, 7, 11, 15]
+test_t: int = 9
+test_out: list[int] = [1, 2]
+assert test_out == two_sum(test, test_t)
 
-test2 = [2, 3, 4]
-test2_t = 6
-test2_out = [1, 3]
-print(two_sum(test2, test2_t))
-assert test2_out == two_sum(test2, test2_t)
+test = [2, 3, 4]
+test_t = 6
+test_out = [1, 3]
+assert test_out == two_sum(test, test_t)
 
-test3 = [-1, 0]
-test3_t = -1
-test3_out = [1, 2]
-print(two_sum(test3, test3_t))
-assert test3_out == two_sum(test3, test3_t)
+test = [-1, 0]
+test_t = -1
+test_out = [1, 2]
+assert test_out == two_sum(test, test_t)
 
-test4 = [0, 0, 3, 4]
-test4_t = 0
-test4_out = [1, 2]
-print(two_sum(test4, test4_t))
-assert test4_out == two_sum(test4, test4_t)
+test = [0, 0, 3, 4]
+test_t = 0
+test_out = [1, 2]
+assert test_out == two_sum(test, test_t)
 
-test5 = [3, 24, 50, 79, 88, 150, 345]
-test5_t = 200
-test5_out = [3, 6]
-print(two_sum(test5, test5_t))
-assert test5_out == two_sum(test5, test5_t)
+test = [3, 24, 50, 79, 88, 150, 345]
+test_t = 200
+test_out = [3, 6]
+assert test_out == two_sum(test, test_t)
