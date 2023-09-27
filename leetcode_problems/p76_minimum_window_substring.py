9ccbaaa958ab9eae3589929197c32e7161c831e0
Pampam <piankovpe@gmail.com>
2023-09-28 00:18:14 +0600

    add working_sol (51.58%, 98.46%) -> (126ms, 17mb)  time: O(n + m) | space: O(m) | Revisit + Follow up

--- leetcode_problems/p76_minimum_window_substring.py
+++ leetcode_problems/p76_minimum_window_substring.py
@@ -3,149 +3,108 @@
 #   If there is no such substring, return the empty string "".
 # The testcases will be generated such that the answer is unique.
 # --------------------------
-# m == s.length  ,  n == t.length  ,  1 <= m, n <= 10 ** 5
+# m == s.length
+# n == t.length
+# 1 <= m, n <= 10 ** 5
 # s and t consist of uppercase and lowercase English letters.
 # --------------------------
 # Follow up: Could you find an algorithm that runs in O(m + n) time?
+from collections import Counter
+from random import choice, randint
+from string import ascii_letters
 
 
 def min_window(s: str, t: str) -> str:
-    # working_sol (21.69%, 65.61%) -> (301ms, 17mb)  time: O(n + (n - j) * m) | space: O(m)
-    if len(t) > len(s):
-        return ""
-    chars: dict[str] = {}
-    cur_chars: dict[str] = {}
-    for char in t:
-        if char not in chars:
-            chars[char] = 1
-            cur_chars[char] = 0
-            continue
-        chars[char] += 1
-    left: int = 0
-    right: int = 0
-    min_sub: str = ""
-    whole: bool = False
-    used: int = 0
-    while right != len(s) + 1 and left < len(s):
-        if used >= len(t):
-            for key in cur_chars:
-                if cur_chars[key] >= chars[key]:
-                    whole = True
-                    continue
-                if chars[key] > cur_chars[key]:
-                    whole = False
-                    break
-        if whole:
-            if len(min_sub) > len(s[left: right]) or len(min_sub) == 0:
-                min_sub = s[left: right]
-                if len(min_sub) == len(t):
-                    return min_sub
-            if s[left] in cur_chars:
-                cur_chars[s[left]] -= 1
-                used -= 1
-                whole = False
-            left += 1
-            continue
-        while left < len(s) and ((s[left] not in cur_chars) or ((cur_chars[s[left]] - chars[s[left]]) > 0)):
-            if (s[left] in cur_chars) and ((cur_chars[s[left]] - chars[s[left]]) > 0):
-                cur_chars[s[left]] -= 1
-                used -= 1
-                whole = False
-            left += 1
-        if (right < len(s)) and (s[right] in cur_chars):
-            cur_chars[s[right]] += 1
-            used += 1
-        right += 1
-    return min_sub
+    # working_sol (56.36%, 98.46%) -> (120ms, 17mb)  time: O(n + m) | space: O(m)
+    # All we need to maintain from 't'.
+    all_symbols: dict[str, int] = Counter(t)
+    # Standard sliding window.
+    left_l: int = 0
+    right_l: int = 0
+    # # of correct symbols used.
+    count: int = 0
+    min_sub: list[int] = [0, 0]
+    max_dif: int = len(s) + 1
+    new: bool = True
+    while right_l != len(s):
+        # Add everything we can, no matter duplicates.
+        if s[right_l] in all_symbols:
+            # But, only count symbols which still not satisfied.
+            if all_symbols[s[right_l]] > 0:
+                count += 1
+            # Count exceeding symbols,
+            #  then we could delete them on left side.
+            all_symbols[s[right_l]] -= 1
+            right_l += 1
+            if count == len(t):
+                # Try to delete everything we can on left side.
+                # (exceeding symbols or symbols we don't need)
+                while s[left_l] not in all_symbols or all_symbols[s[left_l]] < 0:
+                    if s[left_l] in all_symbols:
+                        all_symbols[s[left_l]] += 1
+                    left_l += 1
+                    new = True
+                # To cull extra calcs, better to ignore not changed substrings.
+                # Because count will stay the same after it's reached len(t).
+                if new:
+                    new = False
+                    # A little bit faster than creating slice and checking its length.
+                    # less diff == less distance == shorter substring.
+                    cur_dif: int = right_l - left_l
+                    # Can't be lower.
+                    if cur_dif == len(t):
+                        return s[left_l: right_l]
+                    elif max_dif > cur_dif:
+                        max_dif = cur_dif
+                        min_sub[0], min_sub[1] = left_l, right_l
+        else:
+            right_l += 1
+    return s[min_sub[0]: min_sub[1]]
 
 
-# Time complexity: O(n + (n - j) * m) ->
-#   -> traversing whole symbols_string, and creating 3 dictionaries with equal size => O(m) ->
-#   -> traversing whole input_string from 0 to end with right_pointer, in the worst case,
-#      and (n - m) with left_pointer => O(n + (n - m)) ->
-#   -> for every index on this path checking every KEY in cur_chars + chars + cur_chars, every check is O(1) but
-#      for every KEY is still should be O(m) => O((n - j) * 3m) ->
-#   -> O(m + n + (n - m) + (n - j) * 3m) => O(2n + (n - j) * 3m) =>  O(n + (n - j) * m)
-# ^^ m - len of symbols_string | n - len of input_string | j - number of unique subs with WHOLE in it ^^
+# Time complexity: O(n + m) -> traverse of all indexes of 't' to count occurrences => O(m) ->
+# n - len of input string 's'^^| -> sliding window with using every index once if we don't shrink it ->
+# m - len of input string 't'^^| -> and in the worst case we will use every index twice => O(2n + m).
+# Auxiliary space: O(m) -> dictionary with all unique symbols of input string: 't' => O(m).
 # --------------------------
-# ^^After sleeping on this task, it's not so bad in the end if I just add extra check to cull some calls
-# on check if WHOLE or not, now it's 308ms instead of 800, still not 120ms like top_tiers, but I'm fine with that.
-# And now it's not (n * m) -> ((n - j) * m) <- where's j is number of unique substrings
-#                                              with correct number of search_values.
-# Don't know how to calculate this j. Worst case should be like -> s = "aacbcaacbcaacbca", t = "aba" ->
-# -> "aacb" gives us WHOLE but not same length as t, so we're going to check more ->
-# -> so after that we're checking every index from 3 to 15 is calling check on WHOLE, so it's still O((n - j) * m).
-# But in the best case, like -> s = "aacbcddddddddd", t = "aba" -> we're getting "aacb" ->
-# -> and after shifting left side by 1 index, it's never going to get (used >= len(t)), so it's just 1 check.
-# Better than it was, but still can be changed to check less, maybe I will come up with something later.
-# --------------------------
-# Auxiliary space: O(m) -> 3 dictionaries of the same size == m => O(3m) -> extra constants,
-#                          and string with the size == m in the worst case => O(m) -> O(4m) -> O(m)
-# --------------------------
-# Sheesh. Hard without extra info and only using HINT as guidance, evolving.
-# Dunno about O(n + m), but at least I made a correctly working solution.
-# Actually we're making it in ONE_WAY walk O(n), but with every step we're checking too much data
-# in dictionaries, so if we can't call dictionary operations as O(1), in this case I doubt that.
-# Because search in dictionary is O(1) but we're checking every KEY. Plus there's top solutions with <200ms,
-# when mine is 801ms. So if they're O(n + m) mine is something O(n + m * n) ->
-# -> we're checking every index once, but every KEY in dictionaries will be checked, on every index step.
-# --------------------------
-# Don't think changing extras from list to dict is going to change anything, but if there's
-# a 100 extra it's going to be faster. Well let's try this one as well.
-# Otherwise, I need to find how to check all_symbols used or not faster.
-# Well well well, working in this 266 test_case, 1 to go :)
-# --------------------------
-# Sadly 265/267 cases passed, now it's TimeGate, but at least I made working solution,
-# without extra info(google/gpt). There's a lot of extra checks, and maybe I will find how to cull them.
-# What about length?
-# While doing heavy_lifting I forgot that we can just cull any input with incorrect length.
-# Nah, not the case, but still good to implement.
-# Another one I see is that we can insta return if we found min_sub with equal length to t,
-# because t == min_sub is IDEAL option, no reasons to search anything else.
-# --------------------------
-# Ok. I need to fail, because I'm kinda stuck with thinking that's correct need more test_cases.
-# --------------------------
-# Creating two dictionaries because copying is taking the same time, and I want to use counter and limiter.
-# And without copying should be O(m) not O(2m).
-# --------------------------
-# !
-# s and t consist of uppercase and lowercase English letters. !
-# Should we differentiate them? Because "a" can be equal to "A" or not, no info on that.
-# !
-# such that every character in t (including duplicates) is included in the window !
-# Only this. Hmm. Guess there's would be no reasons to put UPPER and LOWER cases as constraints,
-# if we shouldn't differentiate them.
-# I will stick to this, and search for UPPER and LOWER cases as different characters.
-# Tested with test case s = "A" and t = "a", correct output from them is "".
-# So yes we should differentiate them.
+# Ok. Changed to 52%+, 126ms. Possible to make it faster, but it's more than enough for now.
+# At least x2 of pre-revisit version.
 
 
-test1 = "ADOBECODEBANC"
-test1_t = "ABC"
-test1_out = "BANC"
-print(min_window(test1, test1_t))
-assert test1_out == min_window(test1, test1_t)
+test: str = "ADOBECODEBANC"
+test_t: str = "ABC"
+test_out: str = "BANC"
+assert test_out == min_window(test, test_t)
 
-test2 = "a"
-test2_t = "a"
-test2_out = "a"
-print(min_window(test2, test2_t))
-assert test2_out == min_window(test2, test2_t)
+test = 'ADOBECODEBANC'
+test_t = "AABC"
+test_out = "ADOBECODEBA"
+assert test_out == min_window(test, test_t)
 
-test3 = "a"
-test3_t = "aa"
-test3_out = ""
-print(min_window(test3, test3_t))
-assert test3_out == min_window(test3, test3_t)
+test = "a"
+test_t = "a"
+test_out = "a"
+assert test_out == min_window(test, test_t)
 
-test4 = "A"
-test4_t = "a"
-test4_out = ""
-print(min_window(test4, test4_t))
-assert test4_out == min_window(test4, test4_t)
+test = "a"
+test_t = "aa"
+test_out = ""
+assert test_out == min_window(test, test_t)
 
-test5 = "acbbaca"
-test5_t = "aba"
-test5_out = "baca"
-print(min_window(test5, test5_t))
-assert test5_out == min_window(test5, test5_t)
+test = "A"
+test_t = "a"
+test_out = ""
+assert test_out == min_window(test, test_t)
+
+test = "acbbaca"
+test_t = "aba"
+test_out = "baca"
+assert test_out == min_window(test, test_t)
+
+test = ''
+for _ in range(10 ** 4):
+    test += choice(ascii_letters)
+test_t = test[randint(0, len(test) // 2): randint((len(test) // 2) + 1, len(test) - 1)]
+# print(test)
+# print('-----------------')
+# print(test_t)
