6358c141ff75a238f23543d2028a3697d2575297
Pampam <piankovpe@gmail.com>
2023-09-28 23:03:24 +0600

    add working_sol (81.62%, 93.48%) -> (97ms, 16.2mb)  time: O(n) | space: O(n) | Revisit

--- leetcode_problems/p36_valid_sudoku.py
+++ leetcode_problems/p36_valid_sudoku.py
@@ -1,70 +1,68 @@
 # Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need
-# to be validated according to the following rules:
-#
-# Each row must contain the digits 1-9 without repetition.
-# Each column must contain the digits 1-9 without repetition.
-# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+#  to be validated according to the following rules:
+#   - Each row must contain the digits 1-9 without repetition.
+#   - Each column must contain the digits 1-9 without repetition.
+#   - Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 # Note:
-#
-# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
-# Only the filled cells need to be validated according to the mentioned rules.
+#   - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+#   - Only the filled cells need to be validated according to the mentioned rules.
+# ------------------------
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
 
 
 def valid_sudoku(board: list[list[str]]) -> bool:
-    # working_sol (9.16%, 69.14%)  time: O(n**2) | space: O(n*n)
-    rows = {}
-    for _ in range(9):
-        rows[_] = []
-    columns = {}
-    for _ in range(9):
-        columns[_] = []
-    cubes = {}
-    for _ in range(9):
-        cubes[_] = []
-    cube = 0
-    for y in range(len(board)):
-        if 0 <= y < 3:
-            cube = 0
-        if 3 <= y < 6:
-            cube = 3
-        if 6 <= y < 9:
-            cube = 6
-        for x in range(len(board)):
-            if x % 3 == 0 and x != 0:
-                cube += 1
-            if board[y][x] not in cubes[cube] or board[y][x] == ".":
-                cubes[cube].append(board[y][x])
-            else:
-                return False
-            if board[y][x] not in rows[y] or board[y][x] == ".":
-                rows[y].append(board[y][x])
-            else:
-                return False
-            if board[y][x] not in columns[x] or board[y][x] == ".":
-                columns[x].append(board[y][x])
-            else:
-                return False
-    # for key, value in rows.items():
-    #     print(value, end="\n")
-    # print("------------------")
-    # for key, value in columns.items():
-    #     print(value, end="\n")
-    # print("------------------")
-    # for key, value in cubes.items():
-    #     print(value, end="\n")
+    # working_sol (81.62%, 93.48%) -> (97ms, 16.2mb)  time: O(n) | space: O(n)
+    # Always 9 rows, 9 cols, 9 cubes == constraints.
+    rows: dict[int, set[str]] = {row: set() for row in range(len(board))}
+    columns: dict[int, set[str]] = {col: set() for col in range(len(board[0]))}
+    cubes: dict[int, set[str]] = {cube: set() for cube in range(len(board))}
+    cur_cube: int = 0
+    # Rows from top -> bottom and cubes from left -> right.
+    for row in range(len(board)):
+        # Starting cube.
+        if 0 <= row < 3:
+            cur_cube = 0
+        elif 3 <= row < 6:
+            cur_cube = 3
+        elif 6 <= row < 9:
+            cur_cube = 6
+        for col in range(len(board[0])):
+            # Every 3 columns(indexes) we're stepping into a new cube.
+            if col != 0 and col % 3 == 0:
+                cur_cube += 1
+            if board[row][col] != '.':
+                cell_value: str = board[row][col]
+                # Check for value being unique in cur_cube,
+                if cell_value not in cubes[cur_cube]:
+                    cubes[cur_cube].add(cell_value)
+                else:
+                    return False
+                # row,
+                if cell_value not in rows[row]:
+                    rows[row].add(cell_value)
+                else:
+                    return False
+                # and column.
+                if cell_value not in columns[col]:
+                    columns[col].add(cell_value)
+                else:
+                    return False
     return True
 
-# Time complexity: O(n**2) -> simple case of double for_loop with iteration through every index
-# Space complexity : O(n*n) -> 1+1+1 dictionaries of n size with n size list's as values.
 
-# Really slow solution and I think we can use fewer dictionaries to store sudoku.
-# But I have already seen p 37 and there's going to be filling of correct number into circles and row.
-# I was solving according to this. Now with all rows, columns and cubes data
-# we can populate them without duplicates (hope so).
-# Once again cringe fail but at least at this day I was worn out.
+# Time complexity: O(n) -> we're always have same input size, so creating of dictionaries for cubes, rows, cols => O(1)
+# n - length of input matrix^^| -> traversing whole input matrix, once => O(n).
+# m - height of input matrix^^|
+# Auxiliary space: O(n) -> because we're having same input size we will have same dictionaries, but values placed
+#                          inside will depend on input matrix, we can have 2 values on whole matrix or None, or even
+#                          fully correct sudoku as input -> but we will store at max full rows, columns and cubes ->
+#                          -> rows + columns == input matrix values, except '.' they're ignored ->
+#                          -> cubes will have same values stored => O(2n).
 
 
-test1 = [
+test: list[list[str]] = [
     ["5", "3", ".", ".", "7", ".", ".", ".", "."],
     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
     [".", "9", "8", ".", ".", ".", ".", "6", "."],
@@ -75,11 +73,10 @@ test1 = [
     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
     [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ]
-test1_out = True
-print(valid_sudoku(test1))
-assert test1_out == valid_sudoku(test1)
+test_out: bool = True
+assert test_out == valid_sudoku(test)
 
-test2 = [
+test = [
     ["8", "3", ".", ".", "7", ".", ".", ".", "."],
     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
     [".", "9", "8", ".", ".", ".", ".", "6", "."],
@@ -90,11 +87,10 @@ test2 = [
     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
     [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ]
-test2_out = False
-print(valid_sudoku(test2))
-assert test2_out == valid_sudoku(test2)
+test_out = False
+assert test_out == valid_sudoku(test)
 
-test3 = [
+test = [
     [".", ".", ".", ".", "5", ".", ".", "1", "."],
     [".", "4", ".", "3", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", "3", ".", ".", "1"],
@@ -105,9 +101,5 @@ test3 = [
     [".", "2", ".", "9", ".", ".", ".", ".", "."],
     [".", ".", "4", ".", ".", ".", ".", ".", "."]
 ]
-test3_out = False
-# test3 - fail - cuz I rushed and forgot about 3x3 cubes.....most important part :)
-# Tired today so w.e
-print(valid_sudoku(test3))
-assert test3_out == valid_sudoku(test3)
-
+test_out = False
+assert test_out == valid_sudoku(test)
