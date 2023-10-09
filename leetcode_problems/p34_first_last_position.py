347bd45554be93fa521ce23cbb127af9ce25fadd
Pampam <piankovpe@gmail.com>
2023-10-09 22:48:41 +0600

    add working_sol (91.12%, 50.97%) -> (77ms, 17.72mb)  time: O(log n) | space: O(1) | Revisit

--- leetcode_problems/p34_first_last_position.py
+++ leetcode_problems/p34_first_last_position.py
@@ -1,86 +1,104 @@
 # Given an array of integers nums sorted in non-decreasing order,
-# find the starting and ending position of a given target value.
-#
+#  find the starting and ending position of a given target value.
 # If target is not found in the array, return [-1, -1].
-#
 # You must write an algorithm with O(log n) runtime complexity.
+# --------------------
+# 0 <= nums.length <= 10 ** 5
+# -10 ** 9 <= nums[i] <= 10 ** 9
+# nums is a non-decreasing array.
+# -10 ** 9 <= target <= 10 ** 9
+from random import randint
 
 
-def first_last(nums: list[int], target: int) -> list[int]:
-    # working_sol (41.61%, 35.53%)
-    if len(nums) == 0:
+def search_range(nums: list[int], target: int) -> list[int]:
+    # working_sol (91.12%, 50.97%) -> (77ms, 17.72mb)  time: O(log n) | space: O(1)
+    if not nums:
         return [-1, -1]
     if len(nums) == 1:
         if nums[0] == target:
             return [0, 0]
         return [-1, -1]
-    if nums[-1] < target:
-        return [-1, -1]
-    if nums[0] > target:
-        return [-1, -1]
-    x, y = 0, (len(nums) - 1)
-    start, end = -1, -1
-    while x <= y and (start == -1 or end == -1):
-        if nums[x] == target and start == -1:
-            start = x
-            continue
-        if nums[y] == target and end == -1:
-            end = y
-            continue
-        if nums[x] < target:
-            x += 1
-        if nums[y] > target:
-            y -= 1
-        if nums[x] == target and start != -1:
-            if nums[x] != nums[x + 1]:
-                end = x
-            x += 1
-        if nums[y] == target and end != -1:
-            if nums[y] != nums[y - 1]:
-                start = y
-            y -= 1
-    return [start, end]
+    if nums[0] == nums[-1] == target:
+        return [0, len(nums) - 1]
 
-# Time complexity: O(log n) -> conquer and divide always (log n).
-# Space complexity: O(1) -> not creating any new arrays, just simple 4 values: 1 + 1 + 1 +1.
+    def bs(left_l: int, right_l: int, first: bool) -> int:
+        out: int = -1
+        while left_l <= right_l:
+            middle: int = (left_l + right_l) // 2
+            if nums[middle] == target:
+                out = middle
+                # Shift to left side for most_left(first) index.
+                if first:
+                    right_l = middle - 1
+                # Shift to right side for most_right(last) index.
+                else:
+                    left_l = middle + 1
+            # Need higher.
+            elif nums[middle] < target:
+                left_l = middle + 1
+            # Need lower.
+            elif nums[middle] > target:
+                right_l = middle - 1
+        return out
 
-# Yep. Assumed correctly, there's traps for len == 1 and START, END can be the same index.
-# This speed thing is disgusting. First commit 13% and just repeating it gets me 41,61%.
+    left_index: int = bs(0, len(nums) - 1, True)
+    right_index: int = left_index
+    # If there's no target, no reasons to search last_index.
+    if left_index >= 0:
+        right_index = bs(0, len(nums) - 1, False)
+    return [left_index, right_index]
 
-# Is this even correct task description?
-# What if we have only ONE index for a target, can a START index and END index be the same,
-# or we should return something like [START, -1] | [-1, END] ????
-# What to return when len(nums) == 1? Guess I need to fail commit to find out.
 
-test1 = [5, 7, 7, 8, 8, 10]
-test1_target = 8
-test1_out = [3, 4]
-print(first_last(test1, test1_target))
+# Time complexity: O(log n) -> worst case == 2 indexes to find -> we will use BS on array twice => O(2 * log n).
+# n - len of input array^^|
+# Auxiliary space: O(1) -> always constant INTs used, none of them depends on input => O(1).
+# --------------------
+# Before it was O(n), and for O(log n) it's always BS in such cases.
+# But we can't just find 1 element, so it should continue until we choose most_left|right elements.
+# Shift search into left side when we search for a left_element, and right side for right_element.
+# Like: left_element => right = middle - 1, right_element => left = middle + 1.
+# Everything else should be ok. Only problem is that we need to BSearch twice, for left and right.
 
-test2 = [5, 7, 7, 8, 8, 10]
-test2_target = 6
-test2_out = [-1, -1]
-print(first_last(test2, test2_target))
 
-test3 = []
-test3_target = 0
-test3_out = [-1, -3]
-print(first_last(test3, test3_target))
+test: list[int] = [5, 7, 7, 8, 8, 10]
+test_target: int = 8
+test_out: list[int] = [3, 4]
+assert test_out == search_range(test, test_target)
 
-# Bet they made bad description to catch len == 1. Suppose we can have START and END at the same index.
-test4 = [1]
-test4_target = 1
-test4_out = [0, 0]
-print(first_last(test4, test4_target))
+test = [5, 7, 7, 8, 8, 10]
+test_target = 6
+test_out = [-1, -1]
+assert test_out == search_range(test, test_target)
 
-test5 = [1, 10]
-test5_target = 1
-test5_out = [0, 0]
-print(first_last(test5, test5_target))
+test = []
+test_target = 0
+test_out = [-1, -1]
+assert test_out == search_range(test, test_target)
 
-# Yep. Incorrect description: START, END can be same index and there's no words about it,
-# or it's a first time encounter for me, and it's a classic practice to assume: start == end.
-test6 = [1, 2, 3]
-test6_target = 2
-test6_out = [1, 1]
-print(first_last(test6, test6_target))
+test = [1]
+test_target = 1
+test_out = [0, 0]
+assert test_out == search_range(test, test_target)
+
+test = [1, 10]
+test_target = 1
+test_out = [0, 0]
+assert test_out == search_range(test, test_target)
+
+test = [1, 2, 3]
+test_target = 2
+test_out = [1, 1]
+assert test_out == search_range(test, test_target)
+
+test = [2, 2]
+test_target = 1
+test_out = [-1, -1]
+assert test_out == search_range(test, test_target)
+
+test = [1, 2, 2]
+test_target = 2
+test_out = [1, 2]
+assert test_out == search_range(test, test_target)
+
+test = sorted([randint(-10 ** 9, 10 ** 9) for _ in range(10 ** 4)])
+print(test)
