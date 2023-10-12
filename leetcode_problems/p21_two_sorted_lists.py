defcc9b38a72059387f38a81c350cf7b3fc0b824
Pampam <piankovpe@gmail.com>
2023-10-12 20:09:31 +0600

    add working_sol (98.56%, 90.89%) -> (31ms, 16.16mb)  time: O(min(m, n) | space: O(1) | Revisit

--- leetcode_problems/p21_two_sorted_lists.py
+++ leetcode_problems/p21_two_sorted_lists.py
@@ -1,9 +1,12 @@
 # You are given the heads of two sorted linked lists list1 and list2.
-#
-# Merge the two lists in a one sorted list.
+# Merge the two lists into one sorted list.
 # The list should be made by splicing together the nodes of the first two lists.
-#
 # Return the head of the merged linked list.
+# ---------------------
+# The number of nodes in both lists is in the range [0, 50].
+# -100 <= Node.val <= 100
+# Both list1 and list2 are sorted in non-decreasing order.
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -14,61 +17,66 @@ class ListNode:
         return f"{self.val} -> {self.next}"
 
 
+def create_linked(to_link: list[int]) -> ListNode:
+    tempo = link = ListNode()
+    for index in range(len(to_link)):
+        tempo.val = to_link[index]
+        if index != (len(to_link) - 1):
+            tempo.next = ListNode()
+            tempo = tempo.next
+    return link
+
+
 def merge_two_lists(list1: ListNode, list2: ListNode) -> ListNode:
-    # head_list1 = list1
-    # values_list = []
-    # while head_list1:
-    #     val = head_list1.val
-    #     head_list1 = head_list1.next
-    #     values_list.append(val)
-    # head_list2 = list2
-    # while head_list2:
-    #     val = head_list2.val
-    #     head_list2 = head_list2.next
-    #     values_list.append(val)
-    # values_list.sort()
-    # if len(values_list) == 0:
-    #     return head_list1
-    # new_sorted = ListNode(values_list[0])
-    # temp = new_sorted
-    # for _ in values_list[1:]:
-    #     new_node = ListNode(_)
-    #     temp.next = new_node
-    #     temp = new_node
-    # return new_sorted
-    # without rebuilding  slower, googled, understood, better only on memory
-    temp = ListNode()
-    cursor = temp
-    head1 = list1
-    head2 = list2
-    while head1 and head2:  # sorting values while head1.val correspond with head2.val
-        if head1.val < head2.val:
-            cursor.next = head1
-            head1, cursor = head1.next, head1
+    # working_sol (98.56%, 90.89%) -> (31ms, 16.16mb)  time: O(min(m, n) | space: O(1)
+    merged: ListNode = ListNode()
+    tempo: ListNode = merged
+    # We need merged and sorted.
+    # No reasons to recreate just reassign as merged_sort.
+    while list1 and list2:
+        if list1.val < list2.val:
+            tempo.next = list1
+            tempo = list1
+            list1 = list1.next
         else:
-            cursor.next = head2
-            head2, cursor = head2.next, head2
-    if head1:  # setting extra values in form of a left head1 or head2 list
-        cursor.next = head1
-    elif head2:
-        cursor.next = head2
-    return temp.next
+            tempo.next = list2
+            tempo = list2
+            list2 = list2.next
+    # Reuse what's left in one of the lists.
+    if list1 or list2:
+        if list1:
+            tempo.next = list1
+        else:
+            tempo.next = list2
+    return merged.next
 
 
-test_val = [1, 2, 4]
-head1 = ListNode(val=test_val[0])
-temp = head1
-for _ in test_val[1:]:
-    new_node = ListNode(_)
-    temp.next = new_node
-    temp = new_node
-test_val2 = [1, 3, 4, 6, 8]
-head2 = ListNode(val=test_val2[0])
-temp = head2
-for _ in test_val2[1:]:
-    new_node = ListNode(_)
-    temp.next = new_node
-    temp = new_node
+# Time complexity: O(min(m, n)) -> we're not creating a new nodes, just reassigning list1|list2 ->
+# m - number of nodes in 'list1'^^| -> so it's always traverse of minimum size list and assign of leftovers =>
+# n - number of nodes in 'list2'^^| => O(min(m, n))
+# Auxiliary space: O(1) -> only 1 extra node 'merged' created, everything else is reassign => O(1).
 
 
-print(merge_two_lists(head1, head2))
+test_l1: ListNode | None = create_linked([1, 2, 4])
+test_l2: ListNode | None = create_linked([1, 3, 4])
+test_out: ListNode | None = create_linked([1, 1, 2, 3, 4, 4])
+test_t: ListNode | None = merge_two_lists(test_l1, test_l2)
+while test_out or test_t:
+    assert test_t.val == test_out.val
+    test_t, test_out = test_t.next, test_out.next
+
+test_l1 = None
+test_l2 = None
+test_out = None
+test_t: ListNode | None = merge_two_lists(test_l1, test_l2)
+while test_out or test_t:
+    assert test_t.val == test_out.val
+    test_t, test_out = test_t.next, test_out.next
+
+test_l1 = None
+test_l2 = create_linked([0])
+test_out = create_linked([0])
+test_t: ListNode | None = merge_two_lists(test_l1, test_l2)
+while test_out or test_t:
+    assert test_t.val == test_out.val
+    test_t, test_out = test_t.next, test_out.next
