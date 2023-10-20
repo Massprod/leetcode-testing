3086f77cb1420215810e68c7e05c7c3d0d7bad03
Pampam <piankovpe@gmail.com>
2023-10-20 20:37:44 +0600

    add working_sol (77.53%, 96.58%) -> (37ms, 16.08mb)  time: O(n) | space: O(n) | Follow up: one pass

--- leetcode_problems/p19_remove_nth_node.py
+++ leetcode_problems/p19_remove_nth_node.py
@@ -1,43 +1,106 @@
-# Given the head of a linked list, remove the nth node from the end of the list
-# and return its head.
+# Given the head of a linked list,
+#  remove the nth node from the end of the list and return its head.
+# ----------------
+# The number of nodes in the list is sz.
+# 1 <= sz <= 30
+# 0 <= Node.val <= 100
+# 1 <= n <= sz
+# ----------------
+# Follow up: Could you do this in one pass?
+
 
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
+    """
+    Create single Node for a linked list.
+    """
+
+    def __init__(self, x: int = 0, next: 'ListNode' = None):
+        """
+        :param x: value of the Node.
+        :param next: Node to which it points.
+        """
+        self.val = x
         self.next = next
 
     def __str__(self):
         return f"{self.val} -> {self.next}"
 
 
+def create_linked(to_link: list[int]) -> ListNode | None:
+    """
+    Create linked list.
+
+    :param to_link: values to put into linked list Nodes.
+    """
+    if not to_link:
+        return None
+    tempo = link = ListNode()
+    for index in range(len(to_link)):
+        tempo.val = to_link[index]
+        if index != (len(to_link) - 1):
+            tempo.next = ListNode()
+            tempo = tempo.next
+    return link
+
+
+def t_linked(list1: ListNode | None, list2: ListNode | None) -> None:
+    """
+    Test two linked lists to have same values.
+    :param list1: first linked list to test.
+    :param list2: second linked list to test.
+    :return:
+    """
+    if not list1 or not list2:
+        assert list1 == list2
+    while list1 and list2:
+        assert list1.val == list2.val
+        list1 = list1.next
+        list2 = list2.next
+    assert list1 == list2
+
+
 def remove_node(head: ListNode, n: int) -> ListNode:
-    # working_sol (30.30%, 55.77%)
-    length = 0
-    tempo = head
+    # working_sol (77.53%, 96.58%) -> (37ms, 16.08mb)  time: O(n) | space: O(n)
+    nodes: list[ListNode] = []
+    tempo: ListNode = head
     while tempo:
+        nodes.append(tempo)
         tempo = tempo.next
-        length += 1
-    position = length - n
-    tempo = ListNode()
-    cursor = tempo
-    for x in range(position + 1):
-        if x == position:
-            cursor.next = head.next
-            break
-        cursor.next = head
-        head, cursor = head.next, head
-    return tempo.next
-
-# I'm sure there's no need to get length, but I have no idea how for now.
-# There's a lot of linked_list tasks in a future, just revisit it later.
-# Really need more practice with linked_lists. 2 tasks isn't enough to get them straight.
+    # First node for deletion, we can't return 'head'.
+    if -n == -len(nodes):
+        if len(nodes) == 1:
+            return head.next
+        return nodes[1]
+    # Delete 'n' Node == reassign neighbours.
+    # And we know there's something before it, cuz it's not [0] index.
+    nodes[-n - 1].next = nodes[-n].next
+    return head
 
 
-test_val = [1, 2, 3, 4, 5]
-test1 = ListNode(val=test_val[0])
-temp = test1
-for _ in test_val[1:]:
-    new_node = ListNode(val=_)
-    temp.next = new_node
-    temp = new_node
-remove_node(test1, n=2)
+# Time complexity: O(n) -> traversing whole input linked_list once => O(n).
+# n - Nodes of input linked_list^^|
+# Auxiliary space: O(n) -> every link to a Node is saved => O(n).
+#                       Actually might be called constant, because we're saving only Links and they always mention.
+#                       That recursions using only links to a Nodes are constant space, but I will stick to Linear.
+# ----------------
+# Follow up == One pass.
+# We're not limited by space, so it can be just safe everything in a list and reassign indexes.
+# But can it be done in constant space and one pass?
+# Don't see it, cuz we have no idea how many nodes left we can't just use some buffer.
+# And we can't reassign without second pass in this case, because we need to know how many Nodes left on right side.
+
+
+test: ListNode = create_linked([1, 2, 3, 4, 5])
+test_n: int = 2
+test_out: ListNode = create_linked([1, 2, 3, 5])
+t_linked(test_out, remove_node(test, test_n))
+
+test = create_linked([1])
+test_n = 1
+test_out = create_linked([])
+t_linked(test_out, remove_node(test, test_n))
+
+test = create_linked([1, 2])
+test_n = 1
+test_out = create_linked([1])
+t_linked(test_out, remove_node(test, test_n))
