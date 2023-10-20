cfb507384c946dd820c9a0fa52833ca655641241
Pampam <piankovpe@gmail.com>
2023-10-20 22:38:24 +0600

    add working_sol (71.21%, 92%) -> (44ms, 16.2mb)  time: O(n) | space: O(n) | Revisit

--- leetcode_problems/p82_remove_duplicates_from_sorted_list_ii.py
+++ leetcode_problems/p82_remove_duplicates_from_sorted_list_ii.py
@@ -1,5 +1,5 @@
 # Given the head of a sorted linked list, delete all nodes that have duplicate numbers,
-# leaving only distinct numbers from the original list.
+#  leaving only distinct numbers from the original list.
 # Return the linked list sorted as well.
 # --------------------------
 # The number of nodes in the list is in the range [0, 300].
@@ -8,15 +8,30 @@
 
 
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val: int = val
-        self.next: ListNode = next
+    """
+    Create single Node for a linked list.
+    """
+
+    def __init__(self, x: int = 0, next: 'ListNode' = None):
+        """
+        :param x: value of the Node.
+        :param next: Node to which it points.
+        """
+        self.val = x
+        self.next = next
 
     def __str__(self):
         return f"{self.val} -> {self.next}"
 
 
-def create_linked(to_link: list[int]) -> ListNode:
+def create_linked(to_link: list[int]) -> ListNode | None:
+    """
+    Create linked list.
+
+    :param to_link: values to put into linked list Nodes.
+    """
+    if not to_link:
+        return None
     tempo = link = ListNode()
     for index in range(len(to_link)):
         tempo.val = to_link[index]
@@ -26,96 +41,61 @@ def create_linked(to_link: list[int]) -> ListNode:
     return link
 
 
+def t_linked(list1: ListNode | None, list2: ListNode | None) -> None:
+    """
+    Test two linked lists to have same values.
+    :param list1: first linked list to test.
+    :param list2: second linked list to test.
+    :return:
+    """
+    if not list1 or not list2:
+        assert list1 == list2
+    while list1 and list2:
+        assert list1.val == list2.val
+        list1 = list1.next
+        list2 = list2.next
+    assert list1 == list2
+
+
 def delete_duplicates(head: ListNode) -> ListNode | None:
-    # working_sol (38.60%, 15.26%) -> (50ms, 16.3mb)  time: O(n + m) | space: O(m + (log n))
-    if not head:
+    # working_sol (71.21%, 92%) -> (44ms, 16.2mb)  time: O(n) | space: O(n)
+    if not head or not head.next:
         return head
-    prev_val: int = head.val
-    to_link: list[int] = [prev_val]
-    temp = head.next
-    while temp:
-        if prev_val == temp.val:
-            temp = temp.next
-            if len(to_link) == 0:
-                continue
-            if len(to_link) != 0 and to_link[-1] == prev_val:
-                to_link.pop()
-            continue
-        if prev_val != temp.val:
-            prev_val = temp.val
-            to_link.append(prev_val)
-            if temp.next is None:
-                break
-            temp = temp.next
-    if len(to_link) == 0:
+    nodes: list[ListNode] = [head]
+    prev_node: ListNode = head
+    head = head.next
+    while head:
+        # Ignore and delete same values.
+        if prev_node.val == head.val:
+            if nodes and nodes[-1].val == prev_node.val:
+                nodes.pop()
+            head = head.next
+        # Add new values.
+        else:
+            nodes.append(head)
+            prev_node, head = head, head.next
+    if not nodes:
         return None
-    temp = new_linked = ListNode()
-    for x in range(len(to_link)):
-        temp.val = to_link[x]
-        if x != (len(to_link) - 1):
-            temp.next = ListNode()
-            temp = temp.next
-    return new_linked
+    # Reassign all nodes, we already stored only Nodes with unique values.
+    for x in range(len(nodes) - 1):
+        nodes[x].next = nodes[x + 1]
+    nodes[-1].next = None
+    return nodes[0]
 
 
-# Time complexity: O(n + m) -> looping once through whole input linked_list
-#                              and creating list without duplicates => O(n) ->
-#                              -> looping once through created no_duplicates list and creating new linked_list => O(m).
-#
-# Space complexity: O(2(log n)) -> list to store path_values, with size of == part of the input => O(log n) ->
-#                                     -> creating linked list of the same size => O(log n) -> O(2(log n)).
-# n - size of input l_list ^^
-# m - size of no_duplicates list ^^
-# --------------------------
-# Not obliged to do this in_place, so I will just rebuild linked_list.
-# ! The number of nodes in the list is in the range [0, 300] ! <- again this, returning empty head from start.
-# --------------------------
-# Next task with linked list, I will make universal test function, I don't like it overcomplicated like this.
-# It's ok for testing just lists and I comment out something I don't need,
-# but linked_lists need's to be treated better.
+# Time complexity: O(n) -> worst case == everything is unique -> traversing and appending all Nodes -> O(n) ->
+# n - Nodes of input linked list^^| -> extra traverse of the same Nodes to reassign => O(2n).
+# Auxiliary space: O(n) -> every node link is stored in a list 'nodes' => O(n).
 
 
-test1 = [1, 2, 3, 3, 4, 4, 5]
-test1_linked = create_linked(test1)
-test1_out = [1, 2, 5]
-test = delete_duplicates(test1_linked)
-print(test)
-tempo = test
-for _ in range(len(test1_out)):
-    assert test1_out[_] == tempo.val
-    tempo = tempo.next
-del test
+test: ListNode = create_linked([1, 2, 3, 3, 4, 4, 5])
+test_out: ListNode = create_linked([1, 2, 5])
+t_linked(test_out, delete_duplicates(test))
 
-test2 = [1, 1, 1, 2, 3]
-test2_linked = create_linked(test2)
-test2_out = [2, 3]
-test = delete_duplicates(test2_linked)
-print(test)
-tempo = test
-for _ in range(len(test2_out)):
-    assert test2_out[_] == tempo.val
-    tempo = tempo.next
-del test
+test = create_linked([1, 1, 1, 2, 3])
+test_out = create_linked([2, 3])
+t_linked(test_out, delete_duplicates(test))
 
-# test3 - failed -> missed part with only duplicates...
-test3 = [1, 1]
-test3_linked = create_linked(test3)
-test3_out = []
-test = delete_duplicates(test3_linked)
-print(test)
-tempo = test
-for _ in range(len(test3_out)):
-    assert test3_out[_] == tempo.val
-    tempo = tempo.next
-del test
-
-test4 = [0, 1, 2, 3, 3, 3, 3, 7, 9, 10, 10, 10]
-test4_linked = create_linked(test4)
-test4_out = [0, 1, 2, 7, 9]
-test = delete_duplicates(test4_linked)
-print(test)
-tempo = test
-for _ in range(len(test4_out)):
-    assert test4_out[_] == tempo.val
-    tempo = tempo.next
-del test
+test = create_linked([1, 2, 2, 2])
+test_out = create_linked([1])
+t_linked(test_out, delete_duplicates(test))
