0ea97a580c6aa01c79835c1f5b6c0c0b93d23274
Pampam <piankovpe@gmail.com>
2023-10-27 23:44:09 +0600

    add working_sol (11.37%, 6.31%) -> (190ms, 91.54mb)  time: O(n ** 2) | space: O(n) | Revisit

--- leetcode_problems/p106_construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ leetcode_problems/p106_construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -10,8 +10,6 @@
 # Each value of postorder also appears in inorder.
 # inorder is guaranteed to be the inorder traversal of the tree.
 # postorder is guaranteed to be the postorder traversal of the tree.
-
-
 from collections import deque
 
 
@@ -24,70 +22,62 @@ class TreeNode:
 
 
 def show_tree_level_order(root: TreeNode) -> list[int | None]:
+    # Leetcode result list == level_order.
     if not root:
         return []
     show_tree: list[int | None] = []
-    que: deque = deque()
-    que.appendleft(root)
-    # if there's only None left, then it's last level, and we have nothing to check
-    # any() <- checks everything in iterable and if there's no elements we break.
-    # any() used to eliminate extra level with nulls, when que is empty,
-    #       and still store Nulls for other levels.
-    while any(que):
+    que: deque[TreeNode | None] = deque([root])
+    while que:
         current_node: TreeNode = que.popleft()
-        if current_node is None:
+        if not current_node:
             show_tree.append(None)
             continue
         show_tree.append(current_node.val)
         que.append(current_node.left)
         que.append(current_node.right)
-
+    while not show_tree[-1]:
+        show_tree.pop()
     return show_tree
 
 
 def build_tree(inorder: list[int], postorder: list[int]) -> TreeNode | None:
-    # working_sol (22.55%, 9.94%) -> (216ms, 91.6mb)  time: O(n ** 2) | space: O(n)
+    # working_sol (11.37%, 6.31%) -> (190ms, 91.54mb)  time: O(n ** 2) | space: O(n)
     if not inorder and not postorder:
         return None
-    # postorder root is last index
+    # Current ROOT is last index of postorder.
     root: TreeNode = TreeNode(val=postorder[-1])
-    # find root position
-    in_order_index: int = inorder.index(root.val)
-    # divide left/right subtrees by ROOT in inorder
-    left_subtree: list[int] = inorder[: in_order_index]
-    right_subtree: list[int] = inorder[in_order_index + 1:]
-    # divide left/right subtrees by ROOT in postorder
+    # Find root position inside inorder.
+    inorder_index: int = inorder.index(root.val)
+    # Divide left and right subtrees by ROOT in the inorder.
+    left_subtree: list[int] = inorder[: inorder_index]
+    right_subtree: list[int] = inorder[inorder_index + 1:]
+    # Divide left and right subtrees by ROOT in the postorder.
     postorder_left_subtree: list[int] = postorder[:len(left_subtree)]
     postorder_right_subtree: list[int] = postorder[len(left_subtree):-1]
-    # go deeper, and build with backtrack path
+    # Go deeper, and build with backtrack path.
     root.left = build_tree(left_subtree, postorder_left_subtree)
     root.right = build_tree(right_subtree, postorder_right_subtree)
     return root
 
 
 # Time complexity: O(n ** 2) -> standard build method for this is O(n ** 2) for unbalanced trees,
-# n - len of preorder^^| and O(n * log n) for balanced trees.
-# k - len of inorder^^ |
+# n - len of input list 'preorder'^^|  and O(n * log n) for balanced trees.
+# k - len of input list 'inorder'^^ |
 # Auxiliary space: O(n) -> in the worst case we're going to have unbalanced tree with
 #                          all it's nodes stored in the one side, left/right w.e ->
 #                          -> because of that we're calling recursion for every node and
 #                          number of calls will be equal to n, which gives us stack with size of n => O(n).
 # ------------------
 # Mirror of p105, but in POSTORDER -> ROOT is a last index, and slicing is slightly different.
+# Slicing doesn't include last index of postorder.
 
 
-test1_in = [9, 3, 15, 20, 7]
-test1_post = [9, 15, 7, 20, 3]
-test1_out = [3, 9, 20, None, None, 15, 7]
-test = build_tree(test1_in, test1_post)
-print(show_tree_level_order(test))
-assert test1_out == show_tree_level_order(test)
-del test
+test_inorder: list[int] = [9, 3, 15, 20, 7]
+test_postorder: list[int] = [9, 15, 7, 20, 3]
+test_out: list[int | None] = [3, 9, 20, None, None, 15, 7]
+assert test_out == show_tree_level_order(build_tree(test_inorder, test_postorder))
 
-test2_in = [-1]
-test2_post = [-1]
-test2_out = [-1]
-test = build_tree(test2_in, test2_post)
-print(show_tree_level_order(test))
-assert test2_out == show_tree_level_order(test)
-del test
+test_inorder = [-1]
+test_postorder = [-1]
+test_out = [-1]
+assert test_out == show_tree_level_order(build_tree(test_inorder, test_postorder))
