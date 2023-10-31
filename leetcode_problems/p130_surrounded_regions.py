c99cd233367bf70b7b567e2235b27e45f6648f42
Pampam <piankovpe@gmail.com>
2023-10-31 22:11:51 +0600

    add working_sol (22.36%, 15.83%) -> (153ms, 24.4mb)  time: O(m * n) | space: O(m * n) | Revisit

--- leetcode_problems/p130_surrounded_regions.py
+++ leetcode_problems/p130_surrounded_regions.py
@@ -1,134 +1,83 @@
 # Given an m x n matrix board containing 'X' and 'O', capture all regions
-#   that are 4-directionally surrounded by 'X'.
+#  that are 4-directionally surrounded by 'X'.
 # A region is captured by flipping all 'O's into 'X's in that surrounded region.
 # ------------------
-# m == board.length  ,  n == board[i].length
-# 1 <= m, n <= 200  ,  board[i][j] is 'X' or 'O'.
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 200
+# board[i][j] is 'X' or 'O'
+from collections import deque
 
 
 def solve(board: list[list[str]]) -> None:
-    # working_sol (6.20%, 11.34%) -> (290ms, 47.1mb)  time: O(2 * (m - 2) * (n - 2)) | O(m * n)
-    #                                                 space: O(2 * ((m - 2) * (n - 2)) + (g * 4)) | O(m * n + g)
-    already_flipped: set[tuple[int, int]] = set()
-    path: dict[tuple[int, int]] = {}
-    max_x: int = len(board[0]) - 1
+    # working_sol (22.36%, 15.83%) -> (153ms, 24.4mb)  time: O(m * n) | space: O(m * n)
+    visited: set[tuple[int, int]] = set()
+    to_flip: list[tuple[int, int]] = []
+    # Essentially, all we care is if we hit border or not.
+    # Otherwise, it's surrounded region.
+    y_limits: set[int] = {0, len(board) - 1}
+    x_limits: set[int] = {0, len(board[0]) - 1}
     max_y: int = len(board) - 1
-
-    def is_surrounded(start: tuple[int, int]) -> bool | None:
-        y: int = start[0]
-        x: int = start[1]
-        if y == 0 or x == 0 or y == max_y or x == max_x:
-            return False
-        path[start] = True
-        blocked: set[tuple[int, int]] = set()
-        free: set[tuple[int, int]] = set()
-        if board[y + 1][x] == "X":
-            blocked.add((y + 1, x))
-        else:
-            if (y + 1, x) not in path:
-                free.add((y + 1, x))
-        if board[y - 1][x] == "X":
-            blocked.add((y - 1, x))
-        else:
-            if (y - 1, x) not in path:
-                free.add((y - 1, x))
-        if board[y][x - 1] == "X":
-            blocked.add((y, x - 1))
-        else:
-            if (y, x - 1) not in path:
-                free.add((y, x - 1))
-        if board[y][x + 1] == "X":
-            blocked.add((y, x + 1))
-        else:
-            if (y, x + 1) not in path:
-                free.add((y, x + 1))
-        if len(blocked) == 4:
-            board[y][x] = "X"
-            already_flipped.add(start)
-            return True
-        for point in free:
-            if point[0] == 0 or point[0] == max_y or point[1] == 0 or point[1] == max_x:
-                path[point] = False
-            if is_surrounded(point):
-                path[point] = True
-        if len(blocked) == 3:
-            return True
-
-    for y_ in range(1, max_y):
-        for x_ in range(1, max_x):
-            if ((y_, x_) in already_flipped) or board[y_][x_] == "X":
-                continue
-            is_surrounded((y_, x_))
-            broke: bool = False
-            for _ in path.keys():
-                if path[_] is False:
-                    for coor in path.keys():
-                        already_flipped.add(coor)
-                    broke = True
-                    path = {}
-                    break
-            if not broke:
-                for val in path.keys():
-                    board[val[0]][val[1]] = "X"
-                    already_flipped.add((val[0], val[1]))
-                path = {}
-                continue
-            for _ in path.keys():
-                already_flipped.add(_)
-            path = {}
+    max_x: int = len(board[0]) - 1
+    for y in range(len(board)):
+        for x in range(len(board[0])):
+            # Standard BFS from any 'O' we're not yet visited.
+            if (y, x) not in visited and board[y][x] == 'O':
+                que: deque[tuple[int, int]] = deque([(y, x)])
+                region: list[tuple[int, int]] = [(y, x)]
+                flipping: bool = True
+                while que:
+                    cell: tuple[int, int] = que.popleft()
+                    region.append(cell)
+                    _y: int = cell[0]
+                    _x: int = cell[1]
+                    # Border hit == not flipping, but still need to mark other 'O's of the region
+                    #  as visited to skip later.
+                    if flipping and (_y in y_limits or _x in x_limits):
+                        flipping = False
+                    if 0 <= (_y - 1) and (_y - 1, _x) not in visited and board[_y - 1][_x] == 'O':
+                        que.append((_y - 1, _x))
+                        visited.add((_y - 1, _x))
+                    if max_y >= (_y + 1) and (_y + 1, _x) not in visited and board[_y + 1][_x] == 'O':
+                        que.append((_y + 1, _x))
+                        visited.add((_y + 1, _x))
+                    if 0 <= (_x - 1) and (_y, _x - 1) not in visited and board[_y][_x - 1] == 'O':
+                        que.append((_y, _x - 1))
+                        visited.add((_y, _x - 1))
+                    if max_x >= (_x + 1) and (_y, _x + 1) not in visited and board[_y][_x + 1] == 'O':
+                        que.append((_y, _x + 1))
+                        visited.add((_y, _x + 1))
+                if flipping:
+                    to_flip = to_flip + region
+    for y, x in to_flip:
+        board[y][x] = 'X'
 
 
-# Time complexity: O(2 * (m - 2) * (n - 2)) -> traversing once through whole matrix and ignoring indexes
-# n - len of matrix rows^^ | we have already met in the recursion, ignoring borders as well => O((m - 2) * (n - 2)) ->
-# m - num of matrix col's^^| -> changing indexes on the path into "X", worst case: borders is "X" and insides only "O"
-#                            same path as in recursion, but with changing values on coor => O((m - 2) * (n - 2)) ->
-#                            -> assuming that HASH operations is O(1) like in p128, than reading, clearing path(dict())
-#                            adding elements in already_flipped(set()) and path itself => O(1) -> -> leading us to =>
-#                            => O((m - 2)  * (n - 2)) + O((m -2) * (n -2)) => O(2 * (m - 2) * (n - 2)).
-# Space complexity: O(2 * ((m - 2) * (n - 2)) + (g * 4)) -> worst case == every border index is "X" insides "O" ->
-# g - num of unique "O" ^^| -> extra dict() to store path, which in the worst case can be size of (m - 2) * (n - 2) ->
-#                            -> same goes to the already_flipped(set()), values on the  path we walk added into it =>
-#                            => O(2 * ((m - 2) * (n - 2))) -> every recursion creates two extra sets to store
-#                            all values around coordinate it called(start), both of them store only 4 values
-#                            distributed between them => O(g * 4), recursion called only once for every unique "O".
-# ------------------
-# Solution is a mess and not so good on performance but working.
-# Can be changed to remove recursion and check less extra pointers, but maybe later.
-# There's too much to do to stop for this.
-# ------------------
-# Actually all I needed to check if any "O" connected to a borders, otherwise any path can be changed to "X",
-# because there's only "X" and "O", so if there's no border connection means it's blocked for sure.
-# But how to stop check? Because right now im checking every point on a path without breaking when we get to border.
-# How to correctly break recursion in this case?
-# Assuming that we have O(1) speed to check dictionary like in p128, then I could check dict(),
-# before starting any recursion and insta break from it? Hmm. But that's what im already doing, because
-# right now we're checking WHOLE path and after coming out from recursion flipping all path, or
-# adding it into already_flipped and ignore next_time we meet this coordinate.
-# It's changing nothing. W.e google or revisit, after I get more experience and maybe change it.
-# ------------------
-# Seems working, but I don't want to create nore test cases, so let's fail.
-# ------------------
-# Ok. P79 but simpler on the first look, because there's no check for correct matrix, and we can insta add
-# flipped "O" indexes and ignore them after.
-# There's no limit to what use, so I will stick to recursion, because iterations is way harder for this case.
+# Time complexity: O(m * n) -> see 2 worst cases:
+# m - height of input matrix 'board'^^| First: whole board is 'O' -> then we will check every cell => O(m * n).
+# n - length of input matrix 'board'^^| Second: only borders are 'x' -> we will visit ((m - 2) * (n - 2)) cells,
+#                                               and extra traverse to flip them => O(2 * ((m - 2) * (n - 2)).
+#                             Still Linear, so it should be correct to say => O(m * n).
+# Auxiliary space: O(m * n) -> second worst case -> set 'visited' will have ((m - 2) * (n - 2)) cells ->
+#                              -> 'que' with the same size and 'to_flip' with the same size => O(3 * (m * n)).
 
 
-test1 = [["X", "X", "X", "X"], ["X", "O", "O", "X"], ["X", "X", "O", "X"], ["X", "O", "X", "X"]]
-test1_out = [["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "O", "X", "X"]]
-solve(test1)
-assert test1_out == test1
+test: list[list[str]] = [["X", "X", "X", "X"], ["X", "O", "O", "X"], ["X", "X", "O", "X"], ["X", "O", "X", "X"]]
+test_out: list[list[str]] = [["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "O", "X", "X"]]
+solve(test)
+assert test_out == test
 
-test2 = [["X"]]
-test2_out = [["X"]]
-solve(test2)
-assert test2_out == test2
+test = [["X"]]
+test_out = [["X"]]
+solve(test)
+assert test_out == test
 
-test3 = [["O"]]
-test3_out = [["O"]]
-solve(test3)
-assert test3_out == test3
+test = [["O"]]
+test_out = [["O"]]
+solve(test)
+assert test_out == test
 
-test4 = [
+test = [
     ["O", "O", "X", "O", "O", "O"],
     ["X", "O", "O", "O", "O", "X"],
     ["X", "X", "X", "X", "X", "X"],
@@ -137,7 +86,7 @@ test4 = [
     ["X", "O", "O", "O", "X", "O"],
     ["O", "X", "X", "X", "O", "O"],
 ]
-test4_out = [
+test_out = [
     ['O', 'O', 'X', 'O', 'O', 'O'],
     ['X', 'O', 'O', 'O', 'O', 'X'],
     ['X', 'X', 'X', 'X', 'X', 'X'],
@@ -146,28 +95,22 @@ test4_out = [
     ['X', 'X', 'X', 'X', 'X', 'O'],
     ['O', 'X', 'X', 'X', 'O', 'O'],
 ]
-solve(test4)
-assert test4_out == test4
+solve(test)
+assert test_out == test
 
-# test5 - failed -> I was using set(), just to store values along the path and didn't consider the case.
-#                   If we have only 1 value which returns True it still will be added on a path,
-#                   and if it's value was a last one to check path will have this value in the end.
-#                   When he should because path should be cleared if at least one value is not correct.
-#                   Either I should change it into a dict() and check if all keys added have same value,
-#                   or I can break recursion and clear path after we met boarder points. How?
-test5 = [
+test = [
     ["O", "X", "X", "O", "X"],
     ["X", "O", "O", "X", "O"],
     ["X", "O", "X", "O", "X"],
     ["O", "X", "O", "O", "O"],
     ["X", "X", "O", "X", "O"],
 ]
-test5_out = [
+test_out = [
     ['O', 'X', 'X', 'O', 'X'],
     ['X', 'X', 'X', 'X', 'O'],
     ['X', 'X', 'X', 'O', 'X'],
     ['O', 'X', 'O', 'O', 'O'],
     ['X', 'X', 'O', 'X', 'O'],
 ]
-solve(test5)
-assert test5_out == test5
+solve(test)
+assert test_out == test
