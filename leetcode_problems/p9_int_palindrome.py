73af075e6041940da045c97fbb256129e9c9fbf2
Pampam <piankovpe@gmail.com>
2023-11-22 18:11:40 +0600

    add working_sol (66.92%, 43.50%) -> (58ms, 16.2mb)  time: O(log x) | space: O(1) | Revisit

--- leetcode_problems/p9_int_palindrome.py
+++ leetcode_problems/p9_int_palindrome.py
@@ -1,32 +1,40 @@
-# Given an integer x, return true if x is a
-# palindrome, and false otherwise.
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+#
 # Follow up: Could you solve it without converting the integer to a string?
+# ------------------------
+# -2 ** 31 <= x <= 2 ** 31 - 1
 
 
 def is_palindrome(x: int) -> bool:
-    # string sol
-    # num = list(str(x))
-    # if len(num) == 1:
-    #     return True
-    # for n in range(len(num)):
-    #     if num[n] == num[-n - 1]:
-    #         continue
-    #     else:
-    #         return False
-    # return True
-    # int sol?
-    if x < 0 or x % 10 == 0 and x != 0:  # can't be negative and only 0 can be palindrome
+    # working_sol (66.92%, 43.50%) -> (58ms, 16.2mb)  time: O(log x) | space: O(1)
+    # '-' no way around negative sign.
+    if x < 0:
         return False
-    new: int = 0
-    while x > new:
-        new = int(new * 10 + x % 10)  # appending last digit of x to a new and adding multiplied prev modulus
-        x = x // 10  # removing last digit from x
-    return x == new or x == new // 10  # if length is odd remove 1 extra digit from new, cuz x < new after loop
+    # (10 ** w.e) no way around first 1. Except 0.
+    if x % 10 == 0 and x > 0:
+        return False
+    # Deconstruct by taking last digits of 'x', one by one.
+    half: int = 0
+    while x > half:
+        half = half * 10 + x % 10
+        x //= 10
+    # Even or Odd palindrome.
+    # In both cases 'half' will be higher|equal than original 'x' and we need extra check.
+    return x == half or x == half // 10
 
 
-print(is_palindrome(121121121))
-print(is_palindrome(-202202202))
-print(is_palindrome(-1))
-print(is_palindrome(202202202))
-print(is_palindrome(121))
-print(is_palindrome(150))
+# Time complexity: O(log x) -> essentially we will check only part of the input 'x' => O(log x).
+# Auxiliary space: O(1) -> only 1 constant INT => O(1).
+
+
+test: int = 121
+test_out: bool = True
+assert test_out == is_palindrome(test)
+
+test = -121
+test_out = False
+assert test_out == is_palindrome(test)
+
+test = 10
+test_out = False
+assert test_out == is_palindrome(test)
