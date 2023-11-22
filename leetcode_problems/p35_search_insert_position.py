b7136fad27feac0b2349149145fd1bb99878ed3e
Pampam <piankovpe@gmail.com>
2023-11-22 17:36:51 +0600

    add working_sol (39.71%, 59.38%) -> (57ms, 17mb)  time: O(log n) | space: O(1) | Revisit

--- leetcode_problems/p35_search_insert_position.py
+++ leetcode_problems/p35_search_insert_position.py
@@ -1,77 +1,40 @@
-# Given a sorted array of distinct integers and a target value,
-# return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-#
+# Given a sorted array of distinct integers and a target value, return the index if the target is found.
+# If not, return the index where it would be if it were inserted in order.
 # You must write an algorithm with O(log n) runtime complexity.
+# -------------------
+# 1 <= nums.length <= 10 ** 4
+# -10 ** 4 <= nums[i] <= 10 ** 4
+# nums contains distinct values sorted in ascending order.
+# -10 ** 4 <= target <= 10 ** 4
+
 
 def search_insert(nums: list[int], target: int) -> int:
-    # working_sol (56.8%, 70.38%) time: O(log n) | space: O(1)
-    length = len(nums)
-    if length == 0:
-        return 0
-    if length == 1:
-        if nums[0] == target:
-            return 0
-    if nums[-1] < target:
-        return length
-    if nums[0] > target:
-        return 0
-    x, y = 0, (length - 1)
-    while x <= y:
-        if nums[x] == target:
-            return x
-        if nums[y] == target:
-            return y
-        if target > nums[x]:
-            if target < nums[x + 1] and target != nums[x + 1]:
-                return x + 1
-        if target < nums[y]:
-            if target > nums[y - 1] and target != nums[y - 1]:
-                return y
-        if nums[x] < target < nums[y]:
-            x += 1
-            y -= 1
-
-# Time complexity: O(log n) -> only using one conquer_divide loop, which always O(log n).
-# Space complexity: O(1) -> creating only 1 new constant LENGTH, not changing anything.
-
-# Cringe fails. Only because I didn't think about most simple problems that can occur and revisit code.
-# Considered failing at under, over limit's and ignored len == 1, target == nums[0]....
-# I need to stop rushing solutions looking easy af.
+    # working_sol (39.71%, 59.38%) -> (57ms, 17mb)  time: O(log n) | space: O(1)
+    # Standard BinarySearch.
+    left_l: int = 0
+    right_l: int = len(nums) - 1
+    while left_l <= right_l:
+        middle: int = (left_l + right_l) // 2
+        if nums[middle] == target:
+            return middle
+        elif nums[middle] < target:
+            left_l = middle + 1
+        else:
+            right_l = middle - 1
+    return left_l
 
 
-test1 = [1, 3, 5, 6]
-test1_target = 5
-test1_out = 2
-print(search_insert(test1, test1_target))
+# Time complexity: O(log n) -> standard BS on array => O(log n).
+# n - length of input array 'nums'^^|
+# Space complexity: O(1) -> only 3 constant INTs none of them depends on input => O(1).
 
-test2 = [1, 3, 5, 6]
-test2_target = 2
-test2_out = 1
-print(search_insert(test2, test2_target))
 
-test3 = [1, 3, 5, 6]
-test3_target = 7
-test3_out = 4
-print(search_insert(test3, test3_target))
+test: list[int] = [1, 3, 5, 6]
+test_target: int = 5
+test_out: int = 2
+assert test_out == search_insert(test, test_target)
 
-test4 = [1]
-test4_target = 1
-test4_out = 0
-print(search_insert(test4, test4_target))
-# test4 failed - cuz I rushed and didn't consider len == 1 with value equal to target
-
-test5 = [1]
-test5_target = 0
-test5_out = 0
-print(search_insert(test5, test5_target))
-
-test6 = [1]
-test6_target = 2
-test6_out = 1
-print(search_insert(test6, test6_target))
-
-test7 = [1, 3, 5]
-test7_target = 3
-test7_out = 1
-print(search_insert(test7, test7_target))
-# test7 failed - limited x != y......
+test = [1, 3, 5, 6]
+test_target = 2
+test_out = 1
+assert test_out == search_insert(test, test_target)
