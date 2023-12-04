a2d1446dfa9e80ebd82c268d05397946b11e004f
Pampam <piankovpe@gmail.com>
2023-12-04 18:19:07 +0600

    add working_sol (77.24%, 79.58%) -> (44ms, 16.6mb)  time: O(log n) | space: O(1)

--- leetcode_problems/p33_search_in_rotated.py
+++ leetcode_problems/p33_search_in_rotated.py
@@ -1,96 +1,81 @@
 # There is an integer array nums sorted in ascending order (with distinct values).
-#
-# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index
-# k (1 <= k < nums.length) such that the resulting array is
+# Prior to being passed to your function, nums is possibly rotated at an unknown
+#  pivot index == k (1 <= k < nums.length) such that the resulting array is
 # [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
 # For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
-#
 # Given the array nums after the possible rotation and an integer target,
 # return the index of target if it is in nums, or -1 if it is not in nums.
-#
 # You must write an algorithm with O(log n) runtime complexity.
+# --------------------------
+# 1 <= nums.length <= 5000
+# -10 ** 4 <= nums[i] <= 10 ** 4
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -10 ** 4 <= target <= 10 ** 4
+from random import randint
 
 
 def search_rotated(nums: list[int], target: int) -> int:
-    # working_sol (33.23%, 10.94%) time O(log n) | space O(1)
-    if len(nums) == 0:
+    # working_sol (77.24%, 79.58%) -> (44ms, 16.6mb)  time: O(log n) | space: O(1)
+    if not nums:
         return -1
-    elif len(nums) == 1:
-        if nums[0] == target:
-            return 0
+    left: int = 0
+    right: int = len(nums) - 1
+    while left < right:
+        middle: int = (left + right) // 2 + 1
+        # Ascending slice: left -> middle.
+        if nums[left] <= nums[middle]:
+            # Target inside this slice.
+            if nums[left] <= target < nums[middle]:
+                right = middle - 1
+            else:
+                left = middle
+        # Middle in pivoted part.
         else:
-            return -1
-    # if rotated
-    elif nums[-1] < nums[0]:
-        if nums[-1] < target < nums[0]:
-            return -1
-        elif target >= nums[0]:
-            for x in range(len(nums)):
-                if nums[x] == target:
-                    return x
-                elif nums[x] > nums[x + 1]:
-                    return -1
-        elif target <= nums[-1]:
-            for x in range(len(nums) - 1, -1, -1):
-                if nums[x] == target:
-                    return x
-                elif nums[x] < nums[x - 1]:
-                    return -1
-    # if not rotated
-    elif target > nums[-1]:
-        return -1
-    elif target < nums[0]:
-        return -1
-    elif nums[0] <= target <= nums[-1]:
-        for x in range(len(nums)):
-            if nums[x] == target:
-                return x
+            # Ascending slice with target in pivoted part: middle -> right.
+            if nums[middle] <= target <= nums[right]:
+                left = middle
+            else:
+                right = middle - 1
+    if nums[left] == target:
+        return left
     return -1
 
 
-# Failed 2 attempts, cuz rushed and didn't think about simple options:
-#   1 fail - if we didn't find any target in not rotated list | simple return of -1
-#   2 fail - if len(nums) == 1 no reasons to use loop | return -1 at start
-#   3 could be fail - len(nums) == 0. we can't loop through | return -1 at start
-# Always check pre commit. Even when it's look ultra simple.
-# Time complexity O(log n): worst case we loop through 3/4 of input array.
-#   as I understood: O(n) -> one loop, O(n**2) -> loop in loop, O(log n) -> not full loop with conditions
-# Space complexity O(1): we're not creating anything new or changing input.
-#   as I  understood: O(n) -> creating new array, O(1) -> not creating any new arrays, but only simple values.
-# Not 100% sure about time, space complexity and I need to learn more about that formula not just most_popular cases.
+# Time complexity: O(log n) <- n - length of input array 'nums'.
+# Standard BinarySearch algorithm.
+# --------------------------
+# Auxiliary space: O(1).
+# Only 2 extra constant INTs used.
 
-tests = {}
-# if rotated -> nums[-1] always lower than nums[0]
-# else nums[0] always lower than nums[-1]
-test1 = [4, 5, 6, 7, 0, 1, 2]
-test1_target = 0
-test1_out = 4
-tests[(test1_out, test1_target)] = test1
-print(search_rotated(test1, test1_target))
 
-test2 = [1]
-test2_target = 1
-test2_out = 0
-tests[(test2_out, test2_target)] = test2
-print(search_rotated(test2, test2_target))
+test: list[int] = [4, 5, 6, 7, 0, 1, 2]
+test_target: int = 0
+test_out: int = 4
+assert search_rotated(test, test_target) == test_out
 
-test3 = []
-test3_target = 5
-test3_out = -1
-tests[(test3_out, test3_target)] = test3
-print(search_rotated(test3, test3_target))
+test = [1]
+test_target = 1
+test_out = 0
+assert search_rotated(test, test_target) == test_out
 
-test4 = [1, 2, 3, 4, 5, 6, 7]
-test4_target = 4
-test4_out = 3
-tests[(test4_out, test4_target)] = test4
-print(search_rotated(test4, test4_target))
+test = []
+test_target = 5
+test_out = -1
+assert search_rotated(test, test_target) == test_out
 
-test5 = [4, 5, 6, 7, 0, 1, 2]
-test5_target = 0
-test5_out = 4
-tests[(test5_out, test5_target)] = test5
-print(search_rotated(test5, test5_target))
+test = [1, 2, 3, 4, 5, 6, 7]
+test_target = 4
+test_out = 3
+assert search_rotated(test, test_target) == test_out
 
-for key, value in tests.items():
-    assert search_rotated(value, key[1]) == key[0]
+test = [4, 5, 6, 7, 0, 1, 2]
+test_target = 0
+test_out = 4
+assert search_rotated(test, test_target) == test_out
+
+test = sorted(set([randint(-10 ** 4, 10 ** 4) for _ in range(5000)]))
+pivot_point: int = randint(0, len(test) - 1)
+print(test[pivot_point], 'pivoted at:', pivot_point)
+test = test[pivot_point:] + test[:pivot_point]
+print(test)
