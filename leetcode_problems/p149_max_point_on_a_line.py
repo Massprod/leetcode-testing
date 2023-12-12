6a1429e018de5625bbd19b90c68f92984b5d66b1
Pampam <piankovpe@gmail.com>
2023-12-13 00:05:44 +0600

    add working_sol (92.92%, 90.30%) -> (64ms, 16.23mb)  time: O(n ** 2) | space: O(n)

--- leetcode_problems/p149_max_point_on_a_line.py
+++ leetcode_problems/p149_max_point_on_a_line.py
@@ -1,83 +1,93 @@
 # Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane,
-# return the maximum number of points that lie on the same straight line.
+#  return the maximum number of points that lie on the same straight slope.
 # -----------------------
-# 1 <= points.length <= 300  ,  points[i].length == 2  ,  -104 <= xi, yi <= 104
+# 1 <= points.length <= 300
+# points[i].length == 2
+# -10 ** 4 <= xi, yi <= 10 ** 4
 # All the points are unique.
-import math
 
 
 def max_points(points: list[list[int]]) -> int:
-    # working_sol (66.22%, 29.41%) -> (92ms, 16.5mb)  time: O((n * n) * (log n)) | space: O(n)
-    length: int = len(points)
-    if length == 1:
-        return 1
-    if length == 2:
-        return 2
+    # working_sol (92.92%, 90.30%) -> (64ms, 16.23mb)  time: O(n ** 2) | space: O(n)
+    # 1 point == 1 line.
+    # 2 points == 1 line.
+    if len(points) <= 2:
+        return len(points)
 
-    def normalize_float(x: tuple[int, int], y: tuple[int, int]) -> tuple[int, int]:
-        run: int = y[0] - x[0]
+    def find_slope(point1: tuple[int, int], point2: tuple[int, int]) -> float | str:
+        # slope = (rise / run) = (y2 - y1) / (x2 - x1) = (vertical change / horizontal change)
+        # (x2 - x1)
+        run: int = point2[0] - point1[0]
+        # Vertical line == slope undefined.
+        # We build from `point1_` to w.e others.
+        # So, every undefined slope == +1 point on vertical line, which starts at `point1_`.
         if run == 0:
-            return 1, 0
-        if run < 0:
-            x, y = y, x
-            run = y[0] - x[0]
-        rise: int = y[1] - x[1]
-        greatest_common_div: int = math.gcd(abs(rise), abs(run))
-        return rise // greatest_common_div, run // greatest_common_div
+            return 'vertical'
+        # (y2 - y1)
+        rise: int = point2[1] - point1[1]
+        # (x2 - x1) == 0 => x2 == x1 AND (y2 - y1) == 0 => y2 == y1
+        # Same coordinates, so w.e the line we're going to build from `point1_` it will include all `duplicates`.
+        if run == 0 and rise == 0:
+            return 'duplicates'
+        return rise / run
 
     max_on_line: int = 0
-    for g in range(length):
-        point: tuple = tuple(points[g])
-        slopes: dict = {}
-        for h in range(g + 1, length):
-            pair: tuple = tuple(points[h])
-            if point == pair:
+    # Start from every point possible.
+    # But don't recheck previous points.
+    # Because we already built line from previous -> current.
+    # And if we build this line from current point again, we will simply get current -> previous.
+    # Which is the same line, and we already checked this line slope.
+    for x in range(len(points)):
+        # (x, y)
+        point1_: tuple[int, int] = (points[x][0], points[x][1])
+        # All lines with `point1_` on them.
+        # Same slope == same line.
+        lines: dict[tuple[int, int] | str, int] = {
+            'vertical': 1,
+            'duplicates': 0,
+        }
+        for y in range(x + 1, len(points)):
+            point2_: tuple[int, int] = (points[y][0], points[y][1])
+            slope: float | str = find_slope(point1_, point2_)
+            # Line already exist.
+            if slope in lines:
+                lines[slope] += 1
                 continue
-            slope: tuple = normalize_float(point, pair)
-            if slope in slopes:
-                slopes[slope] += 1
-                continue
-            slopes[slope] = 2
-        if slopes.values():
-            max_on_line = max(max_on_line, max(slopes.values()))
+            # New line between 2 points == 2 points on it.
+            lines[slope] = 2
+        if lines:
+            max_on_line = max(max_on_line, lines.pop('duplicates') + max(lines.values()))
     return max_on_line
 
 
-# Time complexity: O((n * n) * (log n)) -> traversing whole input_list with nested loop => O(n * n) ->
-# n - length of input_list^^   -> for every point we check, normalizing their slope(float),
-#                                 constant actions with same input sizes => O(1) ->
-#                              -> for every index g we're trying to check, creating dictionary and populating it
-#                                 only with part of the input_list values => O(log n) ->
-#                              -> choosing max_on_line between int and populated dictionary => O(1) <- hash table.
-# Space complexity: O(n) -> creating extra dictionary, with size of input_list in the worst_case ->
-# n - length of input_list^^ -> every point will have unique slope with our check_point(points[g]) => O(n)
-# !
-#   For each point p, calculate its slope with other points and use a map to record
-#  how many points have same slope, by which we can find out how many points are on same line
-#  with p as their one point. For each point keep doing the same thing and update
-#  the maximum number of point count found so far.
-#   To get rid of the precision problems, we treat slope as pair ((y2 – y1), (x2 – x1))
-#  instead of ratio, and reduce pair by their gcd before inserting into map. !
-#  No way to solve it without knowing this math ^^.
-# I was trying to solve it, like queen problem...
+# Time complexity: O(n ** 2) <- n - length of input array `points`.
+# Inner loop iterates over the remaining points for every index(point) in `points`.
+# And outer loop iterates for every point in `points` as well.
+# Auxiliary space: O(n).
+# We only store slopes we can build from one `point1_` to every other point in `points`.
+# In the worst case we will have all unique slopes, so we will store only `n` slopes.
 
 
-test1 = [[1, 1], [2, 2], [3, 3]]
-test1_out = 3
-print(max_points(test1))
+test: list[list[int]] = [[1, 1], [2, 2], [3, 3]]
+test_out: int = 3
+assert test_out == max_points(test)
 
-test2 = [[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]
-test2_out = 4
-print(max_points(test2))
+test = [[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]
+test_out = 4
+assert test_out == max_points(test)
 
-# test3 - failed -> failed to consider, that every 2 points can be a 1 line at any coordinates,
-#                   so if we only have >= (2 points) -> it's by default 2 points_straight_line.
-#                   And only after that we should count everything else.
-test3 = [[2, 3], [1, 1]]
-test3_out = 2
-print(max_points(test3))
+test = [[2, 3], [1, 1]]
+test_out = 2
+assert test_out == max_points(test)
 
-# test4 - failed -> not on diagonals and hor,vert. How can we make a line?
-test4 = [[-6, -1], [3, 1], [12, 3]]
-test4_out = 3
-print(max_points(test4))
+test = [[-6, -1], [3, 1], [12, 3]]
+test_out = 3
+assert test_out == max_points(test)
+
+test = [[0, 0], [1, -1], [1, 1]]
+test_out = 2
+assert test_out == max_points(test)
+
+test = [[2, 3], [3, 3], [-5, 3]]
+test_out = 3
+assert test_out == max_points(test)
