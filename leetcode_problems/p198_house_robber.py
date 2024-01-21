c4f2df058b7b44fa218cc05caed18f8bdfbff98a
Pampam <piankovpe@gmail.com>
2024-01-21 10:59:14 +0600

    update working_sol (88.64%, 56.96%) -> (32ms, 16.56mb)  time: O(n) | space: O(1)

--- leetcode_problems/p198_house_robber.py
+++ leetcode_problems/p198_house_robber.py
@@ -11,48 +11,37 @@ from random import randint
 
 
 def rob(nums: list[int]) -> int:
-    # working_sol (88.91%, 76.11%) -> (41ms, 16.2mb)  time: O(n) | space: O(1)
-    for x in range(2, len(nums)):
-        f_option: int = nums[x - 2]
-        s_option: int = 0
-        # ignoring first 2, for correct pathing
-        if x > 2:
-            s_option = nums[x - 3]
-        # first is best
-        if f_option > s_option:
-            nums[x] = f_option + nums[x]
-            continue
-        # second is best
-        if s_option > f_option:
-            nums[x] = s_option + nums[x]
-            continue
-        # equal
-        nums[x] = s_option + nums[x]
-    # 2 maximized paths, so we need to choose only 1
-    if len(nums) > 1:
-        return max(nums[-1], nums[-2])
-    # case with 1 house
-    return nums[-1]
+    # working_sol (88.64%, 56.96%) -> (32ms, 16.56mb)  time: O(n) | space: O(1)
+    # With 2 or less houses, we can only choose one.
+    if len(nums) < 3:
+        return max(nums)
+    # If we start from First house, we can continue only from Third.
+    nums[2] += nums[0]
+    for x in range(3, len(nums)):
+        nums[x] += max(nums[x - 2], nums[x - 3])
+    # We will have 2 maximized paths: ending on last house, and pre-last.
+    # We need the highest value.
+    return max(nums[-1], nums[-2])
 
 
-# Time complexity: O(n) -> traversing through (n - 2) indexes of input_array => O(n - 2) -> O(n).
-# n - len of input_array^^|
-# Auxiliary space: O(1) -> using original input_array to store maximized_paths, and only 2 extra INTs used => O(1).
+# Time complexity: O(n) <- n - length of input array `nums`.
+# Single traverse of the whole input array `nums`.
 # ----------------------
-# Ok. DP is fun, but im still need to learn more about it.
+# Auxiliary space: O(1).
+# Nothing extra used, reusing input array `nums`.
 # ----------------------
 # Extra part, logic and counting is correct. But there's a paths which could be ended on -1 and -2.
 # So we need to find best option from this two, otherwise we're missing maximized path and just returning
-# maximized path leading to a LAST house, and it could be wrong.
-# And extra to that edge case with only 1 house, just return itself.
+# maximized path leading to the LAST house, and it could be wrong.
+# And extra to that, edge case with only 1 house: just return this house value.
 # ----------------------
 # DP problem, and after a night of thinking finally see how it can be solved:
 # Like -> 1, 2, 3, 4, 5 ->
 # -> If we use standard recursion then we're checking paths like 1 -> 3 -> 5 | 1 -> 5 | 3 -> 5 | 2 -> 4 | 2 -> 5
-# which can be actually culled into 1 -> 3 -> 5 | 2 -> 4 | 2 -> 5
-# because we're having only positive integers and there's no reason to skip any houses.
+#  which can be actually culled into 1 -> 3 -> 5 | 2 -> 4 | 2 -> 5
+#  because we're having only positive integers and there's no reason to skip any houses.
 # So we can just summarize everything on this 3 paths and any other path which is going to be continuation of them,
-# and it's already going to have maximized previous part of the array.
+#  and it's already going to have maximized previous part of the array.
 # Like if we start a path from 5 we already have maximized option from previous indexes(houses).
 # We can just use [x - 2] or [x - 3] + [x] => and we will cover all these paths together.
 # Like if we add 6 into this sequence, actually is better to use normal values =>
@@ -65,7 +54,7 @@ def rob(nums: list[int]) -> int:
 #      0 -> 3 | 1 -> 3  <- anything else is in a cop_zone, and we can't use it.
 # => 11, 5, 28, 25, 11, 12
 # -> start == 11 -> x == 4 -> [2] == 28 or [1] == 5 -> best option => [4] = 28 + 11 == 39
-#    ^^Again we covering all options: 0 -> 2 -> 4 | 1 -> 4 |
+#    ^^Again we're covering all options: 0 -> 2 -> 4 | 1 -> 4 |
 #      And because we already have best option stored in 2 we can just take it, and be sure
 #      that's maximized value we could get before 2.
 # => 11, 5, 28, 25, 39, 12
@@ -78,48 +67,27 @@ def rob(nums: list[int]) -> int:
 #       1 -> 3 same
 #       But we're not storing these paths, we're storing BEST option we could take on path before we hit some house.
 #       So we can freely just take index 3 or 2 as best option and be sure it's maximized.
-# --------------
-# Didn't test with big arrays but this should be correct for any size, at least logic seems correct.
 
 
-test1 = [1, 2, 3, 1]
-test1_out = 4
-test_ = rob(test1)
-print(test_)
-assert test1_out == test_
-del test_
+test: list[int] = [1, 2, 3, 1]
+test_out: int = 4
+assert test_out == rob(test)
 
-test2 = [2, 7, 9, 3, 1]
-test2_out = 12
-test_ = rob(test2)
-print(test_)
-assert test2_out == test_
-del test_
+test = [2, 7, 9, 3, 1]
+test_out = 12
+assert test_out == rob(test)
 
-test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-test3_out = 36
-test_ = rob(test3)
-print(test_)
-assert test3_out == test_
-del test_
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+test_out = 36
+assert test_out == rob(test)
 
-test4 = [1]
-test4_out = 1
-test_ = rob(test4)
-print(test_)
-assert test4_out == test_
-del test_
+test = [1]
+test_out = 1
+assert test_out == rob(test)
 
-test5 = [1, 333, 99]
-test5_out = 333
-test_ = rob(test5)
-print(test_)
-assert test5_out == test_
-del test_
+test = [1, 333, 99]
+test_out = 333
+assert test_out == rob(test)
 
-# testing with leetcode testcases, all correct
-# test = []
-# for _ in range(73):
-#     test.append(randint(0, 400))
-# print(test)
-# print(rob(test))
+test = [randint(0, 400) for _ in range(100)]
+print(test)
