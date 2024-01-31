2f6127e6a41917bc05cb0d0f857bdef6c6c7cc76
Pampam <piankovpe@gmail.com>
2024-01-31 12:08:30 +0600

    update working_sol (97.56%, 99.93%) -> (858ms, 29.08mb)  time: O(n) | space: O(n)

--- leetcode_problems/p739_daily_temperatures.py
+++ leetcode_problems/p739_daily_temperatures.py
@@ -2,31 +2,34 @@
 #   return an array answer such that answer[i] is the number of days you have to wait
 #   after the ith day to get a warmer temperature.
 # If there is no future day for which this is possible, keep answer[i] == 0 instead.
-# -------------
+# ---------------------
 # 1 <= temperatures.length <= 10 ** 5
 # 30 <= temperatures[i] <= 100
 
 
 def daily_temps(temperatures: list[int]) -> list[int]:
-    # working_sol (91.19%, 99.90%) -> (1048ms, 29.2mb)  time: O(n) | space: O(n)
-    # Store everything.
+    # working_sol (97.56%, 99.93%) -> (858ms, 29.08mb)  time: O(n) | space: O(n)
+    # [indexes of days with lower temperature]
     list_stack: list[int] = []
-    for x in range(len(temperatures)):
+    for day, temperature in enumerate(temperatures):
         # Delete everything lower, and because we store day indexes we can find distance.
-        while list_stack and temperatures[list_stack[-1]] < temperatures[x]:
-            day: int = list_stack.pop()
-            temperatures[day] = x - day
-        list_stack.append(x)
-    # Annul days which didn't meet higher Temps.
-    for _ in list_stack:
-        temperatures[_] = 0
+        while list_stack and temperatures[list_stack[-1]] < temperature:
+            lower_day: int = list_stack.pop()
+            temperatures[lower_day] = day - lower_day
+        list_stack.append(day)
+    # Annul days which didn't meet higher Temperature days.
+    for day in list_stack:
+        temperatures[day] = 0
     return temperatures
 
 
-# Time complexity: O(n) -> in the worst case, everything is lower except last value ->
-# n - len of input_array^^| -> then we will traverse input once, to add everything in a stack and extra traverse
-#                           stack to get distances => O(2n).
-# Auxiliary space: O(n) -> changing temperatures inplace, but still stack can be a size of input == n => O(n).
+# Time complexity: O(n) <- n - length of input array `temperatures`.
+# Worst case: everything is lower except last value.
+# Then we will traverse input once, to add everything in a stack => O(n).
+# And extra traverse stack to get distances => O(n - 1).
+# ---------------------
+# Auxiliary space: O(n).
+# Changing temperatures inplace, but still stack can be a size of input == `n` => O(n).
 # -------------
 # Standard mono stack with deleting everything lower. Store every index so far and find distance, between
 # stored index and index on which Higher temperature was met.
